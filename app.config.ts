e6b12021f320c325459b97b702e24b201b5cc684
silasfelinus <silasfelinus@gmail.com>
2023-08-07 21:06:00 -0700

    public token added

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,6 @@
 // this is for all non-sensitive config information
 export default defineAppConfig({
   model: 'gpt-3.5-turbo',
-  completion: 'https://api.openai.com/v1/completions'
+  completion: 'https://api.openai.com/v1/completions',
+  NUXT_PUBLIC_STUDIO_TOKENS: 'c5957273350364a6e67371ac16c1818137d62ff8d8e454b5d5eb7093298d347c'
 })

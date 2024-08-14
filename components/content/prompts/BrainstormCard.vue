2a9df0df03b2830736117f01e412d516e318a262
Silas Knight <silasfelinus@gmail.com>
2024-08-14 03:47:29 -0700

    replaced pageStore stragglers

--- components/content/prompts/BrainstormCard.vue
+++ components/content/prompts/BrainstormCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="min-x-200 min-y-100 bg-base-200 rounded-2xl p-2 m-2 border">
     <h3 class="text-lg font-bold">
-      {{ idea.title }}
+      {{ props.idea.title }}
     </h3>
     <p class="text-base">
-      {{ idea.example }}
+      {{ props.idea.example }}
     </p>
   </div>
 </template>

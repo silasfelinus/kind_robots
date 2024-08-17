45d4ded2ea6a2dd67893deb2f0de5574fede885e
Silas Knight <silasfelinus@gmail.com>
2024-08-16 17:16:23 -0700

    minor tweak

--- components/content/tooltips/HeaderDashboard.vue
+++ components/content/tooltips/HeaderDashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="relative flex flex-col items-stretch p-4 fixed top-0 left-0 right-0 z-40 bg-white shadow-md"
+    class="flex flex-col items-stretch p-4 fixed top-0 left-0 right-0 z-40 bg-white shadow-md"
     :style="{ height: headerHeight }"
   >
     <!-- Header Content -->

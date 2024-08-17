99fdbdc6cab78a546443a6bf408ba77d0eecce62
Silas Knight <silasfelinus@gmail.com>
2024-08-16 18:15:38 -0700

    separated nav toggle into new component

---
+++ components/content/tooltips/NavToggle.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    class="p-2 bg-primary rounded-full w-8 h-8 flex items-center justify-center"
+    @click="toggleNav"
+  >
+    <icon name="fluent:row-triple-20-filled" class="text-lg text-white" />
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { defineEmits } from 'vue'
+
+const emit = defineEmits<{
+  (e: 'toggleNav'): void
+}>()
+
+const toggleNav = () => {
+  emit('toggleNav')
+}
+</script>
+
+<style scoped>
+/* Add any specific styles for the toggle button if needed */
+</style>

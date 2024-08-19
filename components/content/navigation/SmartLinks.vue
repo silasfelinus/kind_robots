c2ec7f7150ab07ef88b6939285f988d3946968ad
Silas Knight <silasfelinus@gmail.com>
2024-08-19 13:07:21 -0700

    updated smart links

--- components/content/navigation/SmartLinks.vue
+++ components/content/navigation/SmartLinks.vue
@@ -1,70 +1,87 @@
 <template>
-  <div class="flex items-center justify-center w-full">
-    <!-- Previous Link -->
-    <NuxtLink
-      v-if="prev"
-      :to="prev._path"
-      class="group nav-icon flex flex-col items-center justify-center"
-    >
-      <icon name="typcn:arrow-back-outline" class="nav-icon hover:scale-110" />
-      <div class="nav-text group-hover:opacity-100">
-        {{ prev.title }}
-      </div>
-    </NuxtLink>
-
-    <!-- Random Link -->
-    <NuxtLink
-      :to="randomHighlightPage._path"
-      class="group nav-icon flex flex-col items-center justify-center"
-    >
-      <icon name="game-icons:galaxy" class="nav-icon hover:scale-110" />
-      <div class="nav-text group-hover:opacity-100">
-        {{ randomLinkText }}
-      </div>
-    </NuxtLink>
-
-    <!-- Home Link -->
-    <NuxtLink
-      v-if="!isHomePage"
-      to="/"
-      class="group nav-icon flex flex-col items-center justify-center"
-    >
-      <icon name="line-md:home-md-twotone" class="nav-icon hover:scale-110" />
-      <div class="nav-text group-hover:opacity-100">
-        {{ homeLinkText }}
-      </div>
-    </NuxtLink>
-
-    <!-- Next Link -->
-    <NuxtLink
-      v-if="next"
-      :to="next._path"
-      class="group nav-icon flex flex-col items-center justify-center"
-    >
-      <icon
-        name="typcn:arrow-forward-outline"
-        class="nav-icon hover:scale-110"
-      />
-      <div class="nav-text group-hover:opacity-100">
-        {{ next.title }}
-      </div>
-    </NuxtLink>
+  <div class="flex items-center relative">
+    <!-- Links Container -->
+    <div class="flex items-center justify-center w-full">
+      <!-- Previous Link -->
+      <NuxtLink
+        v-if="prev"
+        :to="prev._path"
+        class="group nav-icon flex flex-col items-center justify-center"
+      >
+        <icon
+          name="typcn:arrow-back-outline"
+          size="48"
+          class="hover:scale-125"
+        />
+        <div class="nav-text group-hover:show-text">
+          {{ prev.title }}
+        </div>
+      </NuxtLink>
+      <!-- Random Link -->
+      <NuxtLink
+        :to="randomHighlightPage._path"
+        class="group nav-icon flex-col items-center justify-center"
+      >
+        <icon
+          name="game-icons:galaxy"
+          class="w-12 h-12 md:w-20 md:h-20 hover:scale-125"
+        />
+        <div class="nav-text group-hover:show-text">
+          {{ randomLinkText }}
+        </div>
+      </NuxtLink>
+      <!-- Home Link -->
+      <NuxtLink
+        v-if="!isHomePage"
+        to="/"
+        class="group nav-icon flex-col items-center justify-center"
+      >
+        <icon
+          name="line-md:home-md-twotone"
+          class="w-12 h-12 md:w-20 md:h-20 hover:scale-125"
+        />
+        <div class="nav-text group-hover:show-text">
+          {{ homeLinkText }}
+        </div>
+      </NuxtLink>
+      <!-- Next Link -->
+      <NuxtLink
+        v-if="next"
+        :to="next._path"
+        class="group nav-icon flex flex-col items-center justify-center"
+      >
+        <icon
+          name="typcn:arrow-forward-outline"
+          class="w-12 h-12 md:w-20 md:h-20 hover:scale-125 m-2"
+        />
+        <div class="nav-text group-hover:show-text">
+          {{ next.title }}
+        </div>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue'
-import { useContentStore } from './../../../stores/contentStore'
+import { computed } from 'vue'
+import { useContentStore } from '../../../stores/contentStore'
 
-// Stores
+const { prev, next, page } = useContent()
 const contentStore = useContentStore()
-const { page } = contentStore
-const { prev, next } = useContent()
 
-// State
-const isHomePage = computed(() => page._path === '/' || page.path === '/')
+const isHomePage = computed(() => {
+  return page.value
+    ? page.value._path === '/' || page.value.path === '/'
+    : false
+})
 
-// Random values (client-side only)
+const randomHighlightPage = computed(
+  () =>
+    contentStore.highlightPages[
+      Math.floor(Math.random() * contentStore.highlightPages.length)
+    ] || {},
+)
+
 const randomLinkTexts = ['Randomizer', 'Teleport!', 'Something else']
 const homeLinkTexts = [
   'HomeScreen',
@@ -73,43 +90,20 @@ const homeLinkTexts = [
   'Visual Contents',
 ]
 
-const randomLinkText = ref('')
-const homeLinkText = ref('')
-
-const randomHighlightPage = computed(() => {
-  const highlightPages = contentStore.highlightPages
-  return highlightPages.length > 0
-    ? highlightPages[Math.floor(Math.random() * highlightPages.length)]
-    : {}
-})
-
-// Set random texts on client-side only
-onMounted(() => {
-  randomLinkText.value =
-    randomLinkTexts[Math.floor(Math.random() * randomLinkTexts.length)]
-  homeLinkText.value =
-    homeLinkTexts[Math.floor(Math.random() * homeLinkTexts.length)]
-})
+const randomLinkText =
+  randomLinkTexts[Math.floor(Math.random() * randomLinkTexts.length)]
+const homeLinkText =
+  homeLinkTexts[Math.floor(Math.random() * homeLinkTexts.length)]
 </script>
 
 <style scoped>
 .nav-icon {
-  @apply flex flex-col items-center justify-center transition-all ease-in-out;
-}
-
-.nav-icon:hover {
-  @apply scale-110;
+  @apply flex text-center transition-all ease-in-out;
+  justify-content: center;
+  align-items: center;
 }
 
 .nav-text {
-  @apply text-base opacity-0 transition-opacity duration-300;
-}
-
-.group:hover .nav-text {
-  @apply opacity-100;
-}
-
-.nav-icon icon {
-  @apply w-16 h-16; /* Increase size of icons */
+  @apply text-lg opacity-0 group-hover:opacity-100 transition-opacity;
 }
 </style>

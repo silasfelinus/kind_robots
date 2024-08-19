ae777b83f43ecbe613c5f925a75315db68587e7e
Silas Knight <silasfelinus@gmail.com>
2024-08-19 16:55:38 -0700

    extreme butterflies

--- app.vue
+++ app.vue
@@ -9,7 +9,7 @@
     <!-- Collapsible Toggle -->
     <div
       v-if="showToggle"
-      class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50"
+      class="fixed top-4 left-1/2 transform -translate-x-1/2 z-100"
       @click.stop="toggleSidebar"
     >
       <button class="bg-secondary text-white p-2 rounded-full shadow-md">

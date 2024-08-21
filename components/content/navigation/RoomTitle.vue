1e52817a04219e3ad7a8376e4d389f52dc2cf142
Silas M Knight <silasfelinus@gmail.com>
2024-08-20 18:39:17 -0700

    Update RoomTitle.vue

--- components/content/navigation/RoomTitle.vue
+++ components/content/navigation/RoomTitle.vue
@@ -2,13 +2,13 @@
   <div class="text-center">
     <h1
       v-if="page && page.title"
-      class="text-2xl inline-block rounded-2xl border p-1 m-1 shadow-lg"
+      class="text-2xl inline-block rounded-2xl border p-1 m-0 shadow-lg"
     >
       The {{ page.title }} Room
     </h1>
     <h1
       v-else
-      class="text-2xl inline-block rounded-2xl border p-1 m-1 shadow-lg"
+      class="text-2xl inline-block rounded-2xl border p-1 m-0 shadow-lg"
     >
       🌈 Fetching details...
     </h1>

d8a3fd8c339ea283c945738efc514c4cdd1e075a
Silas Knight <silasfelinus@gmail.com>
2024-08-20 23:58:12 -0700

    added layouts

---
+++ layouts/single-page.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="h-screen w-screen m-2 border-2 border-accent bg-primary rounded-2xl overflow-hidden"
+  >
+    <slot />
+  </div>
+</template>
---
+++ layouts/vertical-scroll.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-screen overflow-x-hidden overflow-y-auto">
+    <slot />
+  </div>
+</template>

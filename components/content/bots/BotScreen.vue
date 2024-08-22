a6ebc5e2a44f02c3113b4d4cfa584b2e3074cdb2
Silas Knight <silasfelinus@gmail.com>
2024-08-22 16:08:04 -0700

    readded kind robot

--- components/content/bots/BotScreen.vue
+++ components/content/bots/BotScreen.vue
@@ -4,7 +4,7 @@
     <div class="flex flex-col md:flex-row flex-grow">
       <!-- Main Content for Kind Robot: Takes the remaining space -->
       <div class="flex-grow p-2 rounded-2xl m-2 border">
-        <rain-effect />
+        <kind-robot />
       </div>
     </div>
   </div>

fb0eeadffe69978ef5b179537ac1ce8b70f2ae08
Silas Knight <silasfelinus@gmail.com>
2024-08-23 17:23:49 -0700

    changed to spotify playlist

---
+++ components/content/screenfx/kind-playlist.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p class="text-info mb-4">
+      Enjoy our AI-curated playlist, designed to enhance your kind experience
+    </p>
+  </div>
+  <div class="music-toggle-container">
+    <iframe
+      style="border-radius: 12px"
+      src="https://open.spotify.com/embed/playlist/4Y6iPSlNSUcaDG9sTOXyhJ?utm_source=generator"
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allowfullscreen
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    >
+    </iframe>
+  </div>
+</template>
+
+<style scoped>
+.music-toggle-container {
+  max-width: 800px; /* or any other width based on your layout */
+  margin: auto;
+  padding: 20px;
+}
+</style>

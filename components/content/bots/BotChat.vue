f766e9259d26f359274ae33a08f28a0a7a54886c
Silas Knight <silasfelinus@gmail.com>
2024-08-24 15:48:25 -0700

    added conditional pages

---
+++ components/content/bots/BotChat.vue
@@ -0,0 +1,414 @@
+<template>
+  <div class="container rounded-2xl mx-auto p-2 bg-base-200">
+    <!-- Bot Avatar and Details -->
+
+    <!-- Message Interaction Area -->
+    <div class="message-container bg-base-200 p-1 rounded-2xl">
+      <!-- New Message Prompt -->
+      <div class="prompt-area p-2 rounded-2xl">
+        <label for="newMessage" class="block mb-2 font-bold">
+          <div v-if="currentBot" class="user-intro p-2 rounded-2xl m-2">
+            <p class="text-lg">{{ currentBot.userIntro ?? 'User Intro' }}</p>
+          </div>
+        </label>
+        <textarea
+          id="newMessage"
+          v-model="message"
+          rows="5"
+          class="message-input w-full p-2 rounded-md border-2 resize-none"
+          placeholder="Type your message..."
+          @keyup.enter="sendMessage"
+        />
+        <button
+          class="submit-button btn btn-primary mt-2"
+          :disabled="isLoading"
+          @click="sendMessage"
+        >
+          Send Message
+        </button>
+        <milestone-reward v-if="shouldShowMilestoneCheck" :id="4" />
+      </div>
+
+      <!-- Loading Indicator -->
+      <div v-if="isLoading" class="loader flex justify-center mt-2">
+        <ami-butterfly />
+      </div>
+
+      <!-- Conversations -->
+      <div
+        v-for="(conversation, index) in conversations"
+        :key="index"
+        class="response-container m-2 p-4 bg-white rounded-md shadow-md relative flex flex-col items-start"
+      >
+        <div
+          v-for="(msg, msgIndex) in conversation"
+          :key="msgIndex"
+          :class="{ 'flex-row-reverse': msg.role === 'user' }"
+          class="flex items-center message-content"
+        >
+          <ResponseEntry
+            v-if="conversation && msg"
+            :role="msg.role"
+            :content="msg.content ?? ''"
+            :avatar-image="msg.avatarImage ?? '/images/kindtitle.webp'"
+            :bot-name="msg.botName ?? 'Kind Robot'"
+            :subtitle="msg.subtitle ?? 'Your friendly neighborhood AI'"
+          />
+        </div>
+        <!-- Reaction Buttons -->
+        <div class="reaction-buttons mt-2 flex space-x-2">
+          <button
+            class="hover:bg-gray-200"
+            :class="{ 'bg-primary': isReactionActive(index, 'liked') }"
+            @click="toggleReaction(index, 'liked')"
+          >
+            👍
+          </button>
+          <div
+            v-if="showPopup[index]?.liked"
+            class="popup bg-info text-lg rounded-2xl"
+          >
+            Response Liked <icon name="like" class="icon-class" />
+          </div>
+
+          <button
+            class="hover:bg-gray-200"
+            :class="{ 'bg-primary': isReactionActive(index, 'hated') }"
+            @click="toggleReaction(index, 'hated')"
+          >
+            👎
+          </button>
+          <div
+            v-if="showPopup[index]?.hated"
+            class="popup bg-info text-lg rounded-2xl"
+          >
+            Response hated <icon name="hate" class="icon-class" />
+          </div>
+          <button
+            class="hover:bg-gray-200"
+            :class="{ 'bg-primary': isReactionActive(index, 'loved') }"
+            @click="toggleReaction(index, 'loved')"
+          >
+            ❤️
+          </button>
+          <div
+            v-if="showPopup[index]?.loved"
+            class="popup bg-info text-lg rounded-2xl"
+          >
+            Favorited <icon name="❤️" class="icon-class" />
+          </div>
+          <button
+            class="hover:bg-gray-200"
+            :class="{ 'bg-primary': isReactionActive(index, 'flagged') }"
+            @click="toggleReaction(index, 'flagged')"
+          >
+            🚩
+          </button>
+          <div
+            v-if="showPopup[index]?.flagged"
+            class="popup bg-info text-lg rounded-2xl"
+          >
+            Flagged <icon name="🚩" class="icon-class" />
+          </div>
+        </div>
+        <div
+          v-if="
+            activeConversationIndex !== null &&
+            activeConversationIndex === index
+          "
+          class="mt-2 flex items-center"
+        >
+          <textarea
+            v-model="replyMessage"
+            type="text"
+            rows="3"
+            placeholder="Continue conversation..."
+            class="flex-grow p-2 rounded-md border-2 text-lg resize-y"
+            @keyup.enter="continueConversation(index)"
+          />
+          <button
+            class="btn btn-primary ml-2"
+            :disabled="isReplyLoading"
+            @click="continueConversation(index)"
+          >
+            Reply
+          </button>
+          <div
+            v-if="isReplyLoading"
+            class="loader flex justify-center mt-2 ml-2"
+          >
+            <ami-butterfly />
+          </div>
+        </div>
+        <button
+          class="absolute top-2 right-2 text-red-500 hover:text-red-700"
+          @click.stop="deleteConversation(index)"
+        >
+          ×
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted, watchEffect } from 'vue'
+import { useBotStore } from '../../../stores/botStore'
+import { useUserStore } from '../../../stores/userStore'
+import { useChatStore, type ChatExchange } from '../../../stores/chatStore'
+
+const shouldShowMilestoneCheck = ref(false)
+let userKey: string | null = null
+
+onMounted(() => {
+  userKey = localStorage.getItem('user_openai_key')
+})
+
+interface Message {
+  role: 'user' | 'assistant'
+  content: string
+  avatarImage?: string
+  botName?: string
+  subtitle?: string
+}
+
+const conversations = ref<Message[][]>([]) // Correct type here
+const activeConversationIndex = ref<number | null>(null)
+const botStore = useBotStore()
+const userStore = useUserStore()
+const chatStore = useChatStore()
+const currentBot = computed(() => {
+  return (
+    botStore.currentBot ?? {
+      name: 'Unknown Bot',
+      subtitle: 'No subtitle available',
+      description: 'No description available',
+      userIntro: 'Hi Bot',
+      prompt: 'I am a kind robot',
+      avatarImage: '/images/amibotsquare1.webp',
+    }
+  )
+})
+const message = ref('')
+const replyMessage = ref('')
+const isLoading = ref(false)
+const error = ref<string | null>(null)
+const isReplyLoading = ref(false)
+
+const userId = computed(() => userStore.userId || 0)
+const botId = computed(() => botStore.currentBot?.id || 0)
+const botName = computed(() => botStore.currentBot?.name || '')
+const username = computed(() => userStore.username)
+const showPopup = ref<{ [key: number]: { [key: string]: boolean } }>({})
+
+// Function to convert a conversation to ChatExchange
+function convertToChatExchange(
+  conversation: Message[],
+  userId: number,
+  botId: number,
+  botName: string,
+  username: string,
+): ChatExchange {
+  const userPrompt =
+    conversation.find((msg) => msg.role === 'user')?.content ?? ''
+  const botResponse =
+    conversation.find((msg) => msg.role === 'assistant')?.content ?? ''
+
+  return {
+    id: 0,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    botId,
+    botName,
+    userId,
+    username,
+    userPrompt,
+    botResponse,
+    liked: null,
+    hated: null,
+    loved: null,
+    flagged: null,
+    previousEntryId: 0,
+  }
+}
+
+type ReactionType = 'liked' | 'hated' | 'loved' | 'flagged'
+
+const isReactionActive = (index: number, reactionType: ReactionType) => {
+  const currentExchange = chatStore.getExchangeById(index) as ChatExchange
+  return currentExchange?.[reactionType]
+}
+
+watchEffect(() => {
+  if (conversations.value.length > 0) {
+    const lastConversation = conversations.value[conversations.value.length - 1]
+    const lastExchange = convertToChatExchange(
+      lastConversation,
+      userId.value,
+      botId.value,
+      botName.value,
+      username.value,
+    )
+    chatStore.addOrUpdateExchange(lastExchange)
+  }
+})
+
+const sendMessage = async () => {
+  isLoading.value = true
+  try {
+    const fullMessage = currentBot.value?.userIntro
+      ? `${currentBot.value.userIntro} ${message.value}`
+      : message.value
+    shouldShowMilestoneCheck.value = true
+
+    const res = await fetch('/api/botcafe/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: fullMessage }],
+        stream: false,
+        user_openai_key: userKey,
+      }),
+    })
+
+    if (!res.ok) {
+      throw new Error('Failed to send message')
+    }
+
+    const data = await res.json()
+
+    conversations.value.push([
+      {
+        role: 'user',
+        content: message.value,
+        avatarImage: userStore.user?.avatarImage ?? undefined,
+      },
+      {
+        role: 'assistant',
+        content: data.choices[0].message.content,
+        avatarImage: currentBot.value?.avatarImage,
+        botName: currentBot.value?.name,
+        subtitle: currentBot.value?.subtitle,
+      },
+    ])
+    message.value = ''
+  } catch (err) {
+    console.error(err)
+    error.value = 'Failed to send the message. Please try again.'
+  } finally {
+    isLoading.value = false
+  }
+}
+
+const continueConversation = async (index: number) => {
+  isReplyLoading.value = true
+  try {
+    const sanitizedMessages = conversations.value[index].map((msg) => ({
+      ...msg,
+    }))
+
+    const fullMessage = currentBot.value?.userIntro
+      ? `${currentBot.value.userIntro} ${replyMessage.value}`
+      : replyMessage.value
+
+    sanitizedMessages.push({ role: 'user', content: fullMessage })
+
+    const res = await fetch('/api/botcafe/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: sanitizedMessages,
+        stream: false,
+      }),
+    })
+
+    if (!res.ok) {
+      throw new Error('Failed to continue conversation')
+    }
+
+    const data = await res.json()
+
+    conversations.value[index].push({
+      role: 'user',
+      content: replyMessage.value,
+    })
+    conversations.value[index].push({
+      role: 'assistant',
+      content: data.choices[0].message.content,
+    })
+    replyMessage.value = ''
+  } catch (err) {
+    console.error(err)
+  } finally {
+    isReplyLoading.value = false
+  }
+}
+
+watchEffect(() => {
+  if (currentBot.value && currentBot.value.prompt) {
+    message.value = currentBot.value.prompt
+  }
+})
+
+const deleteConversation = (index: number) => {
+  conversations.value.splice(index, 1)
+  activeConversationIndex.value = null
+}
+
+const toggleReaction = (
+  index: number,
+  reactionType: 'liked' | 'hated' | 'loved' | 'flagged',
+) => {
+  const currentExchange = convertToChatExchange(
+    conversations.value[index],
+    userId.value,
+    botId.value,
+    botName.value,
+    username.value,
+  )
+
+  if (currentExchange && currentExchange.id) {
+    const currentReactionState = currentExchange[reactionType] ?? false
+    chatStore.addReaction(currentExchange.id, {
+      [reactionType]: !currentReactionState,
+    })
+  }
+
+  // Show popup
+  if (!showPopup.value[index]) {
+    showPopup.value[index] = {}
+  }
+  showPopup.value[index][reactionType] = true
+
+  // Hide popup after 2 seconds
+  setTimeout(() => {
+    if (showPopup.value[index]) {
+      showPopup.value[index][reactionType] = false
+    }
+  }, 2000)
+}
+</script>
+
+<style>
+.popup {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: #3b82f6; /* Adjusted for Tailwind class bg-info */
+  padding: 4px;
+  border-radius: 1rem; /* Adjusted for Tailwind class rounded-2xl */
+  font-size: 1rem; /* Adjusted for Tailwind class text-lg */
+}
+
+.message-content {
+  white-space: pre-wrap; /* This will preserve newlines and spaces */
+}
+</style>

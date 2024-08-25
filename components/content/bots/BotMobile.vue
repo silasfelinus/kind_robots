08c48b42f5121cb8f421d05979b39b2a360ab333
Silas Knight <silasfelinus@gmail.com>
2024-08-25 16:03:00 -0700

    updating styles for icon

--- components/content/bots/BotMobile.vue
+++ components/content/bots/BotMobile.vue
@@ -11,20 +11,24 @@
     </div>
 
     <!-- Conditionally displayed icons on the left and right, with increased z-index -->
-    <div class="fixed inset-x-0 bottom-0 flex justify-between px-1 z-50">
-      <icon :name="iconLeftClass" @click="flipCard('left')" />
-      <icon :name="iconRightClass" @click="flipCard('right')" />
+    <div class="fixed inset-x-0 bottom-0 flex justify-between z-50 m-2 p-2">
+      <icon :name="iconLeftClass" :size="iconSize" @click="flipCard('left')" />
+      <icon
+        :name="iconRightClass"
+        :size="iconSize"
+        @click="flipCard('right')"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref, computed, onMounted, onUnmounted, nextTick } from 'vue'
 import { useBotStore } from './../../../stores/botStore'
-import BotBubble from './bot-bubble.vue'
-import BotChat from './bot-chat.vue'
+import BotBubble from './BotBubble.vue'
+import BotChat from './BotChat.vue'
 import AddBot from './AddBot.vue'
-import BotMessages from './bot-messages.vue'
+import BotMessages from './BotMessages.vue'
 
 const botStore = useBotStore()
 const currentBot = computed(() => botStore.currentBot)
@@ -32,12 +36,28 @@ const currentChannel = ref('chat') // Default to showing the chat window
 const components = { chat: BotChat, addBot: AddBot, viewMessages: BotMessages }
 const currentComponent = computed(() => components[currentChannel.value])
 
-function flipCard(direction) {
-  const keys = Object.keys(components)
-  const currentIndex = keys.indexOf(currentChannel.value)
-  const nextIndex = direction === 'left' ? currentIndex - 1 : currentIndex + 1
-  currentChannel.value = keys[(nextIndex + keys.length) % keys.length]
+const scrollContainer = ref(null)
+const iconSize = ref(0)
+
+function updateIconSize() {
+  if (scrollContainer.value) {
+    // Calculate the icon size as a fraction of the scroll container's width
+    iconSize.value = scrollContainer.value.offsetWidth / 10
+  }
 }
+
+onMounted(() => {
+  nextTick(() => {
+    updateIconSize()
+    // Set up a ResizeObserver to watch for changes in the container size
+    const resizeObserver = new ResizeObserver(updateIconSize)
+    resizeObserver.observe(scrollContainer.value)
+  })
+})
+
+onUnmounted(() => {
+  // Clean up if necessary, though specifics depend on setup
+})
 
 const iconLeftClass = computed(() =>
   currentChannel.value === 'chat'

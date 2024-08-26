3553dd33b989f4bbeb8816c46ac446b5842fc725
Silas Knight <silasfelinus@gmail.com>
2024-08-25 17:15:44 -0700

    tighter bubbles

--- components/content/bots/BotBubble.vue
+++ components/content/bots/BotBubble.vue
@@ -8,36 +8,39 @@
       <!-- Right Arrow SVG -->
     </div>
 
-    <!-- Bot Scroll Container -->
-    <div
-      ref="scrollContainer"
-      class="flex overflow-x-scroll hide-scrollbar space-x-4 px-2"
-    >
+    <!-- Centering Container -->
+    <div class="mx-auto w-2/3">
+      <!-- Bot Scroll Container -->
       <div
-        v-for="bot in bots"
-        :key="bot.id"
-        :class="[
-          'bot-bubble',
-          bot.id === selectedBotId
-            ? 'flex-row items-start'
-            : 'flex-col items-center',
-          'cursor-pointer p-2 rounded-lg bg-base-200 shadow transition-all duration-300 ease-in-out min-w-max',
-        ]"
-        @click="selectBot(bot.id)"
+        ref="scrollContainer"
+        class="flex overflow-x-scroll hide-scrollbar space-x-4 px-2"
       >
-        <!-- Bot Avatar -->
-        <img
-          :src="bot.avatarImage"
-          alt="Bot's Avatar"
-          class="w-20 h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 rounded-full object-cover transition-all duration-300 ease-in-out"
-        />
-        <!-- Conditional Bot Info -->
-        <div v-if="bot.id === selectedBotId" class="bot-info pl-4 flex-grow">
-          <h3 class="text-lg font-semibold">{{ bot.name }}</h3>
-          <p class="text-sm text-gray-600">{{ bot.tagline }}</p>
+        <div
+          v-for="bot in bots"
+          :key="bot.id"
+          :class="[
+            'bot-bubble',
+            bot.id === selectedBotId
+              ? 'flex-row items-start'
+              : 'flex-col items-center',
+            'cursor-pointer p-2 rounded-lg bg-base-200 shadow transition-all duration-300 ease-in-out min-w-max',
+          ]"
+          @click="selectBot(bot.id)"
+        >
+          <!-- Bot Avatar -->
+          <img
+            :src="bot.avatarImage"
+            alt="Bot's Avatar"
+            class="w-20 h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 rounded-full object-cover transition-all duration-300 ease-in-out"
+          />
+          <!-- Conditional Bot Info -->
+          <div v-if="bot.id === selectedBotId" class="bot-info pl-4 flex-grow">
+            <h3 class="text-lg font-semibold">{{ bot.name }}</h3>
+            <p class="text-sm text-gray-600">{{ bot.tagline }}</p>
+          </div>
+          <!-- Always visible Bot Name -->
+          <h3 v-else class="text-lg font-semibold mt-2">{{ bot.name }}</h3>
         </div>
-        <!-- Always visible Bot Name -->
-        <h3 v-else class="text-lg font-semibold mt-2">{{ bot.name }}</h3>
       </div>
     </div>
   </div>
@@ -62,7 +65,6 @@ function selectBot(botId) {
 }
 
 function calculateScrollWidth() {
-  // Dynamically calculate scroll width based on the container's child elements
   const children = scrollContainer.value?.children
   if (children && children.length > 0) {
     scrollWidth = children[0].offsetWidth + children[0].offsetLeft * 2 // Margin times number of visible elements
@@ -86,7 +88,6 @@ function scrollRight() {
 .hide-scrollbar {
   -ms-overflow-style: none; /* IE and Edge */
   scrollbar-width: none; /* Firefox */
-  scrollbar-width: none; /* For all browsers */
 }
 .hide-scrollbar::-webkit-scrollbar {
   display: none; /* Chrome, Safari, Opera */

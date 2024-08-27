c929bda70d297018a7eb6ab63664dcc4eded5eda
Silas Knight <silasfelinus@gmail.com>
2024-08-27 00:50:31 -0700

    refactoring

---
+++ components/content/navigation/BotMessagesLink.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="icon-link-container">
+    <NuxtLink
+      to="/botmessages"
+      :class="{
+        'icon-link': true,
+        'rounded-2xl': true,
+        'bg-base-200': true,
+        'text-center': true,
+        'hover:scale-110': !isCurrentPage,
+        'hover:glow-animation': !isCurrentPage,
+        'text-gray-400': isCurrentPage,
+      }"
+    >
+      <icon name="material-icons:chat" class="icon-effect" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useContentStore } from './../../../stores/contentStore'
+
+const contentStore = useContentStore()
+const isCurrentPage = computed(
+  () => contentStore.currentPage?._path === '/botmessages',
+)
+</script>
+
+<style scoped>
+.icon-effect {
+  @apply w-6 h-6 md:w-16 md:h-16 cursor-pointer transition-shadow;
+}
+.glow-animation:hover {
+  animation: glow 1.5s infinite;
+}
+</style>
---
+++ content/addbot.md
@@ -0,0 +1,17 @@
+---
+title: 'Add Bot'
+subtitle: "Create a new kind robot"
+description: "Create a new bot and share it with the community"
+gallery: 'botfactory'
+image: 'createbot.webp'
+tags: ['home']
+layout: 'vertical-scroll'
+icon: fa-solid:robot
+category: user
+tooltip: "Create and manage bots that make our chatrooms lively and interactive."
+dottitip: "The more bots, the more our community grows"
+amitip: I love seeing what new bots people come up with!
+sort: highlight
+---
+
+:add-bot

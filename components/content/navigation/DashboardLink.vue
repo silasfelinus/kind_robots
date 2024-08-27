d90cfc896484ea3673a3910126c0eb4c66e924cf
Silas M Knight <silasfelinus@gmail.com>
2024-08-27 14:21:02 -0700

    Update DashboardLink.vue

---
+++ components/content/navigation/DashboardLink.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="icon-link-container">
+    <NuxtLink
+      to="/dashboard"
+      :class="[
+        'flex',
+        'items-center',
+        'justify-center',
+        'rounded-2xl',
+        'bg-base-200',
+        'text-center',
+        'hover:scale-110',
+        'hover:glow-animation',
+        isCurrentPage ? 'text-gray-400' : '',
+      ]"
+    >
+      <icon name="rivet-icons:settings" class="icon-effect" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useContentStore } from './../../../stores/contentStore'
+
+const contentStore = useContentStore()
+const isCurrentPage = computed(
+  () => contentStore.currentPage?._path === '/dashboard',
+)
+</script>
+
+<style scoped>
+.glow-animation:hover {
+  animation: glow 1.5s infinite;
+}
+</style>

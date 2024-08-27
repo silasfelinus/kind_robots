5db3e1c2cc75e150a2114302d12310819a2302f4
Silas Knight <silasfelinus@gmail.com>
2024-08-27 03:36:46 -0700

    link fixes

---
+++ components/content/navigation/HotLink.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="icon-link-container">
+    <NuxtLink
+      to="/hotornot"
+      :class="[
+        'flex',
+        'items-center',
+        'justify-center',
+        'rounded-2xl',
+        'bg-base-200',
+        'text-center',
+        'hover:scale-110',
+        'hover:glow-animation',
+        isCurrentPage ? 'text-gray-400' : '',
+      ]"
+    >
+      <icon
+        name="fxemoji:fire"
+        class="w-6 h-6 md:w-16 md:h-16 cursor-pointer transition-shadow"
+      />
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useContentStore } from './../../../stores/contentStore'
+
+const contentStore = useContentStore()
+const isCurrentPage = computed(
+  () => contentStore.currentPage?._path === '/addbot',
+)
+</script>
+
+<style scoped>
+.glow-animation:hover {
+  animation: glow 1.5s infinite;
+}
+
+@keyframes glow {
+  0%,
+  100% {
+    box-shadow: 0 0 5px rgba(255, 255, 255, 0.75);
+  }
+  50% {
+    box-shadow:
+      0 0 20px rgba(255, 115, 253, 0.75),
+      0 0 30px rgba(255, 115, 253, 0.75);
+  }
+}
+</style>

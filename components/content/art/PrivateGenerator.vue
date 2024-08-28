c9c59d298ce3a226d5f996c79f3c676becc5e992
Silas Knight <silasfelinus@gmail.com>
2024-08-27 21:17:11 -0700

    adjusted mature generator

--- components/content/art/PrivateGenerator.vue
+++ components/content/art/PrivateGenerator.vue
@@ -1,34 +1,28 @@
 <template>
-  <div class="container">
+  <div
+    class="flex flex-col items-stretch w-full h-full overflow-hidden border-2 rounded-2xl p-2 bg-base-200"
+  >
     <!-- Check if showMature is true and display the website, otherwise display the rebel-button -->
-    <iframe v-if="showMature" :src="siteUrl" frameborder="0" class="full-size"></iframe>
-    <rebel-button v-else class="full-size" />
+    <iframe
+      v-if="showMature"
+      :src="siteUrl"
+      frameborder="0"
+      class="w-full h-full"
+    ></iframe>
+    <rebel-button v-else class="w-full h-full" />
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { useUserStore } from '@/stores/userStore';
+import { computed } from 'vue'
+import { useUserStore } from '@/stores/userStore'
 
 // Get the store instance
-const userStore = useUserStore();
+const userStore = useUserStore()
 
 // Computed property to reactively check the showMature flag
-const showMature = computed(() => userStore.showMature);
+const showMature = computed(() => userStore.showMature)
 
 // The URL of the website to display
-const siteUrl = 'https://lola.acrocatranch.com';
+const siteUrl = 'https://lola.acrocatranch.com'
 </script>
-
-<style scoped>
-.container {
-  width: 100%; /* Takes the full width of the parent */
-  height: 100%; /* Takes the full height of the parent */
-  overflow: hidden; /* Prevents scrollbars if the content is too large */
-}
-
-.full-size {
-  width: 100%; /* Full width of the container */
-  height: 100%; /* Full height of the container */
-}
-</style>

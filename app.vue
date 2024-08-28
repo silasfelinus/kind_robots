1ca5410e10bcc01b60ba13241308802a5fdc0b2a
Silas Knight <silasfelinus@gmail.com>
2024-08-28 00:35:10 -0700

    kind sidebar

--- app.vue
+++ app.vue
@@ -1,128 +1,10 @@
 <template>
   <div class="main-container bg-primary">
     <!-- Header with embedded toggle button -->
-    <header-upgrade class="rounded-2xl border p-1 m-1">
-      <button class="absolute bottom-0 right-3 z-50" @click="toggleSidebar">
-        <icon
-          :name="isSidebarOpen ? 'lucide:sidebar' : 'lucide:sidebar-open'"
-          class="h-6 w-6 md:h-12 md:w-12"
-        ></icon>
-      </button>
-    </header-upgrade>
+    <header-upgrade class="rounded-2xl border p-2 m-2"> </header-upgrade>
 
     <div class="flex-grow">
-      <!-- Collapsible Sidebar -->
-      <aside
-        :class="`sidebar flex-shrink-0 transition-width duration-300 ease-in-out overflow-y-auto m-1 p-1 border rounded-2xl bg-secondary ${isSidebarOpen ? 'w-64' : 'w-24'}`"
-        :aria-hidden="isSidebarOpen ? 'false' : 'true'"
-      >
-        <!-- Sidebar Links with Icons and Titles -->
-        <div
-          class="p-4 flex items-center justify-start"
-          title="Home"
-          @click="toggleSidebar"
-        >
-          <home-link class="text-xl w-6 h-6 md:w-16 md:h-16" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/home"
-            class="ml-2 text-lg font-semibold"
-            >Home</router-link
-          >
-        </div>
-        <div
-          class="p-4 flex items-center justify-start"
-          title="Add Bot"
-          @click="toggleSidebar"
-        >
-          <add-bot-link class="text-xl" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/addbot"
-            class="ml-2 text-lg font-semibold"
-            >Add Bot</router-link
-          >
-        </div>
-        <div
-          class="p-4 flex items-center justify-start"
-          title="Chat with Bots"
-          @click="toggleSidebar"
-        >
-          <bot-chat-link class="text-xl" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/botcafe"
-            class="ml-2 text-lg font-semibold"
-            >Chat with Bots</router-link
-          >
-        </div>
-        <div
-          class="p-4 flex items-center justify-start"
-          title="Bot Messages"
-          @click="toggleSidebar"
-        >
-          <bot-messages-link class="text-xl" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/botmessages"
-            class="ml-2 text-lg font-semibold"
-            >Bot Messages</router-link
-          >
-        </div>
-        <div
-          class="p-4 flex items-center justify-start"
-          title="Hot or Not?"
-          @click="toggleSidebar"
-        >
-          <hot-link class="text-xl" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/hotornot"
-            class="ml-2 text-lg font-semibold"
-            >Hot or Not?</router-link
-          >
-        </div>
-        <div
-          class="p-4 flex items-center justify-start"
-          title="Art Gallery"
-          @click="toggleSidebar"
-        >
-          <art-gallery-link class="text-xl" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/artgallery"
-            class="ml-2 text-lg font-semibold"
-            >Art Gallery</router-link
-          >
-        </div>
-        <div
-          class="p-4 flex items-center justify-start"
-          title="Dashboard"
-          @click="toggleSidebar"
-        >
-          <dashboard-link class="text-xl" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/dashboard"
-            class="ml-2 text-lg font-semibold"
-            >Dashboard</router-link
-          >
-        </div>
-        <div
-          v-if="showMature"
-          class="p-4 flex items-center justify-start"
-          title="Mature Content"
-          @click="toggleSidebar"
-        >
-          <icon name="fxemoji:lips" class="text-xl w-6 h-6 md:w-16 md:h-16" />
-          <router-link
-            v-show="isSidebarOpen"
-            to="/mature"
-            class="ml-2 text-lg font-semibold"
-            >Mature Content</router-link
-          >
-        </div>
-      </aside>
+      <kind-sidebar />
 
       <!-- Main Content -->
       <main class="main-content rounded-2xl p-1 m-1">
@@ -133,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
+import { onMounted } from 'vue'
 import { useHead } from '@vueuse/head'
 import { useErrorStore } from '@/stores/errorStore'
 import { useTagStore } from '@/stores/tagStore'
@@ -145,12 +27,7 @@ import { usePitchStore } from '@/stores/pitchStore'
 import { useChannelStore } from '@/stores/channelStore'
 import { useMilestoneStore } from '@/stores/milestoneStore'
 import { useLayoutStore } from '@/stores/layoutStore'
-import AddBotLink from '@/components/content/navigation/AddBotLink.vue'
-import BotChatLink from '@/components/content/navigation/BotChatLink.vue'
-import BotMessagesLink from '@/components/content/navigation/BotMessagesLink.vue'
-import ArtGalleryLink from '@/components/content/navigation/ArtGalleryLink.vue'
-import HotLink from '@/components/content/navigation/HotLink.vue'
-import DashboardLink from '@/components/content/navigation/DashboardLink.vue'
+import KindSidebar from '@/components/content/navigation/KindSidebar.vue'
 
 const errorStore = useErrorStore()
 const tagStore = useTagStore()
@@ -162,13 +39,6 @@ const pitchStore = usePitchStore()
 const channelStore = useChannelStore()
 const milestoneStore = useMilestoneStore()
 const layoutStore = useLayoutStore()
-
-const isSidebarOpen = ref(true)
-const showMature = computed(() => userStore.showMature)
-
-function toggleSidebar() {
-  isSidebarOpen.value = !isSidebarOpen.value
-}
 
 useHead({
   title: 'Kind Robots',
@@ -227,32 +97,11 @@ onMounted(async () => {
   flex-shrink: 0; /* Ensures the header does not shrink */
 }
 
-.sidebar {
-  flex-shrink: 0;
-  transition: width 0.3s ease-in-out;
-  overflow-y: auto; /* Allows scrolling within the sidebar */
-}
-
 /* Flex-grow on main ensures it takes up remaining space */
 .flex-grow {
   display: flex;
   flex-direction: row;
   height: calc(100% - 50px); /* Adjust based on your header's height */
   overflow: hidden; /* Containing internal overflows */
-}
-
-@media (max-width: 768px) {
-  .sidebar {
-    min-width: 150px;
-    max-width: 200px;
-  }
-}
-
-/* Further responsiveness for smaller devices */
-@media (max-width: 480px) {
-  .sidebar {
-    min-width: 120px;
-    width: 100%; /* Sidebar takes full width on very small devices */
-  }
 }
 </style>
---
+++ components/content/navigation/KindSidebar.vue
@@ -0,0 +1,161 @@
+<template>
+  <button class="absolute top-0 right-3 z-50" @click="toggleSidebar">
+    <icon
+      :name="isSidebarOpen ? 'lucide:sidebar' : 'lucide:sidebar-open'"
+      class="h-6 w-6 md:h-12 md:w-12"
+    ></icon>
+  </button>
+  <!-- Collapsible Sidebar -->
+  <aside
+    :class="`sidebar flex-shrink-0 transition-width duration-300 ease-in-out overflow-y-auto m-1 p-1 border rounded-2xl bg-secondary ${isSidebarOpen ? 'w-64' : 'w-24'}`"
+    :aria-hidden="isSidebarOpen ? 'false' : 'true'"
+  >
+    <!-- Sidebar Links with Icons and Titles -->
+    <div
+      class="p-4 flex items-center justify-start"
+      title="Home"
+      @click="toggleSidebar"
+    >
+      <home-link class="flex-grow" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/home"
+        class="ml-2 text-lg font-semibold"
+        >Home</router-link
+      >
+    </div>
+    <div
+      class="p-4 flex items-center justify-start"
+      title="Add Bot"
+      @click="toggleSidebar"
+    >
+      <add-bot-link class="text-xl" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/addbot"
+        class="ml-2 text-lg font-semibold"
+        >Add Bot</router-link
+      >
+    </div>
+    <div
+      class="p-4 flex items-center justify-start"
+      title="Chat with Bots"
+      @click="toggleSidebar"
+    >
+      <bot-chat-link class="text-xl" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/botcafe"
+        class="ml-2 text-lg font-semibold"
+        >Chat with Bots</router-link
+      >
+    </div>
+    <div
+      class="p-4 flex items-center justify-start"
+      title="Bot Messages"
+      @click="toggleSidebar"
+    >
+      <bot-messages-link class="text-xl" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/botmessages"
+        class="ml-2 text-lg font-semibold"
+        >Bot Messages</router-link
+      >
+    </div>
+    <div
+      class="p-4 flex items-center justify-start"
+      title="Hot or Not?"
+      @click="toggleSidebar"
+    >
+      <hot-link class="text-xl" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/hotornot"
+        class="ml-2 text-lg font-semibold"
+        >Hot or Not?</router-link
+      >
+    </div>
+    <div
+      class="p-4 flex items-center justify-start"
+      title="Art Gallery"
+      @click="toggleSidebar"
+    >
+      <art-gallery-link class="text-xl" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/artgallery"
+        class="ml-2 text-lg font-semibold"
+        >Art Gallery</router-link
+      >
+    </div>
+    <div
+      class="p-4 flex items-center justify-start"
+      title="Dashboard"
+      @click="toggleSidebar"
+    >
+      <dashboard-link class="text-xl" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/dashboard"
+        class="ml-2 text-lg font-semibold"
+        >Dashboard</router-link
+      >
+    </div>
+    <div
+      v-if="showMature"
+      class="p-4 flex items-center justify-start"
+      title="Mature Content"
+      @click="toggleSidebar"
+    >
+      <icon name="fxemoji:lips" class="text-xl w-6 h-6 md:w-16 md:h-16" />
+      <router-link
+        v-show="isSidebarOpen"
+        to="/mature"
+        class="ml-2 text-lg font-semibold"
+        >Mature Content</router-link
+      >
+    </div>
+  </aside>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+import AddBotLink from './../navigation/AddBotLink.vue'
+import BotChatLink from './../navigation/BotChatLink.vue'
+import BotMessagesLink from './../navigation/BotMessagesLink.vue'
+import ArtGalleryLink from './../navigation/ArtGalleryLink.vue'
+import HotLink from './../navigation/HotLink.vue'
+import DashboardLink from './../navigation/DashboardLink.vue'
+
+const userStore = useUserStore()
+const showMature = computed(() => userStore.showMature)
+
+const isSidebarOpen = ref(true)
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value
+}
+</script>
+<style>
+.sidebar {
+  flex-shrink: 0;
+  transition: width 0.3s ease-in-out;
+  overflow-y: auto; /* Allows scrolling within the sidebar */
+}
+
+@media (max-width: 768px) {
+  .sidebar {
+    min-width: 150px;
+    max-width: 200px;
+  }
+}
+
+/* Further responsiveness for smaller devices */
+@media (max-width: 480px) {
+  .sidebar {
+    min-width: 120px;
+    width: 100%; /* Sidebar takes full width on very small devices */
+  }
+}
+</style>

3ec75d12562dc11f33323b97050fec55922a9994
Silas M Knight <silasfelinus@gmail.com>
2024-08-28 19:50:04 -0700

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,45 +1,11 @@
 <template>
-  <NuxtLayout :class="layoutName">
-    <ContentDoc />
-  </NuxtLayout>
+  <main>
+    <NuxtLayout :name="page?.layout">
+      <ContentDoc />
+    </NuxtLayout>
+  </main>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, computed } from 'vue'
-import { LayoutKey, useLayoutStore } from './../stores/layoutStore'
-
-const breakpoints = { mobile: 640, tablet: 1024 }
-
-// Initialize deviceType with 'Desktop' corrected to match a value from LayoutKey
-const deviceType = ref<LayoutKey | false>(LayoutKey.Desktop) // assuming LayoutKey is an enum
-
-const layoutStore = useLayoutStore()
-
-// Compute the layout name based directly on the device type or default to 'Desktop'
-const layoutName = computed(() => deviceType.value || LayoutKey.Desktop)
-
-// Function to check and update the device type based on window width
-function checkSize() {
-  const width = window.innerWidth
-  if (width < breakpoints.mobile) {
-    deviceType.value = LayoutKey.Mobile // Correct usage of enum
-  } else if (width >= breakpoints.mobile && width < breakpoints.tablet) {
-    deviceType.value = LayoutKey.Tablet // Correct usage of enum
-  } else {
-    deviceType.value = LayoutKey.Desktop // Correct usage of enum
-  }
-}
-
-onMounted(() => {
-  window.addEventListener('resize', checkSize)
-  checkSize() // Initial check to set the correct layout on load
-
-  // Check if the layout key is defined and then update it in the store
-  if (deviceType.value !== false) {
-    layoutStore.setLayout(deviceType.value)
-  }
-})
-onUnmounted(() => {
-  window.removeEventListener('resize', checkSize)
-})
+const { page } = useContent()
 </script>

3c04539ffc8dcd3bbfc1bb3e91eac993a7ae42a0
Silas Knight <silasfelinus@gmail.com>
2024-08-28 18:45:18 -0700

    testing layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="layout-dashboard">
-    <slot />
+  <div>
+    <header-upgrade />
+    <kind-sidebar />
+    <div class="absolute bottom-0 center z-50 text-gray-200">Desktop</div>
+    <main class="flex-grow rounded-2xl p-1 m-1">
+      <slot />
+    </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import KindSidebar from './../components/content/navigation/KindSidebar.vue'
+</script>
+
+<style>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  overflow: hidden;
+}
+
+.kind-sidebar {
+  flex-shrink: 0;
+  overflow-y: scroll;
+}
+
+.main-content {
+  flex-grow: 1;
+  overflow-y: scroll;
+}
+</style>

f7699f65891f63131fba8de3e1f4e71b4adda2f5
Silas Knight <silasfelinus@gmail.com>
2024-08-29 04:28:29 -0700

    trying to restore full screen

--- components/content/bots/BotChat.vue
+++ components/content/bots/BotChat.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="flex flex-col bg-base-200 h-screen rounded-2xl m-1 p-1">
+  <div class="flex flex-col bg-base-200 h-full w-full rounded-2xl m-1 p-1">
     <!-- Unified Top Half with image and info/stream side by side -->
-    <div class="flex flex-row h-2/3 w-full">
+    <div class="flex flex-row w-full">
       <!-- Allocate more space to the top half -->
       <!-- Bot's Image with oval aspect -->
-      <div class="w-2/3 flex justify-center items-center">
+      <div class="flex justify-center items-center">
         <img
           :src="currentBot?.avatarImage"
           alt="Bot's Avatar"
-          class="w-2/3 h-2/3 rounded-sm object-cover"
+          class="rounded-sm object-cover"
         />
       </div>
       <!-- Info and Stream in the same column -->
-      <div
-        class="w-2/3 h-2/3 overflow-y-scroll flex flex-col justify-between p-2"
-      >
+      <div class="overflow-y-auto flex flex-col justify-between p-2">
         <div class="text-left border rounded-2xl m-2 p-1 bg-base-200">
           <h2 class="text-xl font-bold">{{ currentBot?.name }}</h2>
           <p class="text-md">{{ currentBot?.description }}</p>

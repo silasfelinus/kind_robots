5aff3f5d2c9673a9badeb4cdece1aeff09686623
Silas Knight <silasfelinus@gmail.com>
2024-08-29 04:19:26 -0700

    refined bubbles

--- components/content/bots/BotBubble.vue
+++ components/content/bots/BotBubble.vue
@@ -1,25 +1,21 @@
 <template>
   <div
-    class="relative select-none overflow-x-scroll overflow-y-auto p-1 border rounded-2xl bg-base-200"
+    class="relative select-none overflow-hidden p-1 border rounded-2xl bg-base-200"
   >
-    <!-- Centering Container -->
-    <div ref="scrollContainer" class="mx-auto w-full overflow-x-scroll">
-      <!-- Bot Scroll Container -->
-      <div class="flex space-x-4 px-2">
-        <div
-          v-for="(bot, index) in bots"
-          :key="`bot-${index}`"
-          class="bot-bubble"
-          @click="selectBot(bot.id)"
-        >
-          <img :src="bot.avatarImage" alt="Bot's Avatar" class="bot-img" />
-          <h3 class="bot-name">{{ bot.name }}</h3>
-        </div>
+    <!-- Bot Scroll Container -->
+    <div ref="scrollContainer" class="scroll-container px-2">
+      <div
+        v-for="(bot, index) in bots"
+        :key="`bot-${index}`"
+        class="bot-bubble"
+        @click="selectBot(bot.id)"
+      >
+        <img :src="bot.avatarImage" alt="Bot's Avatar" class="bot-img" />
+        <h3 class="bot-name">{{ bot.name }}</h3>
       </div>
     </div>
   </div>
 </template>
-
 <script setup>
 import { ref, computed, onMounted } from 'vue'
 import { useBotStore } from './../../../stores/botStore'
@@ -28,21 +24,21 @@ const botStore = useBotStore()
 const bots = computed(() => botStore.bots)
 const scrollContainer = ref(null)
 
-// Function to handle bot selection
 function selectBot(botId) {
   botStore.selectBot(botId)
 }
 
-// Automatically select the first bot when the component mounts
 onMounted(() => {
   if (bots.value.length > 0) {
-    selectBot(bots.value[0].id) // Select the first bot
+    selectBot(bots.value[0].id)
   }
 })
 </script>
 <style>
 .scroll-container {
   display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
   scroll-snap-type: x mandatory;
   cursor: grab; /* Cursor indicates draggable area */
 }
@@ -52,22 +48,21 @@ onMounted(() => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 20vw;
+  width: 20vw; /* Adjust as necessary to ensure circular appearance */
   min-width: 180px;
-  max-width: 160px;
+  max-width: 200px; /* Ensure this is square */
+  height: 200px; /* New height to match max-width for circular image */
   margin: 0 8px;
   padding: 10px;
   scroll-snap-align: start;
   text-align: center;
-  height: auto;
-  overflow: visible;
 }
 
 .bot-img {
-  width: 100%;
-  height: auto;
-  max-height: 80px;
+  width: 80%; /* Adjust based on design preference */
+  height: 80%; /* Make the image take up a fixed percentage of the bubble */
   border-radius: 50%;
+  object-fit: cover; /* Ensure images don't get distorted */
 }
 
 .bot-name {

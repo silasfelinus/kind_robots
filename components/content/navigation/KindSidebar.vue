0e04a7a15230be7b2f0d3b6baf2f985d2acf569d
Silas M Knight <silasfelinus@gmail.com>
2024-08-29 00:14:16 -0700

    Update KindSidebar.vue

--- components/content/navigation/KindSidebar.vue
+++ components/content/navigation/KindSidebar.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="relative">
     <button class="absolute top-3 left-3 z-50" @click="toggleSidebar">
-      <icon
-        :name="isSidebarOpen ? 'lucide:sidebar' : 'lucide:sidebar-open'"
-        class="h-6 w-6 text-gray-500"
-      ></icon>
+      <icon :name="isSidebarOpen ? 'lucide:sidebar' : 'lucide:sidebar-open'" class="icon-base text-gray-500"></icon>
     </button>
     <!-- Collapsible Sidebar -->
     <aside
@@ -12,158 +9,75 @@
       :aria-hidden="isSidebarOpen ? 'false' : 'true'"
     >
       <!-- Sidebar Links with Icons and Titles -->
-      <div
-        class="p-4 flex items-center justify-start"
-        title="Home"
-        @click="toggleSidebar"
-      >
-        <home-link class="text-xl" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/home"
-          class="ml-2 text-lg font-semibold"
-          >Home</router-link
+      <div v-for="link in filteredLinks" :key="link.title" @click="toggleSidebar" class="icon-link-container">
+        <NuxtLink
+          :to="link.path"
+          :class="[
+            'flex',
+            'items-center',
+            'justify-center',
+            'rounded-2xl',
+            'text-center',
+            'hover:scale-110',
+            'hover:glow-animation',
+            isCurrentPage(link.path) ? 'text-gray-400' : '',
+          ]"
         >
-      </div>
-      <div
-        class="p-4 flex items-center justify-start"
-        title="Add Bot"
-        @click="toggleSidebar"
-      >
-        <add-bot-link class="text-xl" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/addbot"
-          class="ml-2 text-lg font-semibold"
-          >Add Bot</router-link
-        >
-      </div>
-      <div
-        class="p-4 flex items-center justify-start"
-        title="Chat with Bots"
-        @click="toggleSidebar"
-      >
-        <bot-chat-link class="text-xl" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/botcafe"
-          class="ml-2 text-lg font-semibold"
-          >Chat with Bots</router-link
-        >
-      </div>
-      <div
-        class="p-4 flex items-center justify-start"
-        title="Bot Messages"
-        @click="toggleSidebar"
-      >
-        <bot-messages-link class="text-xl" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/botmessages"
-          class="ml-2 text-lg font-semibold"
-          >Bot Messages</router-link
-        >
-      </div>
-      <div
-        class="p-4 flex items-center justify-start"
-        title="Hot or Not?"
-        @click="toggleSidebar"
-      >
-        <hot-link class="text-xl" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/hotornot"
-          class="ml-2 text-lg font-semibold"
-          >Hot or Not?</router-link
-        >
-      </div>
-      <div
-        class="p-4 flex items-center justify-start"
-        title="Art Gallery"
-        @click="toggleSidebar"
-      >
-        <art-gallery-link class="text-xl" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/artgallery"
-          class="ml-2 text-lg font-semibold"
-          >Art Gallery</router-link
-        >
-      </div>
-      <div
-        class="p-4 flex items-center justify-start"
-        title="Dashboard"
-        @click="toggleSidebar"
-      >
-        <dashboard-link class="text-xl" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/dashboard"
-          class="ml-2 text-lg font-semibold"
-          >Dashboard</router-link
-        >
-      </div>
-      <div
-        v-if="showMature"
-        class="p-4 flex items-center justify-start"
-        title="Mature Content"
-        @click="toggleSidebar"
-      >
-        <icon name="fxemoji:lips" class="text-xl w-6 h-6 md:w-16 md:h-16" />
-        <router-link
-          v-show="isSidebarOpen"
-          to="/mature"
-          class="ml-2 text-lg font-semibold"
-          >Mature Content</router-link
-        >
+          <icon :name="link.icon" class="icon-base cursor-pointer transition-shadow"></icon>
+        </NuxtLink>
       </div>
       <smart-links class="text-center flex-grow justify text-xl" />
     </aside>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { computed } from 'vue'
-import AddBotLink from './../navigation/AddBotLink.vue'
-import BotChatLink from './../navigation/BotChatLink.vue'
-import BotMessagesLink from './../navigation/BotMessagesLink.vue'
-import ArtGalleryLink from './../navigation/ArtGalleryLink.vue'
-import HotLink from './../navigation/HotLink.vue'
-import DashboardLink from './../navigation/DashboardLink.vue'
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useUserStore } from '@/stores/userStore' // Update the path as necessary
+import { useContentStore } from './../../../stores/contentStore'
 
-const userStore = useUserStore()
-const showMature = computed(() => userStore.showMatureContent)
+const userStore = useUserStore();
+const contentStore = useContentStore();
+const showMature = computed(() => userStore.showMatureContent);
+const isSidebarOpen = ref(true);
 
-const isSidebarOpen = ref(true)
+const links = [
+  { title: 'Home', path: '/home', icon: 'heroicons-outline:home' },
+  { title: 'Add Bot', path: '/addbot', icon: 'fluent:bot-add-20-regular' },
+  { title: 'Chat with Bots', path: '/botcafe', icon: 'mdi:chat-processing' },
+  { title: 'Bot Messages', path: '/botmessages', icon: 'fluent:chat-multiple-24-regular' },
+  { title: 'Hot or Not?', path: '/hotornot', icon: 'emojione-monotone:fire' },
+  { title: 'Art Gallery', path: '/artgallery', icon: 'mdi:palette' },
+  { title: 'Dashboard', path: '/dashboard', icon: 'ant-design:dashboard-outline' },
+  { title: 'Mature Content', path: '/mature', icon: 'fxemoji:lips', condition: 'showMature' }
+];
 
 function toggleSidebar() {
-  isSidebarOpen.value = !isSidebarOpen.value
+  isSidebarOpen.value = !isSidebarOpen.value;
 }
+
+const filteredLinks = computed(() => {
+  return links.filter(link => !link.condition || (link.condition === 'showMature' && showMature.value));
+});
+
+const isCurrentPage = (path) => {
+  return contentStore.currentPage?._path === path;
+};
 </script>
-<style>
-.sidebar {
-  position: relative;
-  flex-shrink: 0;
-  transition: width 0.3s ease-in-out;
-  overflow-y: auto; /* Allows scrolling within the sidebar */
-  scrollbar-width: none; /* Hide scrollbar for Firefox */
+
+<style scoped>
+.icon-link-container .hover:glow-animation:hover {
+  animation: glow 1.5s infinite;
 }
 
-.sidebar::-webkit-scrollbar {
-  display: none; /* Hide scrollbar for WebKit browsers */
-}
-
-@media (max-width: 768px) {
-  .sidebar {
-    min-width: 150px;
-    max-width: 200px;
+@keyframes glow {
+  0%, 100% {
+    box-shadow: 0 0 5px rgba(255, 255, 255, 0.75);
   }
-}
-
-@media (max-width: 480px) {
-  .sidebar {
-    min-width: 120px;
-    width: 100%; /* Sidebar takes full width on very small devices */
+  50% {
+    box-shadow:
+      0 0 20px rgba(255, 115, 253, 0.75),
+      0 0 30px rgba(255, 115, 253, 0.75);
   }
 }
 </style>

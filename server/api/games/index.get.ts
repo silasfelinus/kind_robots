03e3e1bb560911df963e0cba89dbb4829bab80af
Silas Knight <silasfelinus@gmail.com>
2024-08-30 05:00:37 -0700

    api routes evolved

--- server/api/games/index.get.ts
+++ server/api/games/index.get.ts
@@ -1,4 +1,4 @@
-//server/api/games/index.get.ts
+// server/api/games/index.get.ts
 import { defineEventHandler } from 'h3'
 import prisma from '../utils/prisma'
 import { errorHandler } from '../utils/error'
@@ -7,14 +7,13 @@ export default defineEventHandler(async () => {
   try {
     const games = await prisma.game.findMany({
       include: {
-        Users: true, // Include related Users
-        Art: true, // Include related Art
-        ArtPrompt: true, // Include related Art Prompts
+        Players: true, // Include related players in each game
       },
     })
 
     return { success: true, games }
   } catch (error: unknown) {
+    console.error('Get Games Error:', error)
     return errorHandler(error)
   }
 })

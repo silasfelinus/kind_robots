dd4d4dd2f018071974ab87b8b3d54f3170b3b4bd
Silas Knight <silasfelinus@gmail.com>
2024-08-29 18:02:46 -0700

    better addbots

--- components/content/bots/AddBot.vue
+++ components/content/bots/AddBot.vue
@@ -1,195 +1,173 @@
-<!-- eslint-disable vue/html-self-closing -->
 <template>
   <div class="rounded-2xl border p-2 m-2 flex flex-col items-center">
     <h1 class="text-3xl mb-4 text-center">Create a New Bot</h1>
     <form
-      class="bg-white shadow-lg rounded-lg p-8 w-full max-w-md"
+      class="bg-white shadow-lg rounded-lg p-8 w-full max-w-4xl mx-auto"
       @submit.prevent="handleSubmit"
     >
-      <div class="mb-4">
-        <label for="name" class="block text-sm font-medium">Name:</label>
-        <input
-          id="name"
-          v-model="name"
-          type="text"
-          class="w-full p-2 rounded border"
-          required
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="subtitle" class="block text-sm font-medium"
-          >Subtitle:</label
-        >
-        <input
-          id="subtitle"
-          v-model="subtitle"
-          type="text"
-          class="w-full p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="description" class="block text-sm font-medium"
-          >Description:</label
-        >
-        <textarea
-          id="description"
-          v-model="description"
-          class="resize w-full p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="avatarImage" class="block text-sm font-medium"
-          >Avatar Image URL:</label
-        >
-        <div class="flex flex-wrap gap-4">
-          <div
-            v-for="(art, index) in artResults"
-            :key="art.id"
-            class="flex flex-col items-center"
+      <div class="flex flex-wrap -mx-2">
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="name" class="block text-sm font-medium">Name:</label>
+          <input
+            id="name"
+            v-model="name"
+            type="text"
+            class="w-full p-2 rounded border"
+            required
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="subtitle" class="block text-sm font-medium"
+            >Subtitle:</label
           >
-            <img
-              :src="art.path"
-              alt="Generated Avatar"
-              class="w-32 h-32 object-cover mb-2"
-            />
-            <button type="button" @click="generateOrFetchAvatar(index)">
-              Generate Another
-            </button>
-            <button type="button" @click="selectAvatar(art.path)">
-              Select
-            </button>
+          <input
+            id="subtitle"
+            v-model="subtitle"
+            type="text"
+            class="w-full p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="description" class="block text-sm font-medium"
+            >Description:</label
+          >
+          <textarea
+            id="description"
+            v-model="description"
+            class="resize w-full p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="avatarImage" class="block text-sm font-medium"
+            >Avatar Image URL:</label
+          >
+          <div class="flex flex-wrap gap-4">
+            <div
+              v-for="(art, index) in artResults"
+              :key="art.id"
+              class="flex flex-col items-center"
+            >
+              <img
+                :src="art.path"
+                alt="Generated Avatar"
+                class="w-32 h-32 object-cover mb-2"
+              />
+              <button type="button" @click="generateOrFetchAvatar(index)">
+                Generate Another
+              </button>
+              <button type="button" @click="selectAvatar(art.path)">
+                Select
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div class="mb-4">
-        <label for="imagePrompt" class="block text-sm font-medium"
-          >Image Prompt:</label
-        >
-        <input
-          id="imagePrompt"
-          v-model="imagePrompt"
-          type="text"
-          class="w-full p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="botType" class="block text-sm font-medium">Bot Type:</label>
-        <select
-          id="botType"
-          v-model="botType"
-          class="w-full p-2 rounded border"
-        >
-          <option value="PROMPTBOT">
-            Prompt Bot: A bot that provides prompts
-          </option>
-          <option value="CHATBOT">
-            Chat Bot: A bot that can chat with users
-          </option>
-          <option value="ARTBOT">Art Bot: A bot that creates art</option>
-        </select>
-      </div>
-
-      <div class="mb-4">
-        <label for="botIntro" class="block text-sm font-medium"
-          >Bot Introduction:</label
-        >
-        <input
-          id="botIntro"
-          v-model="botIntro"
-          type="text"
-          class="w-full p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="userIntro" class="block text-sm font-medium"
-          >User Introduction:</label
-        >
-        <input
-          id="userIntro"
-          v-model="userIntro"
-          type="text"
-          class="w-full p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="prompt" class="block text-sm font-medium">Prompt:</label>
-        <textarea
-          id="prompt"
-          v-model="prompt"
-          class="resize w-full p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="sampleResponse" class="block text-sm font-medium"
-          >Sample Response:</label
-        >
-        <textarea
-          id="sampleResponse"
-          v-model="sampleResponse"
-          class="resize w-full p-2 rounded border"
-        />
-        <button type="button" class="btn btn-primary mt-2" @click="testPrompt">
-          Test Prompt
-        </button>
-        <div v-if="promptTestResult" class="mt-2 text-green-500 animate-pulse">
-          Test Result: {{ promptTestResult }}
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="imagePrompt" class="block text-sm font-medium"
+            >Image Prompt:</label
+          >
+          <input
+            id="imagePrompt"
+            v-model="imagePrompt"
+            type="text"
+            class="w-full p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="botIntro" class="block text-sm font-medium"
+            >Bot Introduction:</label
+          >
+          <input
+            id="botIntro"
+            v-model="botIntro"
+            type="text"
+            class="w-full p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="userIntro" class="block text-sm font-medium"
+            >User Introduction:</label
+          >
+          <input
+            id="userIntro"
+            v-model="userIntro"
+            type="text"
+            class="w-full p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="prompt" class="block text-sm font-medium">Prompt:</label>
+          <textarea
+            id="prompt"
+            v-model="prompt"
+            class="resize w-full p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="sampleResponse" class="block text-sm font-medium"
+            >Sample Response:</label
+          >
+          <textarea
+            id="sampleResponse"
+            v-model="sampleResponse"
+            class="resize w-full p-2 rounded border"
+          />
+          <button
+            type="button"
+            class="btn btn-primary mt-2"
+            @click="testPrompt"
+          >
+            Test Prompt
+          </button>
+          <div
+            v-if="promptTestResult"
+            class="mt-2 text-green-500 animate-pulse"
+          >
+            Test Result: {{ promptTestResult }}
+          </div>
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="isPublic" class="block text-sm font-medium"
+            >Is Public:</label
+          >
+          <input
+            id="isPublic"
+            v-model="isPublic"
+            type="checkbox"
+            class="p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="underConstruction" class="block text-sm font-medium"
+            >Under Construction:</label
+          >
+          <input
+            id="underConstruction"
+            v-model="underConstruction"
+            type="checkbox"
+            class="p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="theme" class="block text-sm font-medium">Theme:</label>
+          <input
+            id="theme"
+            v-model="theme"
+            type="text"
+            class="w-full p-2 rounded border"
+          />
+        </div>
+        <div class="px-2 w-full md:w-1/2 mb-4">
+          <label for="personality" class="block text-sm font-medium"
+            >Personality:</label
+          >
+          <input
+            id="personality"
+            v-model="personality"
+            type="text"
+            class="w-full p-2 rounded border"
+          />
         </div>
       </div>
-
-      <div class="mb-4">
-        <label for="isPublic" class="block text-sm font-medium"
-          >Is Public:</label
-        >
-        <input
-          id="isPublic"
-          v-model="isPublic"
-          type="checkbox"
-          class="p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="underConstruction" class="block text-sm font-medium"
-          >Under Construction:</label
-        >
-        <input
-          id="underConstruction"
-          v-model="underConstruction"
-          type="checkbox"
-          class="p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="theme" class="block text-sm font-medium">Theme:</label>
-        <input
-          id="theme"
-          v-model="theme"
-          type="text"
-          class="w-full p-2 rounded border"
-        />
-      </div>
-
-      <div class="mb-4">
-        <label for="personality" class="block text-sm font-medium"
-          >Personality:</label
-        >
-        <input
-          id="personality"
-          v-model="personality"
-          type="text"
-          class="w-full p-2 rounded border"
-        />
-      </div>
-
       <bot-sample
         :name="name"
         :bot-type="botType"
@@ -204,9 +182,7 @@
         :personality="personality"
         :sample-response="sampleResponse"
       />
-
       <span v-if="isLoading" class="loading loading-ring loading-lg" />
-
       <button type="submit" class="btn btn-success w-full">Create Bot</button>
     </form>
   </div>
@@ -227,9 +203,9 @@ const errorStore = useErrorStore() // Use errorStore for managing errors
 const isLoading = ref(false)
 
 // Form fields
-const botType = ref('PROMPTBOT')
 const name = ref('')
 const subtitle = ref('Kind Robot')
+const botType = ref('PROMPTBOT')
 const description = ref("I'm a kind robot")
 const avatarImage = ref('/images/wonderchest/wonderchest304_(23).webp')
 const botIntro = ref('SloganMaker')

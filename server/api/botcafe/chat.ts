6c5f0c09d3646fa7c1e825fd34a1b8c778c52d28
Silas Knight <silasfelinus@gmail.com>
2024-08-31 15:06:40 -0700

    remove studio, change references to openai key

--- server/api/botcafe/chat.ts
+++ server/api/botcafe/chat.ts
@@ -3,60 +3,48 @@ import { defineEventHandler, readBody } from 'h3'
 
 export default defineEventHandler(async (event) => {
   try {
-    const body = await readBody(event)
-    let { OPENAI_API_KEY } = useRuntimeConfig()
-
-    // Check if user's key is provided in the request
-    if (body.user_openai_key) {
-      OPENAI_API_KEY = body.user_openai_key
-    }
+    const body = await readBody(event);
+    const { OPENAI_API_KEY: defaultKey } = useRuntimeConfig();
+    const apiKey = body.user_openai_key || event.req.headers['x-openai-key'] || defaultKey;
 
     const data = {
       model: body.model || 'gpt-4o-mini',
-      messages: body.messages || [
-        {
-          role: 'user',
-          content: 'write me a haiku about butterflies fighting malaria',
-        },
-      ],
+      messages: body.messages,
       temperature: body.temperature,
       max_tokens: body.maxTokens,
       n: body.n,
       stream: body.stream || false,
-    }
-    const post = body.post || 'https://api.openai.com/v1/chat/completions'
-    console.log('logging:', data)
+    };
+    const post = body.post || 'https://api.openai.com/v1/chat/completions';
 
     const response = await fetch(post, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${OPENAI_API_KEY}`,
+        Authorization: `Bearer ${apiKey}`,
       },
       body: JSON.stringify(data),
-    })
+    });
 
     if (!response.ok) {
-      const errorData = await response.json()
-      console.error('Response:', response)
+      const errorData = await response.json();
       throw createError({
         statusCode: response.status,
         statusMessage: `Error from OpenAI: ${response.statusText}. Details: ${JSON.stringify(errorData)}`,
-      })
+      });
     }
 
-    const responseData = await response.json()
-    return responseData
+    const responseData = await response.json();
+    return responseData;
   } catch (error) {
-    let errorMessage = 'An error occurred while creating the channel.'
-
+    let errorMessage = 'An error occurred while processing the request.';
     if (error instanceof Error) {
-      errorMessage += ` Details: ${error.message}`
+      errorMessage += ` Details: ${error.message}`;
     }
 
     throw createError({
       statusCode: 500,
       statusMessage: errorMessage,
-    })
+    });
   }
-})
+});

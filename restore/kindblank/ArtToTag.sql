619c87d74832344fb1297c4f77a2bde6ef7e3126
Silas Knight <silasfelinus@gmail.com>
2024-09-01 05:07:38 -0700

    restore database

---
+++ restore/kindblank/ArtToTag.sql
@@ -0,0 +1,18 @@
+
+CREATE  DATABASE IF NOT EXISTS	`kindblank` ;
+ use	`kindblank` ;
+/* SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;*/
+/* SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;*/
+/* SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS;*/
+/* SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION ;*/
+/* SET NAMES utf8;*/
+/* SET @OLD_TIME_ZONE=@@TIME_ZONE;*/
+/* SET TIME_ZONE='+00:00';*/
+/* SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;*/
+/* SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';*/
+
+insert into `_ArtToTag` (`A`, `B`) values(1 ,20);
+insert into `_ArtToTag` (`A`, `B`) values(2 ,20);
+insert into `_ArtToTag` (`A`, `B`) values(3 ,20);
+insert into `_ArtToTag` (`A`, `B`) values(4 ,20);
+insert into `_ArtToTag` (`A`, `B`) values(5 ,19);

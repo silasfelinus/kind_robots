acede51a5b20a791ce4a5403a678ffaab9e08702
Silas Knight <silasfelinus@gmail.com>
2024-09-01 05:07:38 -0700

    restore database

---
+++ restore/kindblank/Message.sql
@@ -0,0 +1,15 @@
+
+CREATE  DATABASE IF NOT EXISTS	`kindblank` ;
+ use	`kindblank` ;
+/* SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;*/
+/* SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;*/
+/* SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS;*/
+/* SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION ;*/
+/* SET NAMES utf8;*/
+/* SET @OLD_TIME_ZONE=@@TIME_ZONE;*/
+/* SET TIME_ZONE='+00:00';*/
+/* SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;*/
+/* SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';*/
+
+insert into `Message` (`id`, `createdAt`, `updatedAt`, `sender`, `recipient`, `content`, `channelId`, `botId`, `userId`) values(1 , '2023-10-04 02:38:49' ,null ,"WELCOMEBOT" ,"Kind User" ,"Hello! Welcome to our Livechat!" ,2 ,0 ,0);
+insert into `Message` (`id`, `createdAt`, `updatedAt`, `sender`, `recipient`, `content`, `channelId`, `botId`, `userId`) values(2 , '2023-10-05 16:59:42' ,null ,"WELCOMEBOT" ,"Kind User" ,"Hello! Welcome to our Livechat!" ,2 ,0 ,0);
---
+++ restore/kindblank/Tag.sql
@@ -0,0 +1,31 @@
+
+CREATE  DATABASE IF NOT EXISTS	`kindblank` ;
+ use	`kindblank` ;
+/* SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;*/
+/* SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;*/
+/* SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS;*/
+/* SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION ;*/
+/* SET NAMES utf8;*/
+/* SET @OLD_TIME_ZONE=@@TIME_ZONE;*/
+/* SET TIME_ZONE='+00:00';*/
+/* SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;*/
+/* SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';*/
+
+
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(2 , '2023-09-24 17:29:21' , '2023-10-01 19:19:42' ,"tag" ,"Abstract Art" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(6 , '2023-09-24 17:46:44' , '2023-10-01 19:19:42' ,"tag" ,"Liked" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(10 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Nsfw" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(11 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Sfw" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(12 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Flagged" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(13 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Weird" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(14 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Cat" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(15 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Hybrid" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(16 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Ai" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(17 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Favorite" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(18 , '2023-09-24 18:03:03' , '2023-10-01 19:19:23' ,"tag" ,"Highlight" ,0 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(19 , '2023-09-29 22:46:39' , '2023-10-01 19:19:23' ,"pitch" ,"AMI" ,1 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(20 , '2023-09-29 22:46:39' , '2023-10-01 19:19:23' ,"pitch" ,"zebrapunk" ,1 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(21 , '2023-09-29 22:46:39' , '2023-10-01 19:19:23' ,"pitch" ,"Fred Nylon" ,1 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(22 , '2023-09-29 22:46:39' , '2023-10-01 19:19:23' ,"pitch" ,"slothcore" ,1 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(23 , '2023-10-01 00:23:37' , '2023-10-01 20:17:56' ,"pitch" ,"dreamscape" ,1 ,1 ,null ,null ,null ,0 ,null ,null);
+insert into `Tag` (`id`, `createdAt`, `updatedAt`, `label`, `title`, `userId`, `isPublic`, `channelId`, `flavorText`, `pitch`, `isMature`, `sloganId`, `postId`) values(24 , '2023-10-03 00:11:48' ,null ,"pitch" ,"gothcore" ,0 ,0 ,null ,null ,null ,0 ,null ,null);

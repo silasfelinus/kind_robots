339c11e3eb4cc22e24e16a4f35e5b2b0696653b1
Silas Knight <silasfelinus@gmail.com>
2024-09-02 16:09:12 -0700

    adding build step to automate wonderlab folder access

--- server/api/utils/folderNames.ts
+++ server/api/utils/folderNames.ts
@@ -4,30 +4,39 @@ import { promises as fs } from 'fs'
 import path from 'path'
 import { defineEventHandler } from 'h3'
 
+interface Folder {
+  folderName: string;
+  components: string[];
+}
+
 export default defineEventHandler(async () => {
   try {
     const componentPath = path.resolve(process.cwd(), 'components/content')
-    console.log('Resolved component path:', componentPath); // Log the resolved path
-
     const folderNames = await fs.readdir(componentPath)
-    console.log('Folder names read:', folderNames); // Log the raw folder names
-
-    const folders = []
+    const folders: string[] = []
 
     for (const folderName of folderNames) {
       const folderPath = path.join(componentPath, folderName)
       const stat = await fs.stat(folderPath)
-      console.log('Processing:', folderPath); // Log each folder path processed
 
       if (stat.isDirectory()) {
         folders.push(folderName)
       }
     }
 
-    console.log('Folders array to return:', folders); // Log the final folders array
     return { response: folders }
   } catch (error) {
     console.error('Failed to fetch folder names:', error)
-    return { response: 'Failed to fetch folder names' }
+
+    try {
+      // Fallback to the JSON file
+      const jsonPath = path.resolve(process.cwd(), 'public/components.json')
+      const jsonData = await fs.readFile(jsonPath, 'utf-8')
+      const folders = (JSON.parse(jsonData) as Folder[]).map(folder => folder.folderName)
+      return { response: folders }
+    } catch (jsonError) {
+      console.error('Failed to fetch from JSON fallback:', jsonError)
+      return { response: 'Failed to fetch folder names' }
+    }
   }
 })

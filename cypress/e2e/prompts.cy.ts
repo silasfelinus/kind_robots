ba12a85057aeb9b98d209e913da5f212ff3302ee
Silas Knight <silasfelinus@gmail.com>
2024-09-03 16:27:45 -0700

    post api adjustments

--- cypress/e2e/prompts.cy.ts
+++ cypress/e2e/prompts.cy.ts
@@ -61,7 +61,7 @@ describe('Prompt Management API Tests', () => {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property('prompt');
       expect(response.body.prompt).to.be.an('string');
-      expect(response.body.prompt.prompt).to.eq('devil bunny');
+      expect(response.body.prompt).to.eq('devil bunny');
     });
   });
 

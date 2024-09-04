f942755095ec3c6d552b2f918f794cebc70d730e
Silas Knight <silasfelinus@gmail.com>
2024-09-04 02:16:00 -0700

    added items batch get

---
+++ server/api/items/index.get.ts
@@ -0,0 +1,24 @@
+// /server/api/items/index.get.ts
+import { defineEventHandler } from 'h3';
+import { errorHandler } from '../utils/error';
+import prisma from '../utils/prisma';
+
+export default defineEventHandler(async () => {
+  try {
+    // Fetch all cart items
+    const cartItems = await getAllCartItems();
+
+    if (!cartItems || cartItems.length === 0) {
+      return { success: false, message: 'No cart items found', statusCode: 404 };
+    }
+
+    return { success: true, cartItems };
+  } catch (error: unknown) {
+    return errorHandler(error);
+  }
+});
+
+// Function to retrieve all CartItems
+export async function getAllCartItems() {
+  return await prisma.cartItem.findMany(); // Fetches all cart items
+}

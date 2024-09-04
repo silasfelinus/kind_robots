cb9ac0e1161781c384964a4b63b5000671c3427d
Silas Knight <silasfelinus@gmail.com>
2024-09-03 19:17:29 -0700

    update pitch test for proper format

--- cypress/e2e/pitches.cy.ts
+++ cypress/e2e/pitches.cy.ts
@@ -75,7 +75,7 @@ describe('Pitch Management API Tests', () => {
       },
       body: {
         pitch: updatedPitchName,
-        description: 'This is an updated pitch description',
+        flavorText: 'This is an updated pitch description',
       },
     }).then((response) => {
       expect(response.status).to.eq(200, 'Response status should be 200');

3566d626d66f2edf90f39a477f47c5b099a303f5
Silas Knight <silasfelinus@gmail.com>
2024-09-05 14:17:20 -0700

    components in database instead of file

---
+++ server/api/components/folders.get.ts
@@ -0,0 +1,25 @@
+//server/api/components/folders.get.ts
+
+import { defineEventHandler } from 'h3'
+import prisma from '../utils/prisma'
+import { errorHandler } from '../utils/error'
+
+export default defineEventHandler(async () => {
+  try {
+    const folders = await prisma.component.findMany({
+      select: {
+        folderName: true,
+      },
+      distinct: ['folderName'], // Ensure unique folder names
+    })
+
+    const folderNames = folders.map(folder => folder.folderName)
+
+    return {
+      success: true,
+      folderNames,
+    }
+  } catch (error: unknown) {
+    return errorHandler(error)
+  }
+})

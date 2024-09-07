57c7c1363d2989e612dd53c207ffa3d398558783
Silas Knight <silasfelinus@gmail.com>
2024-09-06 23:13:18 -0700

    wrong header

--- components/content/layout/HeaderUpgrade.cy.ts
+++
@@ -1,8 +0,0 @@
-import HeaderUpgrade from './HeaderUpgrade.vue'
-
-describe('<HeaderUpgrade />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(HeaderUpgrade)
-  })
-})

6b533d956043b8b267ca81f725c6f5cd3eec39e8
Silas Knight <silasfelinus@gmail.com>
2024-09-07 15:23:27 -0700

    login button links to dashboard

--- components/content/admin/LoginButton.vue
+++ components/content/admin/LoginButton.vue
@@ -3,11 +3,10 @@
     <!-- Button column -->
     <div class="ml-4">
       <div v-if="isLoggedIn">
-        {{ username }}!
+        <router-link to="/dashboard" class="cursor-pointer hover:underline">
+          {{ username }}!
+        </router-link>
         <jellybean-count />
-        <button class="b p-2 rounded-lg text-white text-sm" @click="logout">
-          logout
-        </button>
       </div>
       <div v-else>
         <button
@@ -26,14 +25,12 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useUserStore } from './../../../stores/userStore'
 
 const userStore = useUserStore()
 const user = computed(() => userStore.user)
 const isLoggedIn = computed(() => userStore.isLoggedIn)
-const isLoading = ref(false)
-const errorMessage = ref('')
 
 const showLogin = ref(false)
 
@@ -41,16 +38,5 @@ const username = computed(() => user.value?.username || 'Kind Guest')
 
 const toggleLogin = () => {
   showLogin.value = !showLogin.value
-}
-
-const logout = async () => {
-  try {
-    isLoading.value = true
-    await userStore.logout()
-  } catch {
-    errorMessage.value = 'Failed to logout. Please try again.'
-  } finally {
-    isLoading.value = false
-  }
 }
 </script>

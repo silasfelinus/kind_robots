6ea370a81195a9f768b8f9181311d031d9138de5
Silas Knight <silasfelinus@gmail.com>
2024-09-08 23:50:25 -0700

    Bbetter api

--- components/content/rewards/ShowReward.vue
+++ components/content/rewards/ShowReward.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps } from 'vue'
+import { ref } from 'vue'
 
 // Destructure reward directly from props
 const { reward } = defineProps<{

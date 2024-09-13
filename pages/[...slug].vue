1fcce7b1f365b7e602a1704218ccfdc032b7be98
Silas M Knight <silasfelinus@gmail.com>
2024-09-13 16:26:15 -0700

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -8,4 +8,22 @@
 
 <script setup lang="ts">
 const { page } = useContent()
+
+useHead({
+  title: 'Kind Robots',
+  meta: [
+    { name: 'og:title', content: 'Welcome to the Kind Robots' },
+    {
+      name: 'description',
+      content: 'OpenAI-supported Promptbots here to assist humanity.',
+    },
+    {
+      name: 'og:description',
+      content:
+        'Make and Share OpenAI prompts, AI-assisted art, and find the secret jellybeans',
+    },
+    { name: 'og:image', content: '/images/kindtitle.webp' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+  ],
+})
 </script>

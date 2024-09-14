09a3a09e4a9105decb58054586b3d3d8c0047b8c
Silas Knight <silasfelinus@gmail.com>
2024-09-14 04:40:02 -0700

    adjustments

--- training/steps.js
+++ training/steps.js
@@ -2,24 +2,27 @@ export const steps = [
   {
     id: 'intro',
     title: 'Welcome to Kind Robots',
-    description: 'We bring people and AI together to do amazing things.',
-    image: '/images/intro/welcome.webp',
-    altText: 'Intro Image',
+    description:
+      "We're here to enrich your experience on planet earth, and nourish the earth in the process",
+    image: 'w/intro/welcome.webp',
+    altText: 'Welcome to Kind Robots',
     buttonText: 'Let’s Begin',
     bgClass: 'bg-accent',
   },
   {
-    id: 'discover',
-    title: 'Discover More',
-    description: 'Explore our sections and learn more.',
+    id: 'art',
+    title: 'Create Art',
+    description:
+      'Manage Pitches, create Art, and share Galleries with other users using our AI-infused art modellers!',
     image: '/images/intro/art1.webp',
-    altText: 'Discover Image',
+    altText: 'Create Art',
     buttonText: 'Next',
     bgClass: 'bg-base-100',
   },
   {
-    id: 'features',
-    title: 'Learn About Our Features',
+    id: 'botcafe',
+    title:
+      'Share words with our resident AI chat experts. Manage writing prompts, create and bots, and possibly interact with other humans!',
     description: 'Check out all the amazing things we can do together.',
     image: '/images/intro/words.webp',
     altText: 'Features Image',
@@ -28,9 +31,9 @@ export const steps = [
   },
   {
     id: 'outro',
-    title: 'Thank You for Joining Us!',
+    title: 'Singular Philanthropy',
     description:
-      'We’re so grateful for your support and hope you enjoyed the experience!',
+      'Kind Robots was created by Silas Knight. It is intended primarily as a fundraiser for malaria nets. https://againstmalaria.com/amibot',
     image: '/images/intro/footer.webp',
     altText: 'Thank You Image',
     buttonText: 'Finish',

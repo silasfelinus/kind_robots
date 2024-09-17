0e5f310ee3fbabe7f1b7155a8806e14e177bc609
Silas Knight <silasfelinus@gmail.com>
2024-09-17 14:58:57 -0700

    reducing borders

--- app.vue
+++ app.vue
@@ -24,9 +24,7 @@
 
       <!-- Main Content -->
       <div class="flex-grow flex justify-center items-center">
-        <div
-          class="w-full max-w-4xl rounded-2xl p-1 bg-base-200 overflow-y-auto"
-        >
+        <div class="w-full max-w-4xl bg-base-200 overflow-y-auto">
           <nuxt-page />
         </div>
       </div>

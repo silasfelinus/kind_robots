f8c13ae3e555a1559051a8a883ddc3352758f88b
Silas Knight <silasfelinus@gmail.com>
2024-09-18 15:31:55 -0700

    testing nuxt ui

--- components/content/layout/KindSidebarSimple.vue
+++ components/content/layout/KindSidebarSimple.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex">
-    <aside
+    <Aside
       v-if="(displayStore.sidebarLeft as DisplayState) !== 'hidden'"
       class="transition-all duration-300 bg-base-200 hide-scrollbar flex-grow p-2"
       :class="{
@@ -14,32 +14,17 @@
         top: displayStore.headerVh + 'vh',
       }"
     >
-      <div>
-        <div class="p-1">
-          <!-- Sidebar Links with Icons and Titles -->
-          <div
-            v-for="link in filteredLinks"
-            :key="link.title"
-            :to="link.path"
-            :style="{ height: iconHeight + 'px', margin: '1px 0' }"
-            class="Icon-link-container flex items-center space-x-2 hover:bg-base-100 hover:scale-105 rounded-xl mt-1 mb-1 p-1"
-          >
-            <!-- Icon for each link -->
-            <Icon
-              :name="link.icon"
-              class="h-12 w-12 transition-all duration-300 ease-in-out"
-            />
-            <!-- Only show the link title when the sidebar is fully open -->
-            <span
-              v-if="isSidebarOpen"
-              class="text-sm font-semibold ml-2 transition-opacity duration-300"
-            >
-              {{ link.title }}
-            </span>
-          </div>
-        </div>
-      </div>
-    </aside>
+      <!-- Sidebar Links with Icons and Titles -->
+      <Aside.Link
+        v-for="link in filteredLinks"
+        :key="link.title"
+        :to="link.path"
+        :icon="link.icon"
+        :label="link.title"
+        :style="{ height: iconHeight + 'px', margin: '1px 0' }"
+        class="hover:bg-base-100 hover:scale-105 rounded-xl mt-1 mb-1 p-1"
+      />
+    </Aside>
   </div>
 </template>
 
@@ -51,9 +36,6 @@ import { sidebarLinks } from '@/assets/sidebar'
 
 // Access the display store for the sidebar state
 const displayStore = useDisplayStore()
-
-// Computed properties to check the sidebar state
-const isSidebarOpen = computed(() => displayStore.sidebarLeft === 'open')
 
 // Sidebar Links
 const filteredLinks = computed(() => sidebarLinks)

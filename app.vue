6fe134f99f88aeb12398b2acfbff92053042fea5
Silas M Knight <silasfelinus@gmail.com>
2024-09-18 17:40:53 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -23,17 +23,17 @@
       </header>
 
       <!-- Main Layout -->
-      <main class="flex-1 w-full flex">
+      <div class="flex-1 w-full flex">
         <kind-sidebar-simple />
         <!-- Main Content with scrollable area -->
-        <div class="flex-grow overflow-y-auto">
+        <main class="flex-grow overflow-y-auto">
           <div class="flex justify-center items-center">
             <div class="w-full max-w-4xl rounded-2xl bg-base-200">
               <NuxtPage />
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
 
       <!-- Footer (Stick to Bottom) -->
       <footer

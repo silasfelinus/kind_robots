cecdd1f8f68e9b3f2144e1d1c56096a1151bb56b
Silas Knight <silasfelinus@gmail.com>
2024-09-19 19:09:13 -0700

    removed transition

--- app.vue
+++ app.vue
@@ -74,14 +74,4 @@ onBeforeUnmount(() => {
 main {
   overflow-y: auto;
 }
-
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.2s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
 </style>

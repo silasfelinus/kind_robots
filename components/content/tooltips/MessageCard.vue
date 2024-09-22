ef8b9575c6b26216c77220142c0527b9a8da0f3e
Silas Knight <silasfelinus@gmail.com>
2024-09-21 18:31:42 -0700

    restore message card

--- components/content/tooltips/MessageCard.vue
+++ components/content/tooltips/MessageCard.vue
@@ -1,14 +1,19 @@
 <template>
   <transition name="fade-slide">
     <div
-      
+      v-if="show"
       class="message-card"
       :class="[
         bgClass,
         'z-10 p-1 rounded-lg border flex items-center space-x-2',
       ]"
     >
-      
+      <button
+        class="absolute top-0 right-0 m-1 text-sm"
+        @click="emitRemoveCard"
+      >
+        .
+      </button>
 
       <img :src="imageSrc" :alt="altText" class="rounded-full w-10 h-10" />
       <div>
@@ -24,7 +29,8 @@
 </template>
 
 <script setup lang="ts">
-const { bgClass, imageSrc, altText, username, message } = defineProps<{
+const { show, bgClass, imageSrc, altText, username, message } = defineProps<{
+  show: boolean
   bgClass: string
   imageSrc: string
   altText: string
@@ -32,5 +38,9 @@ const { bgClass, imageSrc, altText, username, message } = defineProps<{
   message: string
 }>()
 
+const emit = defineEmits(['remove-card'])
 
+const emitRemoveCard = () => {
+  emit('remove-card')
+}
 </script>

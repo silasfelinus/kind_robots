eb451f2f2acaf76b524c215165348ea58b15ff36
Silas M Knight <silasfelinus@gmail.com>
2024-09-21 18:17:27 -0700

    Update PageInfo.vue

--- components/content/tooltips/PageInfo.vue
+++ components/content/tooltips/PageInfo.vue
@@ -31,23 +31,18 @@
 
         <!-- Title and Subtitle -->
         <div class="flex flex-col justify-center items-center">
-          <h1 class="text-2xl font-bold text-secondary">{{ pageTitle }}</h1>
+          <h1 class="text-2xl font-bold text-secondary">{{ page.title }}</h1>
           <h2 v-if="pageSubtitle" class="text-lg font-medium text-accent">
-            {{ pageSubtitle }}
+            {{ page.subtitle }}
           </h2>
         </div>
 
         <!-- Description -->
         <div class="text-center text-base-content max-w-md">
-          <p>{{ pageDescription }}</p>
+          <p>{{ page.description }}</p>
         </div>
 
-        <!-- Tooltip Info -->
-        <div class="mt-2 text-center text-neutral">
-          <p v-if="pageTooltip" class="italic">{{ pageTooltip }}</p>
-          <p v-if="pageDottitip" class="mt-1">{{ pageDottitip }}</p>
-          <p v-if="pageAmitip" class="mt-1 text-sm">{{ pageAmitip }}</p>
-        </div>
+        <page-info-tips />
       </div>
 
       <!-- OK button and Show Info checkbox -->
@@ -101,15 +96,7 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
-// Access dynamic content from the page
-const pageTitle = computed(() => page?.title || 'Welcome')
-const pageSubtitle = computed(() => page?.subtitle || 'No subtitle available')
-const pageDescription = computed(
-  () => page?.description || 'No description available',
-)
-const pageTooltip = computed(() => page?.tooltip || 'No tooltip available')
-const pageDottitip = computed(() => page?.dottitip || 'No dottitip available')
-const pageAmitip = computed(() => page?.amitip || 'No amitip available')
+
 </script>
 
 <style scoped>

64d816306f8617796b4651015f05eae80c8720c7
Silas M Knight <silasfelinus@gmail.com>
2024-09-24 04:47:48 -0700

    Update displayStore.ts

--- stores/displayStore.ts
+++ stores/displayStore.ts
@@ -96,7 +96,7 @@ export const useDisplayStore = defineStore('display', {
           case 'small':
             return this.sidebarLeft === 'open' ? 13 : this.sidebarLeft === 'compact' ? 12 : 2
           case 'medium':
-            return this.sidebarLeft === 'open' ? 13 : this.sidebarLeft === 'compact' ? 10 : 2
+            return this.sidebarLeft === 'open' ? 16 : this.sidebarLeft === 'compact' ? 10 : 2
           case 'large':
             return this.sidebarLeft === 'open' ? 13 : this.sidebarLeft === 'compact' ? 8 : 1
           case 'extraLarge':

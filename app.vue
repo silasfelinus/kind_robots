9cf1d9c9268392623b8276def924fbc2f291686e
Silas Knight <silasfelinus@gmail.com>
2024-09-25 20:28:08 -0700

    added header

--- app.vue
+++ app.vue
@@ -6,7 +6,33 @@
       :style="{
         height: 'calc(var(--vh, 1vh) * ' + displayStore.headerVh + ')',
       }"
-    ></header>
+    >
+      <!-- Sidebar Toggle -->
+      <div class="absolute top-2 left-4 p-1 z-40 text-white">
+        <sidebar-toggle class="text-4xl" />
+      </div>
+
+      <!-- Navigation Links -->
+      <nav-links class="flex-grow ml-16" />
+      <!-- Added margin to avoid overlapping with the sidebar toggle -->
+
+      <!-- Tutorial and Back Buttons -->
+      <button
+        v-if="displayStore.showTutorial"
+        class="bg-info text-base-200 rounded-lg shadow-md hover:bg-info-focus transition duration-300 z-50 p-2 mr-4"
+        @click="displayStore.toggleTutorial"
+      >
+        Launch
+      </button>
+
+      <button
+        v-else
+        class="bg-secondary text-base-200 rounded-lg shadow-md hover:bg-secondary-focus transition duration-300 z-50 p-2 mr-4"
+        @click="displayStore.toggleTutorial"
+      >
+        Instructions
+      </button>
+    </header>
 
     <!-- Main content area with sidebars and main content -->
     <div

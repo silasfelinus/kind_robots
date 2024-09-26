f9f5b957f9cdd09d1c7eef80713772355a07a12e
Silas Knight <silasfelinus@gmail.com>
2024-09-25 20:55:46 -0700

    header alignment and refactored

--- components/content/layout/MainHeader.vue
+++ components/content/layout/MainHeader.vue
@@ -1,21 +1,21 @@
 <template>
   <header
-    class="sticky top-0 z-30 w-full bg-base-200 flex items-center justify-between"
+    class="w-full bg-base-200 items-center justify-between"
     :style="{ height: `${displayStore.headerVh}vh` }"
   >
     <!-- Sidebar Toggle -->
-    <div class="absolute top-2 left-4 p-1 z-40 text-white">
+    <div class="p-1 z-40 text-white ml-2">
       <sidebar-toggle class="text-4xl" />
     </div>
 
     <!-- Navigation Links -->
-    <nav-links class="flex-grow ml-16" />
+    <nav-links class="flex-grow" />
     <!-- Added margin to avoid overlapping with the sidebar toggle -->
 
     <!-- Tutorial and Back Buttons -->
     <button
       v-if="displayStore.showTutorial"
-      class="bg-info text-base-200 rounded-lg shadow-md hover:bg-info-focus transition duration-300 z-50 p-2 mr-4"
+      class="bg-info text-base-200 rounded-lg shadow-md hover:bg-info-focus transition duration-300 z-50 p-1 mr-2"
       @click="displayStore.toggleTutorial"
     >
       Launch
@@ -23,7 +23,7 @@
 
     <button
       v-else
-      class="bg-secondary text-base-200 rounded-lg shadow-md hover:bg-secondary-focus transition duration-300 z-50 p-2 mr-4"
+      class="bg-secondary text-base-200 rounded-lg shadow-md hover:bg-secondary-focus transition duration-300 z-50 p-1 mr-2"
       @click="displayStore.toggleTutorial"
     >
       Instructions

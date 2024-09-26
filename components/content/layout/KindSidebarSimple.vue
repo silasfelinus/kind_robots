c93367756dc7d897dd87842f0e6248cbce09f2dd
Silas Knight <silasfelinus@gmail.com>
2024-09-26 02:07:42 -0700

    removing import for icon names so they load easier in tailwind

--- components/content/layout/KindSidebarSimple.vue
+++ components/content/layout/KindSidebarSimple.vue
@@ -12,7 +12,7 @@
     >
       <!-- Sidebar Links with Icons and Titles -->
       <div
-        v-for="link in filteredLinks"
+        v-for="link in hardcodedLinks"
         :key="link.title"
         :style="{ height: iconHeight + 'px', margin: '1px 0' }"
         class="Icon-link-container flex items-center space-x-2 hover:bg-base-300 hover:scale-105 rounded-xl mt-1 mb-1 p-1 text-accent"
@@ -41,19 +41,49 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, computed } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useDisplayStore } from '@/stores/displayStore'
-import { sidebarLinks } from '@/assets/sidebar'
 import { useRouter } from 'vue-router'
+
+// Hardcoded sidebar links
+const hardcodedLinks = [
+  { title: 'Home', path: '/', icon: 'line-md:home-md-twotone' },
+  { title: 'Add Bot', path: '/addbot', icon: 'fluent:bot-add-20-regular' },
+  { title: 'Bot Cafe', path: '/botcafe', icon: 'mdi:chat-processing' },
+  {
+    title: 'Pitch Manager',
+    path: '/pitch',
+    icon: 'fluent:chat-bubbles-question-16-regular',
+  },
+  { title: 'Art Maker', path: '/artmaker', icon: 'game-icons:easel' },
+  { title: 'Hot or Not?', path: '/hotornot', icon: 'emojione-monotone:fire' },
+  { title: 'Art Gallery', path: '/artgallery', icon: 'mdi:palette' },
+  {
+    title: 'Art Challenge',
+    path: '/artchallenge',
+    icon: 'game-icons:tabletop-players',
+  },
+  {
+    title: 'Brainstorm',
+    path: '/brainstorm',
+    icon: 'game-icons:robot-antennas',
+  },
+  {
+    title: 'Memory Match',
+    path: '/memory',
+    icon: 'material-symbols:art-track-outline-rounded',
+  },
+  { title: 'Dashboard', path: '/dashboard', icon: 'mingcute:settings-6-fill' },
+  { title: 'Mature Content', path: '/mature', icon: 'fxemoji:lips' },
+  { title: 'Wonderlab', path: '/wonderlab', icon: 'game-icons:gear-hammer' },
+  { title: 'About', path: '/about', icon: 'game-icons:abstract-037' },
+]
 
 // Access router to handle navigation
 const router = useRouter()
 
 // Access the display store for the sidebar state
 const displayStore = useDisplayStore()
-
-// Computed properties to filter links
-const filteredLinks = computed(() => sidebarLinks)
 
 // Adjust height calculations based on window size and available space
 const availableSidebarHeight = ref(
@@ -67,7 +97,7 @@ const navigate = (path: string) => {
 
 onMounted(() => {
   const calculateIconHeight = () => {
-    const totalLinks = sidebarLinks.length
+    const totalLinks = hardcodedLinks.length
     const marginSpace = 4 * totalLinks // Adjust for link margins
     const sidebarHeightInPx =
       (availableSidebarHeight.value * window.innerHeight) / 100

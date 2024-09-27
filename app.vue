3b7a03807d19188f597f3600f6ff174763787d88
Silas Knight <silasfelinus@gmail.com>
2024-09-27 15:12:14 -0700

    still working on load

--- app.vue
+++ app.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="main-layout absolute inset-0 bg-base-300">
     <!-- Loader Component -->
-    <div>
-      <kind-loader></kind-loader>
-    </div>
 
     <!-- Header -->
     <header

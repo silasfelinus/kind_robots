56321efa714af5cf73f039d0fb7cc1041c97a19a
Silas Knight <silasfelinus@gmail.com>
2024-09-26 21:25:49 -0700

    error processing

--- components/content/prompts/AddPitch.vue
+++ components/content/prompts/AddPitch.vue
@@ -200,7 +200,7 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import { usePitchStore, type PitchType } from './../../../stores/pitchStore'
+import { usePitchStore, PitchType } from './../../../stores/pitchStore'
 import { useUserStore } from './../../../stores/userStore'
 import { useErrorStore, ErrorType } from './../../../stores/errorStore'
 

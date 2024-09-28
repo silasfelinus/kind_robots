c7ff845391c2a4cc54b96188e4436ad03e980726
Silas M Knight <silasfelinus@gmail.com>
2024-09-27 23:06:48 -0700

    Update index.ts

--- stores/index.ts
+++ stores/index.ts
@@ -1,22 +1,5 @@
 // stores/index.ts
-// stores/index.ts
-import type { Ref } from 'vue'
 
-export * from './botStore'
-export * from './errorStore'
-export * from './galleryStore'
-export * from './resourceStore'
-export * from './screenStore'
-export * from './statusStore'
-export * from './themeStore'
-export * from './userStore'
-export * from './choiceStore'
-export * from './dreamStore'
-
-export interface Store<T = unknown> {
-  loadStore: () => Promise<T> // Replace `T` with a more specific type if possible
-  status: Ref<'loading' | 'loaded' | 'error'>
-}
 
 export const loadPromptStore = async () => {
   const { usePromptStore } = await import('@/stores/promptStore');

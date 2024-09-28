899567c424a9e0000ec8198eabf632e8de05edf1
Silas M Knight <silasfelinus@gmail.com>
2024-09-27 23:06:21 -0700

    Update index.ts

--- stores/index.ts
+++ stores/index.ts
@@ -1,4 +1,5 @@
 // stores/index.ts
+// stores/index.ts
 import type { Ref } from 'vue'
 
 export * from './botStore'
@@ -16,3 +17,34 @@ export interface Store<T = unknown> {
   loadStore: () => Promise<T> // Replace `T` with a more specific type if possible
   status: Ref<'loading' | 'loaded' | 'error'>
 }
+
+export const loadPromptStore = async () => {
+  const { usePromptStore } = await import('@/stores/promptStore');
+  return usePromptStore();
+};
+
+export const loadArtStore = async () => {
+  const { useArtStore } = await import('@/stores/artStore');
+  return useArtStore();
+};
+
+export const loadBotStore = async () => {
+  const { useBotStore } = await import('@/stores/botStore');
+  return useBotStore();
+};
+
+export const loadGalleryStore = async () => {
+  const { useGalleryStore } = await import('@/stores/galleryStore');
+  return useGalleryStore();
+};
+
+export const loadComponentStore = async () => {
+  const { useComponentStore } = await import('@/stores/componentStore');
+  return useComponentStore();
+};
+
+export const loadTagStore = async () => {
+  const { useTagStore } = await import('@/stores/tagStore');
+  return useTagStore();
+};
+

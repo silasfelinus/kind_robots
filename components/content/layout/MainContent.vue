d3651d428557b4728b76eb282433ee0d69f6cf94
Silas M Knight <silasfelinus@gmail.com>
2024-09-30 23:04:42 -0700

    Update MainContent.vue

--- components/content/layout/MainContent.vue
+++ components/content/layout/MainContent.vue
@@ -11,44 +11,46 @@
     </div>
 
     <!-- For large viewports, display two columns -->
-    <div v-if="isLargeViewport" class="two-column">
+    <div v-if="isLargeViewport && !isFullScreen" class="two-column">
       <div class="left-column">
         <SplashTutorial />
       </div>
       <div class="right-column">
         <NuxtPage />
       </div>
-      <div v-if="isFullScreen" class="full-screen-toggle">
-        <button @click="toggleFullScreen">{{ fullScreenButtonText }}</button>
-      </div>
     </div>
+
+    <!-- Fullscreen view -->
+    <div v-if="isLargeViewport && isFullScreen" class="fullscreen-content">
+      <NuxtPage />
+    </div>
+
+    <!-- Full-Screen Toggle Button for Large Viewports -->
+    <button
+      v-if="isLargeViewport"
+      class="bg-primary text-base-200 rounded-lg shadow-md hover:bg-primary-focus transition duration-300 z-40 p-1 ml-4"
+      @click="toggleFullScreen"
+    >
+      {{ fullScreenButtonText }}
+    </button>
+
+    <!-- Launch and Instructions Button (for Small/Medium Viewports) -->
+    <button
+      v-if="showLaunchButton"
+      class="bg-info text-base-200 rounded-lg shadow-md hover:bg-info-focus transition duration-300 z-40 p-1 ml-4 mr-4"
+      @click="toggleTutorial"
+    >
+      Launch
+    </button>
+
+    <button
+      v-if="showInstructionsButton"
+      class="bg-secondary text-base-200 rounded-lg shadow-md hover:bg-secondary-focus transition duration-300 z-40 p-1 ml-4"
+      @click="toggleTutorial"
+    >
+      Instructions
+    </button>
   </div>
-
-  <!-- Full-Screen Toggle Button for Large Viewports -->
-  <button
-    v-if="isLargeViewport"
-    class="bg-primary text-base-200 rounded-lg shadow-md hover:bg-primary-focus transition duration-300 z-40 p-1 ml-4"
-    @click="toggleFullScreen"
-  >
-    {{ isFullScreen ? 'Two Columns' : 'Full Screen' }}
-  </button>
-
-  <!-- Launch and Instructions Button (for Small/Medium Viewports) -->
-  <button
-    v-if="showLaunchButton"
-    class="bg-info text-base-200 rounded-lg shadow-md hover:bg-info-focus transition duration-300 z-40 p-1 ml-4 mr-4"
-    @click="toggleTutorial"
-  >
-    Launch
-  </button>
-
-  <button
-    v-if="showInstructionsButton"
-    class="bg-secondary text-base-200 rounded-lg shadow-md hover:bg-secondary-focus transition duration-300 z-40 p-1 ml-4"
-    @click="toggleTutorial"
-  >
-    Instructions
-  </button>
 </template>
 
 <script setup lang="ts">
@@ -57,27 +59,27 @@ import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
 
+// Viewport conditions
 const isMobileViewport = computed(() => displayStore.isMobileViewport)
 const isLargeViewport = computed(() => displayStore.isLargeViewport)
 const showTutorial = computed(() => displayStore.showTutorial)
 const isFullScreen = computed(() => displayStore.isFullScreen)
 
-const showLaunchButton = computed(
-  () =>
-    !displayStore.showTutorial &&
-    ['small', 'medium'].includes(displayStore.viewportSize),
+// Button visibility logic
+const showLaunchButton = computed(() =>
+  !displayStore.showTutorial && ['small', 'medium'].includes(displayStore.viewportSize),
 )
 
-const showInstructionsButton = computed(
-  () =>
-    displayStore.showTutorial &&
-    ['small', 'medium'].includes(displayStore.viewportSize),
+const showInstructionsButton = computed(() =>
+  displayStore.showTutorial && ['small', 'medium'].includes(displayStore.viewportSize),
 )
 
+// Button text for fullscreen toggle
 const fullScreenButtonText = computed(() =>
   isFullScreen.value ? 'Exit Full Screen' : 'Full Screen',
 )
 
+// Methods to toggle full screen and tutorial
 const toggleFullScreen = () => {
   displayStore.toggleFullScreen()
 }
@@ -99,6 +101,13 @@ const toggleTutorial = () => {
   display: grid;
   grid-template-columns: 1fr 1fr;
   height: 100%;
+}
+
+.fullscreen-content {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .left-column,

cc56a595dbd1839c9579a9a8704961e05db20288
Silas M Knight <silasfelinus@gmail.com>
2024-10-01 00:51:30 -0700

    Update KindButtons.vue

--- components/content/layout/KindButtons.vue
+++ components/content/layout/KindButtons.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="flex items-center space-x-4">
+  <div class="kind-buttons fixed inset-0 flex items-end justify-end p-4 z-50">
+    <!-- Fullscreen / Two-column Toggle -->
     <button
-      class="bg-primary text-base-200 rounded-lg shadow-md hover:bg-primary-focus transition duration-300 p-2"
+      class="bg-primary text-base-200 rounded-lg shadow-md hover:bg-primary-focus transition duration-300 p-2 m-2"
       @click="toggleFullScreen"
     >
       {{ fullScreenButtonText }}
     </button>
-    <!-- Add more buttons here if needed -->
+
+    <!-- Tutorial / NuxtPage Toggle -->
+    <button
+      class="bg-accent text-base-200 rounded-lg shadow-md hover:bg-accent-focus transition duration-300 p-2 m-2"
+      @click="toggleTutorial"
+    >
+      Switch to {{ showTutorial ? 'Nuxt Page' : 'Tutorial' }}
+    </button>
   </div>
 </template>
 
@@ -16,24 +24,31 @@ import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
 
-// Fullscreen button logic
+// Fullscreen / Two-column toggle button
+const isFullScreen = computed(() => displayStore.isFullScreen)
 const fullScreenButtonText = computed(() =>
   displayStore.isFullScreen ? 'Exit Full Screen' : 'Full Screen'
 )
 
-// Fullscreen toggle
+// Tutorial / NuxtPage toggle button
+const showTutorial = computed(() => displayStore.showTutorial)
 const toggleFullScreen = () => {
   displayStore.toggleFullScreen()
+}
+
+const toggleTutorial = () => {
+  displayStore.toggleTutorial()
 }
 </script>
 
 <style scoped>
-button {
-  z-index: 50;
-  transition: background-color 0.3s ease;
+.kind-buttons {
+  width: 100vw;
+  height: 100vh;
+  pointer-events: none; /* This ensures that the component doesn't interfere with other elements */
 }
 
-button:hover {
-  cursor: pointer;
+button {
+  pointer-events: auto; /* Ensures buttons themselves are clickable */
 }
 </style>

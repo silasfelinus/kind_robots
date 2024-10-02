456efa74d007fbc735a85b5fca63a2daf0c4a985
Silas Knight <silasfelinus@gmail.com>
2024-10-02 01:35:54 -0700

    better buttons redux

--- components/content/layout/KindButtons.vue
+++ components/content/layout/KindButtons.vue
@@ -1,16 +1,36 @@
 <template>
   <div class="kind-buttons">
     <!-- Fullscreen / Two-column Toggle (Top-right corner) -->
-    <button
-      class="fixed hidden lg:block top-4 right-4 bg-accent text-base-200 rounded-lg shadow-md hover:bg-primary transition duration-300 p-1"
-      @click="toggleFullScreen"
+    <div
+      class="fixed hidden lg:flex top-4 right-4 items-center space-x-2 bg-accent text-base-200 rounded-lg shadow-md p-2 transition duration-300"
     >
-      {{ fullScreenButtonText }}
-    </button>
+      <!-- Two-column layout button -->
+      <button
+        class="icon-button"
+        :class="{
+          'bg-info text-base-200': isFullScreen,
+          'bg-gray-400 text-gray-700': !isFullScreen,
+        }"
+        @click="toggleFullScreen"
+      >
+        <Icon name="mdi-view-column" class="w-6 h-6" />
+      </button>
+      <!-- One-column layout button -->
+      <button
+        class="icon-button"
+        :class="{
+          'bg-info text-base-200': !isFullScreen,
+          'bg-gray-400 text-gray-700': isFullScreen,
+        }"
+        @click="toggleFullScreen"
+      >
+        <Icon name="mdi-monitor" class="w-6 h-6" />
+      </button>
+    </div>
 
     <!-- Tutorial / NuxtPage Toggle (Bottom-center of the screen) -->
     <button
-      v-if="!displayStore.isFullScreen"
+      v-if="!isFullScreen"
       class="fixed bottom-4 right-4 transform -translate-x-1/2 bg-primary text-base-200 rounded-lg shadow-md hover:bg-accent transition duration-300 z-50 p-1"
       @click="toggleTutorial"
     >
@@ -25,15 +45,13 @@ import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
 
-const fullScreenButtonText = computed(() =>
-  displayStore.isFullScreen ? '1 Column' : '2 Columns',
-)
-
-const showTutorial = computed(() => displayStore.showTutorial)
+const isFullScreen = computed(() => displayStore.isFullScreen)
 
 const toggleFullScreen = () => {
   displayStore.toggleFullScreen()
 }
+
+const showTutorial = computed(() => displayStore.showTutorial)
 
 const toggleTutorial = () => {
   displayStore.toggleTutorial()
@@ -45,7 +63,14 @@ const toggleTutorial = () => {
   pointer-events: auto;
 }
 
-button:hover {
+.icon-button {
   cursor: pointer;
+  padding: 8px;
+  border-radius: 9999px;
+  transition: background-color 0.3s;
+}
+
+.icon-button:hover {
+  filter: brightness(1.1);
 }
 </style>

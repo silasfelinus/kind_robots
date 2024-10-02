8506a54650bf31d946f73875b8d30c494077bb4f
Silas Knight <silasfelinus@gmail.com>
2024-10-01 17:11:05 -0700

    better loader

--- components/content/screenfx/butterfly-animation.vue
+++ components/content/screenfx/butterfly-animation.vue
@@ -1,43 +1,14 @@
 <template>
-  <div class="loading-overlay" :class="{ 'fade-out': fadeOut }">
+  <div class="loading-overlay">
     <!-- Multiple Butterflies with Animation Delay -->
-    <ami-butterfly
-      v-for="i in butterflyCount"
-      :key="i"
-      :class="{ 'butterfly-fade-out': butterflyFadeOut }"
-    />
+    <ami-butterfly v-for="i in butterflyCount" :key="i" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-import { useDisplayStore } from '@/stores/displayStore'
+import { ref } from 'vue'
 
-const displayStore = useDisplayStore()
-
-const butterflyCount = ref(20)
-const fadeOut = ref(false) // Overlay fade-out state
-const butterflyFadeOut = ref(false) // New state for butterflies' fade-out
-
-// Watch the display store's isAnimating state
-watch(
-  () => displayStore.isAnimating,
-  (newValue) => {
-    if (!newValue) {
-      // When the animation ends, trigger the butterfly fade-out
-      butterflyFadeOut.value = true
-
-      // Delay the overlay fade-out to allow butterflies to finish fading out
-      setTimeout(() => {
-        fadeOut.value = true
-      }, 1000) // Delay to match the butterfly fade-out transition
-    } else {
-      // Reset fade states if starting animation again
-      butterflyFadeOut.value = false
-      fadeOut.value = false
-    }
-  },
-)
+const butterflyCount = ref(20) // Number of butterflies to display
 </script>
 
 <style scoped>
@@ -51,24 +22,5 @@ watch(
   z-index: 9999;
   transition: opacity 1s;
   pointer-events: auto;
-}
-.loading-overlay.fade-out {
-  opacity: 0;
-  pointer-events: none;
-}
-
-/* Fade-out animation for butterflies */
-.butterfly-fade-out {
-  opacity: 0;
-  transition: opacity 1s;
-}
-
-.nuxt-wrapper {
-  opacity: 0;
-  transition: opacity 1s;
-}
-
-.nuxt-wrapper.fade-in {
-  opacity: 1;
 }
 </style>

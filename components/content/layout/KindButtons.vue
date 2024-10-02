7f99b95d026dab0bd919c177172d3486eaedeb24
Silas Knight <silasfelinus@gmail.com>
2024-10-01 23:47:23 -0700

    remodeled buttons

--- components/content/layout/KindButtons.vue
+++ components/content/layout/KindButtons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="kind-buttons z-50">
+  <div class="kind-buttons">
     <!-- Fullscreen / Two-column Toggle (Top-right corner) -->
     <button
       class="fixed hidden lg:block top-4 right-4 bg-accent text-base-200 rounded-lg shadow-md hover:bg-primary transition duration-300 p-1"
@@ -11,7 +11,7 @@
     <!-- Tutorial / NuxtPage Toggle (Bottom-center of the screen) -->
     <button
       v-if="!displayStore.isFullScreen"
-      class="fixed bottom-4 right-4 transform -translate-x-1/2 bg-primary text-base-200 rounded-lg shadow-md hover:bg-accent transition duration-300 z-50 p-1"
+      class="fixed bottom-4 right-4 transform -translate-x-1/2 bg-primary text-base-200 rounded-lg shadow-md hover:bg-accent transition duration-300 z-[60] p-1"
       @click="toggleTutorial"
     >
       Show {{ showTutorial ? 'Nuxt Page' : 'Tutorial' }}

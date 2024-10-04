95ddfa35e3e2e47262b92d9b52ad75c4a8a12169
Silas Knight <silasfelinus@gmail.com>
2024-10-03 17:07:57 -0700

    minimal layout

---
+++ layouts/minimal.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="relative flex flex-col h-screen bg-primary">
+    <!-- Header -->
+    <header-upgrade
+      v-if="toggleSidebar"
+      ref="headerRef"
+      class="flex flex-col items-center bg-base-200 rounded-2xl p-2 m-2 border"></header-upgrade>
+    <!-- Header -->
+
+    <!-- Toggle Button always visible -->
+    <div class="absolute right-4 top-4 z-50">
+      <button
+        class="bg-accent text-white p-2 rounded-full shadow-md"
+        @click="toggleSidebarFunction">
+        <span class="text-lg">
+          <Icon :name="toggleSidebar ? 'fxemoji:eye' : 'nimbus:eye-off'" />
+        </span>
+      </button>
+    </div>
+
+    <!-- Main Content -->
+    <main
+      ref="mainContentRef"
+      class="flex flex-col items-center bg-base-200 rounded-2xl p-2 m-2 border">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useHead } from '@vueuse/head'
+import { useErrorStore } from '@/stores/errorStore'
+import { useTagStore } from '@/stores/tagStore'
+import { useUserStore } from '@/stores/userStore'
+import { useArtStore } from '@/stores/artStore'
+import { useThemeStore } from '@/stores/themeStore'
+import { useBotStore } from '@/stores/botStore'
+import { usePitchStore } from '@/stores/pitchStore'
+import { useChannelStore } from '@/stores/channelStore'
+import { useMilestoneStore } from '@/stores/milestoneStore'
+import { useLayoutStore } from '@/stores/layoutStore'
+
+const errorStore = useErrorStore()
+const tagStore = useTagStore()
+const userStore = useUserStore()
+const artStore = useArtStore()
+const themeStore = useThemeStore()
+const botStore = useBotStore()
+const pitchStore = usePitchStore()
+const channelStore = useChannelStore()
+const milestoneStore = useMilestoneStore()
+const layoutStore = useLayoutStore()
+
+useHead({
+  title: 'Kind Robots',
+  meta: [
+    { name: 'og:title', content: 'Welcome to the Kind Robots' },
+    {
+      name: 'description',
+      content: 'OpenAI-supported Promptbots here to assist humanity.'
+    },
+    {
+      name: 'og:description',
+      content:
+        'Make and Share OpenAI prompts, AI-assisted art, and find the secret jellybeans'
+    },
+    { name: 'og:image', content: '/images/kindtitle.webp' },
+    { name: 'twitter:card', content: 'summary_large_image' }
+  ]
+})
+
+const headerRef = ref<HTMLElement | null>(null)
+const mainContentRef = ref<HTMLElement | null>(null)
+const toggleSidebar = ref(true)
+
+const toggleSidebarFunction = () => {
+  toggleSidebar.value = !toggleSidebar.value
+  console.log('Toggle Sidebar:', toggleSidebar.value) // Debug: Check the state change
+}
+
+onMounted(async () => {
+  try {
+    await botStore.loadStore()
+    console.log('loading user')
+    await userStore.initializeUser()
+    console.log('user loaded')
+    await artStore.init()
+    await tagStore.initializeTags()
+    await themeStore.initTheme()
+    await pitchStore.initializePitches()
+    await channelStore.initializeChannels()
+    await milestoneStore.initializeMilestones()
+    await layoutStore.initializeStore()
+    console.log('Initialization complete.')
+  } catch (error: unknown) {
+    errorStore.setError(
+      ErrorType.UNKNOWN_ERROR,
+      `Initialization failed: ${
+        error instanceof Error ? error.message : String(error)
+      }`
+    )
+  }
+})
+</script>
+
+<style scoped>
+button {
+  font-size: 1.5rem;
+  width: 3rem;
+  height: 3rem;
+}
+</style>

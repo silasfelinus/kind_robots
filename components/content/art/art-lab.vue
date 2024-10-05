969abe19e0c9240b7640dccd2681a2f8e7e3c3ef
Silas Knight <silasfelinus@gmail.com>
2024-10-05 14:28:31 -0700

    consistent screen styling

--- components/content/art/art-lab.vue
+++ components/content/art/art-lab.vue
@@ -1,37 +1,67 @@
 <template>
-  <div class="art-lab-container flex flex-col z-10" :style="{ height: displayStore.mainHeight }">
+  <div
+    class="art-lab-container flex flex-col items-center min-h-screen w-full overflow-hidden p-4"
+  >
     <!-- Art Section Buttons (Fixed at the top) -->
-    <div class="flex justify-center space-x-2 sm:space-x-1 md:space-x-4 lg:space-x-6 mb-4">
-      <button class="btn btn-primary border-accent border-1 px-4 py-2 sm:px-2 sm:py-1 md:px-3 lg:px-6" @click="selectArtSection('art-maker')">
+    <div
+      class="flex justify-center space-x-1 md:space-x-3 lg:space-x-5 w-full mb-3"
+    >
+      <button
+        class="px-2 md:px-4 lg:px-6 text-lg font-semibold border-accent rounded-lg"
+        @click="selectArtSection('art-maker')"
+      >
         Art Maker
       </button>
 
-      <button class="btn  btn-primary border-accent border-1 px-4 py-2 sm:px-2 sm:py-1 md:px-3 lg:px-6" @click="selectArtSection('art-collection')">
+      <button
+        class="px-2 md:px-4 lg:px-6 text-lg font-semibold border-accent rounded-lg"
+        @click="selectArtSection('art-collection')"
+      >
         Art Collection
       </button>
 
-      <button class="btn  btn-primary border-accent border-1 px-4 py-2 sm:px-2 sm:py-1 md:px-3 lg:px-6" @click="selectArtSection('art-gallery')">
+      <button
+        class="px-2 md:px-4 lg:px-6 text-lg font-semibold border-accent rounded-lg"
+        @click="selectArtSection('art-gallery')"
+      >
         Art Gallery
       </button>
 
-      <button class="btn  btn-primary border-accent border-1 px-4 py-2 sm:px-2 sm:py-1 md:px-3 lg:px-6" @click="selectArtSection('hot-or-not')">
+      <button
+        class="px-2 md:px-4 lg:px-6 text-lg font-semibold border-accent rounded-lg"
+        @click="selectArtSection('hot-or-not')"
+      >
         Hot or Not
       </button>
     </div>
 
     <!-- Art Sections -->
-    <div class="art-sections flex-grow overflow-y-auto">
+    <div
+      class="art-sections flex-grow w-full max-w-4xl overflow-y-auto p-2 md:p-4 lg:p-6"
+    >
       <!-- Art Maker Screen -->
-      <lazy-art-maker v-if="selectedArtSection === 'art-maker'" @close="handleSectionClose"></lazy-art-maker>
+      <lazy-art-maker
+        v-if="selectedArtSection === 'art-maker'"
+        @close="handleSectionClose"
+      />
 
       <!-- Art Collection Screen -->
-      <lazy-art-collection v-if="selectedArtSection === 'art-collection'" @close="handleSectionClose"></lazy-art-collection>
+      <lazy-art-collection
+        v-if="selectedArtSection === 'art-collection'"
+        @close="handleSectionClose"
+      />
 
       <!-- Art Gallery Screen -->
-      <lazy-art-gallery v-if="selectedArtSection === 'art-gallery'" @close="handleSectionClose"></lazy-art-gallery>
+      <lazy-art-gallery
+        v-if="selectedArtSection === 'art-gallery'"
+        @close="handleSectionClose"
+      />
 
       <!-- Hot or Not Screen -->
-      <lazy-hot-or-not v-if="selectedArtSection === 'hot-or-not'" @close="handleSectionClose"></lazy-hot-or-not>
+      <lazy-hot-or-not
+        v-if="selectedArtSection === 'hot-or-not'"
+        @close="handleSectionClose"
+      />
     </div>
 
     <!-- Debug Message -->
@@ -43,14 +73,10 @@
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
-import { useDisplayStore } from './../../../stores/displayStore'
 
 // State variables
 const debugMessage = ref<string | null>(null) // For debugging the section selection
 const selectedArtSection = ref<string | null>(null) // Track selected section (art-maker, art-collection, art-gallery, hot-or-not)
-
-// Access the display store
-const displayStore = useDisplayStore()
 
 // Watch for selected section and update the debug message
 watch(
@@ -70,36 +96,3 @@ const selectArtSection = (section: string) => {
   selectedArtSection.value = section
 }
 </script>
-
-<style scoped>
-.art-lab-container {
-  width: 100%;
-  overflow: hidden;
-}
-
-/* Art Sections Padding and Overflow */
-.art-sections {
-  height: 100%; /* Ensures it respects the parent's height */
-  overflow-y: auto; /* Allow scrolling inside */
-  padding: 1rem;
-}
-
-/* Responsive Adjustments for Padding */
-@media (max-width: 600px) {
-  .art-sections {
-    padding: 0.5rem;
-  }
-}
-
-@media (min-width: 768px) {
-  .art-sections {
-    padding: 1.5rem;
-  }
-}
-
-@media (min-width: 1024px) {
-  .art-sections {
-    padding: 2rem;
-  }
-}
-</style>

49afd4fa8dc8854d1e22ff188fbc7de7a59f6309
Silas Knight <silasfelinus@gmail.com>
2024-10-05 19:16:11 -0700

    tighter banner conditions based on title and subtitle sizes

--- components/content/layout/KindBanner.vue
+++ components/content/layout/KindBanner.vue
@@ -1,17 +1,22 @@
 <template>
   <div
-    class="banner-container flex flex-col lg:flex-row items-center justify-center h-[10vh] lg:h-[15vh] border-accent border bg-primary text-white px-4 py-3 rounded-lg shadow-lg w-full mb-6"
+    class="banner-container flex flex-col lg:flex-row items-center justify-center border-accent border bg-primary text-white px-4 py-3 rounded-lg shadow-lg w-full mb-6"
   >
     <!-- Display the image if it exists, otherwise use default -->
-    <img
-      v-if="page && page.image"
-      :src="'/images/' + page.image"
-      alt="Page banner image"
-      class="banner-image object-cover rounded-lg h-[9vh] lg:h-[14vh] mb-4 lg:mb-0 lg:mr-4"
-    />
+    <div class="flex-shrink-0 h-auto">
+      <img
+        v-if="page && page.image"
+        :src="'/images/' + page.image"
+        alt="Page banner image"
+        class="object-contain rounded-lg max-h-full"
+        :style="{ height: '100%', width: 'auto' }"
+      />
+    </div>
 
     <!-- Title and Subtitle -->
-    <div class="flex flex-col justify-center text-center lg:text-left lg:w-3/4">
+    <div
+      class="flex flex-col justify-center text-center lg:text-left lg:w-3/4 ml-4"
+    >
       <h1 class="text-2xl md:text-4xl font-extrabold">
         {{ page.title || 'Kind Robots' }}
       </h1>

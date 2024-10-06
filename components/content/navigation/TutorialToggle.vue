3ec1e8a75ea1fdbed4e4e78ee855e63f011ed65f
Silas M Knight <silasfelinus@gmail.com>
2024-10-05 20:48:36 -0700

    Update TutorialToggle.vue

---
+++ components/content/navigation/TutorialToggle.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    v-if="!isFullScreen"
+    class="fixed top-30 right-4 bg-primary text-base-200 rounded-lg shadow-md hover:bg-accent transition duration-300 z-50 p-1"
+    @click="toggleTutorial"
+  >
+    Show {{ showTutorial ? 'Nuxt Page' : 'Tutorial' }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useDisplayStore } from '@/stores/displayStore'
+
+const displayStore = useDisplayStore()
+
+const isFullScreen = computed(() => displayStore.isFullScreen)
+
+const showTutorial = computed(() => displayStore.showTutorial)
+
+const toggleTutorial = () => {
+  displayStore.toggleTutorial()
+}
+</script>
+
+<style scoped>
+button {
+  pointer-events: auto;
+}
+</style>

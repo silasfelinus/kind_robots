7c5a5d6f9c50749cd2469760a4cfe6dbe7cfca17
Silas Knight <silasfelinus@gmail.com>
2024-10-05 18:13:23 -0700

    banner smaller image on large monitors

--- components/content/layout/KindBanner.vue
+++ components/content/layout/KindBanner.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="banner-container flex flex-col lg:flex-row items-center h-1/4 border-accent border-1 bg-primary text-white px-4 py-3 rounded-lg shadow-lg w-full mb-6"
+    class="banner-container flex flex-col lg:flex-row items-center justify-center h-[15vh] lg:h-[20vh] border-accent border bg-primary text-white px-4 py-3 rounded-lg shadow-lg w-full mb-6"
   >
     <!-- Display the image if it exists, otherwise use default -->
     <img
       v-if="page && page.image"
       :src="'/images/' + page.image"
       alt="Page banner image"
-      class="banner-image object-cover rounded-lg mb-4 lg:mb-0 lg:mr-4"
+      class="banner-image object-cover rounded-lg lg:w-1/4 w-3/4 mb-4 lg:mb-0 lg:mr-4"
     />
 
     <!-- Title and Subtitle -->
-    <div class="flex flex-col justify-center w-full text-center lg:text-left">
+    <div class="flex flex-col justify-center text-center lg:text-left lg:w-3/4">
       <h1 class="text-2xl md:text-4xl font-extrabold">
         {{ page.title || 'Kind Robots' }}
       </h1>
@@ -25,25 +25,3 @@
 <script setup>
 const { page } = useContent()
 </script>
-
-<style scoped>
-.banner-container {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-}
-
-/* Image adjusts based on container height and keeps natural aspect ratio */
-.banner-image {
-  height: 100%;
-  width: auto; /* This keeps the width responsive based on height */
-  border-radius: 0.5rem;
-}
-
-@media (min-width: 768px) {
-  .banner-image {
-    max-width: auto;
-  }
-}
-</style>

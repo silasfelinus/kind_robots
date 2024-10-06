e83646724b22d2da166e39da8cbb850559e0f081
Silas M Knight <silasfelinus@gmail.com>
2024-10-06 06:06:23 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -15,7 +15,7 @@
     >
       <!-- Header -->
       <div
-        class="bg-base-100 flex items-left fixed justify-between w-full p-2 z-30"
+        class="bg-base-100 flex items-left justify-between w-full p-2 z-30"
         :style="{ height: headerHeight }"
       >
         <!-- Sidebar Toggle -->
@@ -25,7 +25,7 @@
         <header-upgrade />
       </div>
 
-      <!-- Sidebar left (Fixed) -->
+      <!-- Sidebar left (Fixed, no scrolling) -->
       <kind-sidebar-simple
         class="bg-base-100 fixed top-0 left-0 z-10"
         :style="{
@@ -35,27 +35,26 @@
         }"
       ></kind-sidebar-simple>
 
-      <!-- Main Content (Scrollable within its boundaries) -->
+      <!-- Main Content (Scrollable only if needed) -->
       <main
-        :class="{ 'flip-card': !isFullScreen && !isMobile }"
-        class="bg-base-100 p-2 rounded-2xl z-10 overflow-hidden" 
+        class="bg-base-100 p-2 rounded-2xl z-10 overflow-hidden"
         :style="{
           gridColumn: '2 / 3',
           height: `calc(100vh - ${headerHeight} - ${footerHeight})`,
         }"
       >
-        <div class="h-full overflow-y-auto">
+        <div class="h-full">
           <!-- Mobile View (no flip card) -->
           <div v-if="isMobile">
             <SplashTutorial
               v-if="showTutorial"
               class="h-full w-full z-10 rounded-2xl"
             />
-            <NuxtPage v-else class="h-full w-full z-10 rounded-2xl" />
+            <NuxtPage v-else class="h-full w-full z-10 overflow-y-auto rounded-2xl" />
           </div>
 
           <!-- Fullscreen mode (Desktop) -->
-          <div v-else-if="isFullScreen" class="h-full rounded-2xl z-10">
+          <div v-else-if="isFullScreen" class="h-full rounded-2xl z-10 overflow-y-auto">
             <NuxtPage class="h-full w-full" />
           </div>
 
@@ -76,9 +75,9 @@
         </div>
       </main>
 
-      <!-- Sidebar right (Fixed) -->
+      <!-- Sidebar right (Scrollable if content overflows) -->
       <aside
-        class="bg-base-100 fixed top-0 right-0 z-20"
+        class="bg-base-100 fixed top-0 right-0 z-20 overflow-y-auto"
         :style="{
           width: sidebarRightWidth,
           height: `calc(100vh - ${headerHeight} - ${footerHeight})`,
@@ -91,7 +90,7 @@
         </div>
       </aside>
 
-      <!-- Footer -->
+      <!-- Footer (Static, no scrolling) -->
       <footer
         class="flex justify-center items-center"
         :style="{ gridRow: '3 / 4', height: footerHeight }"

c5f9930700f17c5af24b9db0bca528c4482d8fc4
Silas M Knight <silasfelinus@gmail.com>
2024-10-05 20:57:44 -0700

    Update TutorialToggle.vue

--- components/content/navigation/TutorialToggle.vue
+++ components/content/navigation/TutorialToggle.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="fixed top-30 right-4 z-50 p-1 rounded-lg shadow-md bg-primary hover:bg-accent transition duration-300 cursor-pointer"
+    class="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-primary hover:bg-accent transition duration-300 cursor-pointer rounded-lg shadow-md p-1"
+    :style="{ bottom: footerHeight }"
     @click="toggleTutorial"
   >
-    <!-- Swap icons based on the showTutorial state -->
     <Icon
       v-if="showTutorial"
-      name="mdi-information-outline" 
+      name="mdi-information-outline"
       class="text-base-200 w-6 h-6"
     />
     <Icon
       v-else
-      :name="pageIcon" 
+      :name="pageIcon"
       class="text-base-200 w-6 h-6"
     />
   </div>
@@ -20,6 +20,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useDisplayStore } from '@/stores/displayStore'
+import { useContent } from '@/composables/useContent'
 
 // Access the display store
 const displayStore = useDisplayStore()
@@ -27,6 +28,10 @@ const displayStore = useDisplayStore()
 // Access the content, including page.icon
 const { page } = useContent()
 
+// Footer height from displayStore
+const footerHeight = computed(() => displayStore.footerHeight)
+
+// Compute the tutorial and icon state
 const showTutorial = computed(() => displayStore.showTutorial)
 
 // Toggle the tutorial state
@@ -35,12 +40,12 @@ const toggleTutorial = () => {
 }
 
 // Use a computed value to get the page icon from useContent
-const pageIcon = computed(() => page.icon || 'mdi-page-layout-sidebar-right') // Default to an icon if page.icon is missing
+const pageIcon = computed(() => page.icon || 'mdi-page-layout-sidebar-right')
 </script>
 
 <style scoped>
-/* Add styles for the clickable icon */
 div {
   display: inline-block;
+  position: absolute;
 }
 </style>

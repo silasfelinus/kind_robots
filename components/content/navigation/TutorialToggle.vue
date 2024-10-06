2c73b8e2076573e275a02902ec5efd34f39914d8
Silas M Knight <silasfelinus@gmail.com>
2024-10-06 01:26:31 -0700

    Update TutorialToggle.vue

--- components/content/navigation/TutorialToggle.vue
+++ components/content/navigation/TutorialToggle.vue
@@ -1,36 +1,26 @@
 <template>
-  <div class="relative h-full w-full">
-    <!-- Other Main Content -->
-
-    <!-- Tutorial Toggle: Centered at the bottom of the main content -->
-    <div
-      class="flex justify-center items-center w-full"
-      :style="{ paddingBottom: footerHeight }"
-    >
-      <div
-        class="bg-primary hover:bg-accent transition duration-300 cursor-pointer rounded-lg shadow-md p-2 flex items-center justify-center"
-        @click="toggleTutorial"
-      >
-        <Icon
-          v-if="showTutorial"
-          name="mdi-information-outline"
-          class="text-base-200 w-6 h-6"
-        />
-        <Icon
-          v-else
-          :name="pageIcon"
-          class="text-base-200 w-6 h-6"
-        />
-      </div>
-    </div>
+  <div
+    class="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-primary hover:bg-accent transition duration-300 cursor-pointer rounded-lg shadow-md p-1"
+    :style="{ bottom: footerHeight }"
+    @click="toggleTutorial"
+  >
+    <Icon
+      v-if="showTutorial"
+      name="mdi-information-outline"
+      class="text-base-200 w-6 h-6"
+    />
+    <Icon
+      v-else
+      :name="pageIcon"
+      class="text-base-200 w-6 h-6"
+    />
   </div>
 </template>
-
-
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useDisplayStore } from '@/stores/displayStore'
+
 
 // Access the display store
 const displayStore = useDisplayStore()
@@ -38,19 +28,11 @@ const displayStore = useDisplayStore()
 // Access the content, including page.icon
 const { page } = useContent()
 
-// Fullscreen and tutorial state
-const showTutorial = computed(() => displayStore.showTutorial)
-const isFullScreen = computed(() => displayStore.isFullScreen)
-
-// Layout dimensions
-const headerHeight = computed(() => displayStore.headerHeight)
-const mainHeight = computed(() => displayStore.mainHeight)
+// Footer height from displayStore
 const footerHeight = computed(() => displayStore.footerHeight)
-const sidebarLeftWidth = computed(() => displayStore.sidebarLeftWidth)
-const sidebarRightWidth = computed(() => displayStore.sidebarRightWidth)
 
-// Mobile detection
-const isMobile = computed(() => displayStore.isMobileViewport)
+// Compute the tutorial and icon state
+const showTutorial = computed(() => displayStore.showTutorial)
 
 // Toggle the tutorial state
 const toggleTutorial = () => {
@@ -58,10 +40,7 @@ const toggleTutorial = () => {
 }
 
 // Use a computed value to get the page icon from useContent
-// Ensure page.icon is a string and provide a fallback to avoid the startsWith error
-const pageIcon = computed(() => {
-  return typeof page.icon === 'string' ? page.icon : 'mdi-page-layout-sidebar-right'
-})
+const pageIcon = computed(() => page.icon || 'mdi-page-layout-sidebar-right')
 </script>
 
 <style scoped>

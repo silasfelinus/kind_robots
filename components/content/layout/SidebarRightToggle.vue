57aee9bd860f5496e7bea4e7f4c343193e0a6445
Silas M Knight <silasfelinus@gmail.com>
2024-10-06 17:45:10 -0700

    Update SidebarRightToggle.vue

--- components/content/layout/SidebarRightToggle.vue
+++ components/content/layout/SidebarRightToggle.vue
@@ -1,11 +1,13 @@
 <template>
-  <button
-    class="flex items-center justify-center p-1 hover:bg-secondary rounded-lg"
-    :style="buttonStyle"
-    @click="toggleSidebarRight"
-  >
-    <Icon name="carbon:tool-box" class="toggle-icon" />
-  </button>
+  <div>
+    <!-- Toggle button for larger displays (controlled by parent container) -->
+    <button
+      class="flex items-center justify-center p-2 bg-accent text-white rounded-lg shadow-lg hover:bg-secondary"
+      @click="toggleSidebarRight"
+    >
+      <Icon :name="iconName" class="w-6 h-6" />
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -14,31 +16,24 @@ import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
 
-// Compute the icon size based on the header height
-const buttonStyle = computed(() => {
-  const iconSize = Math.max(5, displayStore.headerVh - 3)
-  return {
-    width: `${iconSize}vh`,
-    height: `${iconSize}vh`,
+// Determine the icon based on the sidebarRightState (open, hidden)
+const iconName = computed(() => {
+  switch (displayStore.sidebarRightState) {
+    case 'hidden':
+      return 'mdi:menu-right'
+    case 'open':
+      return 'mdi:chevron-right'
+    default:
+      return 'mdi:menu-right'
   }
 })
 
-// Toggle the left sidebar
+// Toggle the right sidebar
 const toggleSidebarRight = () => {
   displayStore.toggleSidebar('sidebarRightState')
 }
 </script>
 
 <style scoped>
-.toggle-icon {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-
-button {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+/* No additional styles needed, using Tailwind CSS classes */
 </style>

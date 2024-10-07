e734e08643c876300969297c1a21c478949b0721
Silas Knight <silasfelinus@gmail.com>
2024-10-07 01:45:02 -0700

    consistent toggle activity

--- components/content/layout/MainContent.vue
+++ components/content/layout/MainContent.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="relative h-full flex flex-col">
-    <!-- Left Sidebar Toggle -->
-
-    <!-- Right Sidebar Toggle (Mobile: Also toggles tutorial) -->
+    <!-- Right Sidebar Toggle (Mobile & Desktop) -->
     <div
       class="absolute right-0 top-1/2 transform -translate-y-1/2 z-20 p-2 bg-secondary text-white rounded-l-lg cursor-pointer"
-      @click="isMobile ? toggleTutorial() : toggleRightSidebar()"
+      @click="toggleSidebarAndTutorial"
     >
-      <span v-if="displayStore.sidebarRightState === 'open' && !isMobile">
+      <!-- Arrow icon for desktop -->
+      <span v-if="!isMobile && displayStore.sidebarRightState === 'open'">
         &#8250;
       </span>
-      <span v-if="displayStore.sidebarRightState === 'hidden' && !isMobile">
+      <span v-if="!isMobile && displayStore.sidebarRightState === 'hidden'">
         &#8249;
       </span>
-      <span v-if="isMobile">
-        &#x1F4D6;
-        <!-- Tutorial icon for mobile -->
-      </span>
+      <!-- Book icon for mobile (represents tutorial toggle) -->
+      <span v-if="isMobile"> &#x1F4D6; </span>
     </div>
 
-    <!-- Mobile View (no flip card) -->
+    <!-- Mobile View (Tutorial or Content) -->
     <div v-if="isMobile" class="flex-grow overflow-y-auto">
       <SplashTutorial
         v-if="showTutorial"
@@ -28,7 +25,7 @@
       <NuxtPage v-else class="h-full w-full z-10 rounded-2xl" />
     </div>
 
-    <!-- Fullscreen mode (Desktop) -->
+    <!-- Fullscreen Mode (Desktop, Content Only) -->
     <div
       v-else-if="isFullScreen"
       class="h-full w-full overflow-y-auto rounded-2xl z-10 flex-grow"
@@ -36,18 +33,23 @@
       <NuxtPage class="h-full w-full" />
     </div>
 
-    <!-- Flip-card mode (Desktop) -->
-    <div
-      v-else
-      class="flip-card-inner h-full z-10 flex-grow"
-      :class="{ 'is-flipped': !showTutorial }"
-    >
-      <div class="flip-card-front rounded-2xl h-full w-full">
-        <SplashTutorial class="h-full w-full" />
-      </div>
+    <!-- Flip-card Mode (Desktop with Sidebar for Tutorial) -->
+    <div v-else class="relative flex-grow z-10">
+      <!-- Main Content (NuxtPage) -->
       <div class="flip-card-back rounded-2xl overflow-y-auto h-full w-full">
         <NuxtPage class="h-full w-full" />
       </div>
+
+      <!-- Right Sidebar (Tutorial) -->
+      <aside
+        class="bg-secondary fixed top-0 right-0 h-full transition-all duration-500 ease-in-out"
+        :class="{
+          'w-0': displayStore.sidebarRightState === 'hidden',
+          'w-80': displayStore.sidebarRightState === 'open',
+        }"
+      >
+        <SplashTutorial class="h-full w-full" />
+      </aside>
     </div>
   </div>
 </template>
@@ -64,15 +66,14 @@ const isMobile = computed(() => displayStore.isMobileViewport)
 const isFullScreen = computed(() => displayStore.isFullScreen)
 const showTutorial = computed(() => displayStore.showTutorial)
 
-// Function to toggle the right sidebar
-const toggleRightSidebar = () => {
+// Function to toggle both the right sidebar and the tutorial
+const toggleSidebarAndTutorial = () => {
+  // Toggle the tutorial visibility
+  displayStore.showTutorial = !displayStore.showTutorial
+
+  // Toggle the sidebar state
   displayStore.sidebarRightState =
     displayStore.sidebarRightState === 'open' ? 'hidden' : 'open'
-}
-
-// Function to toggle the tutorial
-const toggleTutorial = () => {
-  displayStore.showTutorial = !displayStore.showTutorial
 }
 </script>
 
@@ -92,10 +93,6 @@ const toggleTutorial = () => {
   transform-style: preserve-3d;
 }
 
-.flip-card-inner.is-flipped {
-  transform: rotateY(180deg);
-}
-
 .flip-card-front,
 .flip-card-back {
   position: absolute;
@@ -112,5 +109,18 @@ const toggleTutorial = () => {
 .flip-card-back {
   transform: rotateY(180deg);
   z-index: 1;
+}
+
+/* Sidebar transition and width handling */
+aside {
+  width: 80px; /* Default closed width */
+}
+
+aside.w-0 {
+  width: 0;
+}
+
+aside.w-80 {
+  width: 20rem; /* Opened sidebar width */
 }
 </style>

50a3547194b852f0960415bf0808c11ac934adfb
Silas Knight <silasfelinus@gmail.com>
2024-10-08 15:23:54 -0700

    adjusting icons

--- components/content/screenfx/ButterflyToggle.vue
+++ components/content/screenfx/ButterflyToggle.vue
@@ -2,6 +2,7 @@
   <div
     class="Icon-container flex items-center justify-center space-x-1 rounded-lg bg-accent-100 flex-col"
   >
+    <!-- Butterfly Icon Box -->
     <div
       class="Icon-box transition-transform transform hover:scale-125 cursor-pointer rounded-full hover:bg-accent-200"
       @click="toggleAmiSwarm"
@@ -11,9 +12,11 @@
         title="Kind Butterflies"
         :active="fxStore.showAmiSwarm"
         :class="{ glow: fxStore.showAmiSwarm }"
-        class="w-6 h-6 md:w-12 md:h-12 cursor-pointer transform transition-transform ease-in-out hover:scale-110"
+        class="w-8 h-8 cursor-pointer transform transition-transform ease-in-out hover:scale-110"
       />
     </div>
+
+    <!-- Butterfly Swarm Animation -->
     <div v-if="fxStore.showAmiSwarm">
       <ami-butterfly
         v-for="i in butterflyCount"
@@ -22,13 +25,15 @@
       />
     </div>
 
-    <!-- Moved the label below the Icon -->
+    <!-- Label Below Icon -->
     <div
       v-if="!fxStore.showAmiSwarm"
-      class="label-container mt-1 text-xl text-default font-bold"
-    />
-    <div v-else class="label-container mt-1 text-xl text-default font-bold">
-      We're free!
+      class="label-container mt-1 text-default font-bold"
+    >
+      Click Me!
+    </div>
+    <div v-else class="label-container mt-1 text-default font-bold">
+      We're Free!
     </div>
   </div>
 </template>
@@ -43,7 +48,7 @@ const toggleAmiSwarm = () => fxStore.toggleAmiSwarm()
 const butterflyCount = 15
 </script>
 
-<style>
+<style scoped>
 .Icon-box {
   border-radius: 50%;
 }
@@ -51,7 +56,6 @@ const butterflyCount = 15
 .glow {
   animation: glow 1s ease-in-out infinite alternate;
   border-radius: 50%;
-  /* Add this line to apply the delay */
   animation-delay: var(--animation-delay, 0s);
 }
 
@@ -76,11 +80,6 @@ const butterflyCount = 15
   font-size: 1rem;
   display: flex;
   align-items: center;
-  color: bg-accent-100; /* Adjust color to fit your design */
-}
-
-.arrow {
-  margin-right: 5px;
-  font-weight: bold;
+  color: var(--text-accent);
 }
 </style>

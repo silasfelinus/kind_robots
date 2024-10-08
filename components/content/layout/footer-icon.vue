8ada8362a04f2913a8ae168b180c664c83545a4d
Silas Knight <silasfelinus@gmail.com>
2024-10-08 15:17:04 -0700

    footer icon has nav, app vue shows toggle again

--- components/content/layout/footer-icon.vue
+++ components/content/layout/footer-icon.vue
@@ -3,7 +3,11 @@
     class="footer-toggle fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-secondary text-accent p-2 rounded-full opacity-75 cursor-pointer z-50"
     @click="toggleFooter"
   >
-    <span v-if="isFooterOpen">&#x25BC;</span>
+    <span v-if="isFooterOpen"
+      >&#x25BC;
+      <horizontal-nav />
+    </span>
+
     <span v-else>&#x25B2;</span>
   </div>
 </template>

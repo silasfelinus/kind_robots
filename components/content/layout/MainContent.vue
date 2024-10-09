d9c9a6d692caddfe66df728e9159edbdc8cc0ed2
Silas Knight <silasfelinus@gmail.com>
2024-10-08 23:24:45 -0700

    sidebars ignore footer, toggle moved to corner

--- components/content/layout/MainContent.vue
+++ components/content/layout/MainContent.vue
@@ -2,17 +2,16 @@
   <div class="relative h-full flex flex-col">
     <!-- Main Content Area -->
     <div class="relative flex-grow h-full flex flex-col">
-      
       <!-- Fullscreen Toggle (Top Center) -->
       <fullscreen-toggle
-        class="absolute top-2 left-1/2 transform -translate-x-1/2 w-8 h-8 z-20 text-accent cursor-pointer"
+        class="absolute top-2 right-2 transform -translate-x-1/2 w-8 h-8 z-20 text-accent cursor-pointer"
       />
 
       <!-- Left Toggle Button (inside content) -->
       <left-toggle
         class="absolute left-2 top-1/2 transform -translate-y-1/2 w-8 h-8 z-20 text-accent cursor-pointer"
       />
-      
+
       <!-- Right Sidebar Toggle (inside content) -->
       <right-toggle
         class="absolute right-2 top-1/2 transform -translate-y-1/2 w-8 h-8 z-20 text-accent cursor-pointer"
@@ -21,12 +20,18 @@
 
       <!-- Main Content (Tutorial or Content) -->
       <div v-if="isMobile" class="flex-grow overflow-y-auto">
-        <SplashTutorial v-if="showTutorial" class="h-full w-full z-10 rounded-2xl" />
+        <SplashTutorial
+          v-if="showTutorial"
+          class="h-full w-full z-10 rounded-2xl"
+        />
         <NuxtPage v-else class="h-full w-full z-10 rounded-2xl" />
       </div>
 
       <!-- Fullscreen Mode (Desktop, Content Only) -->
-      <div v-else-if="isFullScreen" class="h-full w-full overflow-y-auto rounded-2xl z-10 flex-grow">
+      <div
+        v-else-if="isFullScreen"
+        class="h-full w-full overflow-y-auto rounded-2xl z-10 flex-grow"
+      >
         <NuxtPage class="h-full w-full" />
       </div>
 
@@ -35,12 +40,16 @@
         <div class="flip-card flex-grow">
           <div class="flip-card-inner" :class="{ flipped: showTutorial }">
             <!-- Main Content (NuxtPage) -->
-            <div class="flip-card-front rounded-2xl overflow-y-auto h-full w-full">
+            <div
+              class="flip-card-front rounded-2xl overflow-y-auto h-full w-full"
+            >
               <NuxtPage class="h-full w-full" />
             </div>
 
             <!-- Splash Tutorial -->
-            <div class="flip-card-back rounded-2xl overflow-y-auto h-full w-full">
+            <div
+              class="flip-card-back rounded-2xl overflow-y-auto h-full w-full"
+            >
               <SplashTutorial class="h-full w-full" />
             </div>
           </div>

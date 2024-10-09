b753f1f7e258691168e9beedb100a39344a2b961
Silas Knight <silasfelinus@gmail.com>
2024-10-08 23:10:25 -0700

    removed safelist, testing toggle logs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,41 +4,6 @@ export default {
     './pages/**/*.{html,ts,js,vue}',
     './components/**/*.{html,ts,js,vue}',
   ],
-  safelist: [
-    {
-      pattern: /line-md:home-md-twotone/,
-    },
-    {
-      pattern: /fluent:bot-add-20-regular/,
-    },
-    {
-      pattern: /fluent:chat-bubbles-question-16-regular/,
-    },
-    {
-      pattern: /game-icons:easel/,
-    },
-    {
-      pattern: /mdi:brain/,
-    },
-    {
-      pattern: /game-icons:gear-hammer/,
-    },
-    {
-      pattern: /mingcute:settings-6-fill/,
-    },
-    {
-      pattern: /emojione:butterfly/,
-    },
-    {
-      pattern: /mdi-brush/,
-    },
-    {
-      pattern: /mdi:chevron-left/,
-    },
-    {
-      pattern: /mdi:chevron-right/,
-    },
-  ],
   theme: {
     extend: {},
   },

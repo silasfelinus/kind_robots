e5f30ce6443f22333781a464e6f5acacb3133876
Silas Knight <silasfelinus@gmail.com>
2024-10-08 17:31:44 -0700

    safelisted icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  jit: true,
   content: [
     './pages/**/*.{html,ts,js,vue}',
     './components/**/*.{html,ts,js,vue}',
+  ],
+  safelist: [
+    {
+      pattern: /line-md:home-md-twotone/,
+    },
+    {
+      pattern: /fluent:bot-add-20-regular/,
+    },
+    {
+      pattern: /fluent:chat-bubbles-question-16-regular/,
+    },
+    {
+      pattern: /game-icons:easel/,
+    },
+    {
+      pattern: /material-symbols:art-track-outline-rounded/,
+    },
+    {
+      pattern: /game-icons:gear-hammer/,
+    },
+    {
+      pattern: /mingcute:settings-6-fill/,
+    },
+    {
+      pattern: /emojione:butterfly/,
+    },
+    {
+      pattern: /mdi-brush/,
+    },
   ],
   theme: {
     extend: {},

573b122ff1a220dd824fa3bcfff92ce7e4d3fe9e
Silas M Knight <silasfelinus@gmail.com>
2024-10-08 20:39:38 -0700

    Update RightToggle.vue

--- components/content/layout/RightToggle.vue
+++ components/content/layout/RightToggle.vue
@@ -2,10 +2,10 @@
   <div>
     <!-- Toggle button for larger displays (controlled by parent container) -->
     <button
-      class="flex items-center justify-center p-2 bg-accent text-white rounded-lg shadow-lg hover:bg-secondary"
-      @click="toggleSidebarRight"
+      class="flex items-center justify-center p-2 text-accent rounded-lg shadow-lg hover:bg-secondary"
+      @click="toggleSidebarRightAndTutorial"
     >
-      <Icon :name="iconName" class="w-6 h-6" />
+      <Icon :name="iconName" class="w-8 h-8" />
     </button>
   </div>
 </template>
@@ -16,21 +16,18 @@ import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
 
-// Determine the icon based on the sidebarRightState (open, hidden)
+// Determine the icon based on the sidebarRightState (open or closed)
 const iconName = computed(() => {
-  switch (displayStore.sidebarRightState) {
-    case 'hidden':
-      return 'mdi:menu-right'
-    case 'open':
-      return 'mdi:chevron-right'
-    default:
-      return 'mdi:menu-right'
-  }
+  return displayStore.sidebarRightState === 'open' ? 'mdi:chevron-right' : 'mdi:chevron-left'
 })
 
-// Toggle the right sidebar
-const toggleSidebarRight = () => {
+// Function to toggle both the right sidebar and the tutorial visibility
+const toggleSidebarRightAndTutorial = () => {
+  // Toggle the right sidebar
   displayStore.toggleSidebar('sidebarRightState')
+
+  // Toggle the showTutorial state
+  displayStore.showTutorial = !displayStore.showTutorial
 }
 </script>
 

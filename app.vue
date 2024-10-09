ba63991877d232400a42cb6289e38f277435c889
Silas Knight <silasfelinus@gmail.com>
2024-10-08 17:22:49 -0700

    explicit avatar image sizing

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@
       <!-- Avatar Image, automatically fills available height -->
       <avatar-image
         alt="User Avatar"
-        class="aspect-square rounded-2xl flex-shrink-0"
+        class="aspect-square min-h-8 min-w-8 rounded-2xl flex-shrink-0"
         :style="{
           height: '100%', // Fills the available height of the header
         }"

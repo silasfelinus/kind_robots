a672ac24b074455996bb55b7bc4d09f13c1681cb
Silas Knight <silasfelinus@gmail.com>
2024-10-08 19:29:39 -0700

    4-column header

--- components/content/layout/HeaderUpgrade.vue
+++ components/content/layout/HeaderUpgrade.vue
@@ -1,13 +1,12 @@
 <template>
   <header
-    class="flex items-center justify-between fixed rounded-2xl border-1 border-accent p-2 bg-base-300 z-20 mx-2 my-2 max-w-full"
+    class="grid grid-cols-4 items-center fixed rounded-2xl border-1 border-accent p-2 bg-base-300 z-20 mx-2 my-2 max-w-full"
     :style="{
       height: displayStore.headerHeight + 'vh',
     }"
   >
-    <!-- Avatar, Title, Subtitle, and Login Button Section -->
-    <div class="flex items-center justify-between w-full">
-      <!-- Avatar Image, automatically fills available height -->
+    <!-- Column 1: Avatar Image -->
+    <div class="flex items-center justify-start">
       <avatar-image
         alt="User Avatar"
         class="aspect-square min-h-8 min-w-8 rounded-2xl flex-shrink-0"
@@ -15,35 +14,31 @@
           height: '100%', // Fills the available height of the header
         }"
       />
-      <!-- Title, Subtitle, and Login Button Column -->
-      <div
-        class="flex flex-col items-center justify-center flex-grow text-center"
-      >
-        <h1
-          class="text-[13px] md:text-[20px] lg:text-[25px] xl:text-[30px] text-primary font-semibold truncate"
-        >
-          The {{ page.title || 'Room' }} Room
-        </h1>
-        <h2
-          class="text-[12px] md:text-[15px] lg:text-[17px] xl:text-[20px] text-accent italic truncate"
-        >
-          {{ subtitle }}
-        </h2>
-      </div>
-
-      <!-- Login Button Column -->
-      <div class="ml-auto">
-        <login-button class="w-full max-w-[120px]" />
-      </div>
     </div>
 
-    <!-- Non-login Icons (Vertical on small screens, horizontal on larger screens) -->
-    <div
-      class="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2 flex-shrink-0"
-    >
+    <!-- Column 2: Title and Subtitle -->
+    <div class="flex flex-col text-center">
+      <h1
+        class="text-[13px] md:text-[20px] lg:text-[25px] xl:text-[30px] text-primary font-semibold truncate"
+      >
+        The {{ page.title || 'Room' }} Room
+      </h1>
+      <h2
+        class="text-[12px] md:text-[15px] lg:text-[17px] xl:text-[20px] text-accent italic truncate"
+      >
+        {{ subtitle }}
+      </h2>
+    </div>
+
+    <!-- Column 3: Login Button -->
+    <div class="flex justify-center">
+      <login-button class="w-full max-w-[120px]" />
+    </div>
+
+    <!-- Column 4: Icons (Theme and Butterfly) -->
+    <div class="flex justify-end items-center space-x-2">
       <!-- Theme Icon -->
       <theme-icon class="w-8 h-8" />
-
       <!-- Butterfly Toggle Icon -->
       <butterfly-toggle class="w-8 h-8" />
     </div>

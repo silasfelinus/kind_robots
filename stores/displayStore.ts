7ace45016ae8e9871bc032028c33e98ebf5dc209
Silas Knight <silasfelinus@gmail.com>
2024-10-09 02:00:30 -0700

    3x3 grid

--- stores/displayStore.ts
+++ stores/displayStore.ts
@@ -315,6 +315,7 @@ export const useDisplayStore = defineStore('display', {
         disabled: 'hidden',
       };
       this[side] = stateCycle[this[side]];
+      console.log(side + "sidebar is now " + stateCycle)
       this.saveState();
     },
 

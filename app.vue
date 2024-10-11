aa3aa6a11302b97c5bdef6bb220e0f3af2d77967
Silas M Knight <silasfelinus@gmail.com>
2024-10-11 13:28:16 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -14,13 +14,12 @@
 
     <!-- Left Sidebar -->
     <aside
-      class="fixed top-0 left-0 z-20 box-border transition-all duration-500 ease-in-out"
+      class="fixed left-0 z-20 box-border transition-all duration-500 ease-in-out"
       :class="{ 'overflow-hidden': !sidebarLeftOpen }"
       :style="{
-        height: mainHeight,
         width: sidebarLeftWidth,
-        top: headerHeight /* Align the sidebar starting under the header */,
-        bottom: footerHeight /* Align sidebar ending above the footer */,
+        top: headerHeight, 
+        bottom: footerHeight
       }"
     >
       <kind-sidebar-simple v-if="sidebarLeftOpen" />
@@ -28,9 +27,8 @@
 
     <!-- Main Content -->
     <main
-      class="relative z-10 box-border overflow-hidden transition-all duration-300"
+      class="absolute z-10 box-border overflow-hidden transition-all duration-300"
       :style="{
-        height: mainHeight,
         top: headerHeight,
         bottom: footerHeight,
         marginLeft: sidebarLeftWidth,
@@ -42,13 +40,12 @@
 
     <!-- Right Sidebar -->
     <aside
-      class="fixed top-0 right-0 z-20 box-border transition-all duration-500 ease-in-out bg-base-300"
+      class="fixed right-0 z-20 box-border transition-all duration-500 ease-in-out bg-base-300"
       :class="{ 'overflow-hidden': !showTutorial }"
       :style="{
         width: sidebarRightWidth,
-        height: mainHeight,
-        top: headerHeight /* Align the sidebar starting under the header */,
-        bottom: footerHeight /* Align sidebar ending above the footer */,
+        top: headerHeight,
+        bottom: footerHeight
       }"
     >
       <splash-tutorial v-if="showTutorial" class="h-full w-full" />
@@ -70,19 +67,15 @@ import { useDisplayStore } from '@/stores/displayStore'
 // Access the displayStore for managing the layout state
 const displayStore = useDisplayStore()
 
-// Compute header height, no need for padding since it's the full height of the element
-const headerHeight = computed(
-  () => `calc(var(--vh) * ${displayStore.headerVh})`,
-)
+// Compute header height
+const headerHeight = computed(() => `calc(var(--vh) * ${displayStore.headerVh})`)
 
 // Sidebar widths for left and right
 const sidebarLeftWidth = computed(() => displayStore.sidebarLeftWidth)
 const sidebarRightWidth = computed(() => displayStore.sidebarRightWidth)
 
 // Footer height
-const footerHeight = computed(
-  () => `calc(var(--vh) * ${displayStore.footerVh})`,
-)
+const footerHeight = computed(() => `calc(var(--vh) * ${displayStore.footerVh})`)
 
 // Control for tutorial visibility
 const showTutorial = computed(() => displayStore.showTutorial)
@@ -91,12 +84,5 @@ const showTutorial = computed(() => displayStore.showTutorial)
 const footerOpen = computed(() => displayStore.footerState === 'open')
 
 // Check if the left sidebar is open
-const sidebarLeftOpen = computed(
-  () => displayStore.sidebarLeftState !== 'hidden',
-)
-
-// Calculate the main content height dynamically, adjusting for header and footer heights
-const mainHeight = computed(() => {
-  return `calc(var(--vh) * 100 - ${headerHeight.value} - ${footerHeight.value})`
-})
+const sidebarLeftOpen = computed(() => displayStore.sidebarLeftState !== 'hidden')
 </script>

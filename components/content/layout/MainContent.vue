aeafaeff671d5307a8787fb6a4d7861e83bc164b
Silas Knight <silasfelinus@gmail.com>
2024-10-11 17:09:55 -0700

    corrected bad bot listing

--- components/content/layout/MainContent.vue
+++ components/content/layout/MainContent.vue
@@ -7,12 +7,12 @@
     >
       <!-- Left Toggle Button (inside content) -->
       <left-toggle
-        class="absolute left-1 top-1/2 transform -translate-y-1/2 w-12 h-12 z-40 text-accent cursor-pointer box-border"
+        class="absolute left-0 top-1/2 transform -translate-y-1/2 w-12 h-12 z-50 text-accent cursor-pointer box-border"
       />
 
       <!-- Right Sidebar Toggle (inside content) -->
       <right-toggle
-        class="absolute right-0 top-1/2 transform -translate-y-1/2 w-12 h-12 z-40 text-accent cursor-pointer box-border"
+        class="absolute right-0 top-1/2 transform -translate-y-1/2 w-12 h-12 z-50 text-accent cursor-pointer box-border"
       />
 
       <!-- Main Content (Tutorial or Content) -->

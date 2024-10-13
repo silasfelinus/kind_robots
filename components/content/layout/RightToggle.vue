eb39ef5954782853258bd835c029fc4be1e70f59
Silas M Knight <silasfelinus@gmail.com>
2024-10-12 20:43:45 -0700

    Update RightToggle.vue

--- components/content/layout/RightToggle.vue
+++ components/content/layout/RightToggle.vue
@@ -3,7 +3,7 @@
     <!-- Toggle button for right sidebar and tutorial -->
     <button
       class="flex items-center justify-center text-accent rounded-lg shadow-lg hover:bg-secondary text-lg h-10 w-10"
-      @click="toggleTutorialWithSidebar"
+      @click="toggleTutorialSidebar"
     >
       {{ rightIconText }}
     </button>
@@ -16,27 +16,16 @@ import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
 
-const isMobile = computed(() => displayStore.isMobileViewport)
 
 // Determine the icon text based on the sidebarRightState (hidden or open)
 const rightIconText = computed(() => {
   return displayStore.sidebarRightState === 'hidden' ? '<' : '>'
 })
 
-// Function to toggle tutorial and sidebar
-const toggleTutorialWithSidebar = () => {
-  if (!isMobile.value) {
-    if (displayStore.sidebarRightState === 'hidden') {
-      displayStore.sidebarRightState = 'open'
-      displayStore.showTutorial = true
-    } else {
-      displayStore.sidebarRightState = 'hidden'
-      displayStore.showTutorial = false
-    }
-  } else {
-    // Handle mobile-specific behavior here if necessary
-    console.log('Mobile mode, sidebar state remains unchanged.')
-  }
+
+const toggleTutorialSidebar = () => {
+displayStore.toggleTutorial()
+  
 }
 </script>
 

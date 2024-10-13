1f67e6e44f3e85acee5f30311730abd64d53f475
Silas Knight <silasfelinus@gmail.com>
2024-10-12 22:49:39 -0700

    proper icons

--- components/content/layout/LeftToggle.vue
+++ components/content/layout/LeftToggle.vue
@@ -18,11 +18,11 @@ const displayStore = useDisplayStore()
 // Determine the icon text based on the sidebarLeftState (open, compact, hidden)
 const iconText = computed(() => {
   if (displayStore.sidebarLeftState === 'open') {
-    return '<' // Icon for open state
+    return '×' // Icon for open state
   } else if (displayStore.sidebarLeftState === 'compact') {
-    return '>' // Icon for compact state
+    return '<' // Icon for compact state
   }
-  return '×' // Icon for hidden state
+  return '>' // Icon for hidden state
 })
 
 // Toggle the left sidebar between 'open', 'compact', and 'hidden'

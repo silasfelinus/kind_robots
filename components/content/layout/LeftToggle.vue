4b1efddca7647d4c0e000225b056070f9ba2b32a
Silas Knight <silasfelinus@gmail.com>
2024-10-12 21:19:56 -0700

    tinkering with  toggles

--- components/content/layout/LeftToggle.vue
+++ components/content/layout/LeftToggle.vue
@@ -15,17 +15,25 @@ import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
 
-// Determine the icon text based on the sidebarLeftState (compact, hidden, open)
+// Determine the icon text based on the sidebarLeftState (open, compact, hidden)
 const iconText = computed(() => {
-  if (displayStore.sidebarLeftState === 'hidden') {
-    return '>'
+  if (displayStore.sidebarLeftState === 'open') {
+    return '<' // Icon for open state
+  } else if (displayStore.sidebarLeftState === 'compact') {
+    return '>' // Icon for compact state
   }
-  return '<'
+  return '×' // Icon for hidden state
 })
 
-// Toggle the left sidebar
+// Toggle the left sidebar between 'open', 'compact', and 'hidden'
 const toggleSidebarLeft = () => {
-  displayStore.toggleSidebar('sidebarLeftState')
+  if (displayStore.sidebarLeftState === 'open') {
+    displayStore.sidebarLeftState = 'compact'
+  } else if (displayStore.sidebarLeftState === 'compact') {
+    displayStore.sidebarLeftState = 'hidden'
+  } else {
+    displayStore.sidebarLeftState = 'open'
+  }
 }
 </script>
 

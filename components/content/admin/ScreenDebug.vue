9f4568fb7e7c4f1a65dac231eb54e57ee4c12015
Silas Knight <silasfelinus@gmail.com>
2024-10-13 23:45:34 -0700

    redone debug

--- components/content/admin/ScreenDebug.vue
+++ components/content/admin/ScreenDebug.vue
@@ -1,16 +1,20 @@
 <template>
   <div
-    class="fixed z-50 bg-opacity-90 bg-black text-white p-4 rounded-lg"
+    class="fixed z-50 bg-opacity-90 bg-black text-white p-4 rounded-lg max-w-lg max-h-96 overflow-y-auto"
     style="top: 50%; left: 50%; transform: translate(-50%, -50%)"
   >
     <h2 class="text-lg font-bold">Debug Info</h2>
     <ul class="text-sm space-y-2">
+      <!-- Sidebar states -->
       <li><strong>Sidebar Left State:</strong> {{ sidebarLeftState }}</li>
       <li><strong>Sidebar Right State:</strong> {{ sidebarRightState }}</li>
       <li><strong>Footer State:</strong> {{ footerState }}</li>
-      <li><strong>Is Mobile:</strong> {{ isMobile }}</li>
+      <li><strong>Flip State:</strong> {{ flipState }}</li>
+      <li><strong>Is Animating:</strong> {{ isAnimating }}</li>
+      <li><strong>Current Animation:</strong> {{ currentAnimation }}</li>
+      <li><strong>Is Mobile Viewport:</strong> {{ isMobile }}</li>
 
-      <!-- Display numerical and string versions for dimensions -->
+      <!-- Dimensions -->
       <li><strong>Main Width (num):</strong> {{ mainVw }}vw</li>
       <li><strong>Main Width (str):</strong> {{ mainWidth }}</li>
       <li><strong>Main Height (num):</strong> {{ mainVh }}vh</li>
@@ -32,8 +36,8 @@
       <li><strong>Footer Height (num):</strong> {{ footerVh }}vh</li>
       <li><strong>Footer Height (str):</strong> {{ footerHeight }}</li>
 
-      <!-- Additional debug info -->
-      <li><strong>Padding:</strong> {{ sectionPadding }}</li>
+      <!-- Padding and multipliers -->
+      <li><strong>Padding (px):</strong> {{ sectionPadding }}</li>
       <li>
         <strong>Left Sidebar Multiplier:</strong> {{ sidebarLeftMultiplier }}
       </li>
@@ -41,7 +45,7 @@
         <strong>Right Sidebar Multiplier:</strong> {{ sidebarRightMultiplier }}
       </li>
 
-      <!-- Toggle Element Positions -->
+      <!-- Toggle positions -->
       <li><strong>Left Toggle Position:</strong> {{ leftTogglePosition }}</li>
       <li><strong>Right Toggle Position:</strong> {{ rightTogglePosition }}</li>
       <li>
@@ -65,6 +69,9 @@ const displayStore = useDisplayStore()
 const sidebarLeftState = computed(() => displayStore.sidebarLeftState)
 const sidebarRightState = computed(() => displayStore.sidebarRightState)
 const footerState = computed(() => displayStore.footerState)
+const flipState = computed(() => displayStore.flipState)
+const isAnimating = computed(() => displayStore.isAnimating)
+const currentAnimation = computed(() => displayStore.currentAnimation)
 const isMobile = computed(() => displayStore.isMobileViewport)
 
 // Numerical values
@@ -83,39 +90,29 @@ const sidebarRightWidth = computed(() => displayStore.sidebarRightWidth)
 const headerHeight = computed(() => displayStore.headerHeight)
 const footerHeight = computed(() => displayStore.footerHeight)
 
-// Other variables
-const sectionPadding = '16px'
-
-// Multiplier values
-const sidebarLeftMultiplier = computed(() =>
-  displayStore.sidebarLeftState !== 'hidden' &&
-  displayStore.sidebarLeftState !== 'disabled'
-    ? 2
-    : 1,
-)
-const sidebarRightMultiplier = computed(() =>
-  displayStore.sidebarRightState !== 'hidden' &&
-  displayStore.sidebarRightState !== 'disabled'
-    ? 2
-    : 1,
+// Padding and multipliers
+const sectionPadding = computed(() => displayStore.sectionPadding)
+const sidebarLeftMultiplier = computed(() => displayStore.sidebarLeftMultiplier)
+const sidebarRightMultiplier = computed(
+  () => displayStore.sidebarRightMultiplier,
 )
 const footerMultiplier = computed(() => (footerState.value === 'open' ? 2 : 1))
 
 // Toggle element positions (including calculations)
 const leftTogglePosition = computed(() => {
-  return `Top: calc(${headerHeight.value} + ${sectionPadding} * 2), Left: calc(${sidebarLeftWidth.value} + ${sectionPadding} * ${sidebarLeftMultiplier.value})`
+  return `Top: calc(${headerHeight.value} + ${sectionPadding.value} * 2), Left: calc(${sidebarLeftWidth.value} + ${sectionPadding.value} * ${sidebarLeftMultiplier.value})`
 })
 
 const rightTogglePosition = computed(() => {
-  return `Top: calc(${headerHeight.value} + ${sectionPadding} * 2), Right: calc(${sidebarRightWidth.value} + ${sectionPadding} * ${sidebarRightMultiplier.value})`
+  return `Top: calc(${headerHeight.value} + ${sectionPadding.value} * 2), Right: calc(${sidebarRightWidth.value} + ${sectionPadding.value} * ${sidebarRightMultiplier.value})`
 })
 
 const footerTogglePosition = computed(() => {
-  return `Bottom: calc(${footerHeight.value} + ${sectionPadding} * ${footerMultiplier.value}), Left: calc(${sidebarLeftWidth.value} + ${sectionPadding})`
+  return `Bottom: calc(${footerHeight.value} + ${sectionPadding.value} * ${footerMultiplier.value}), Left: calc(${sidebarLeftWidth.value} + ${sectionPadding.value})`
 })
 
 const sidefootTogglePosition = computed(() => {
-  return `Bottom: calc(${footerHeight.value} + ${sectionPadding} * ${footerMultiplier.value}), Right: calc(${sidebarRightWidth.value} + ${sectionPadding} * ${sidebarRightMultiplier.value})`
+  return `Bottom: calc(${footerHeight.value} + ${sectionPadding.value} * ${footerMultiplier.value}), Right: calc(${sidebarRightWidth.value} + ${sectionPadding.value} * ${sidebarRightMultiplier.value})`
 })
 </script>
 
@@ -128,5 +125,9 @@ ul {
 }
 li {
   padding: 4px 0;
+}
+.debug-panel {
+  max-height: 400px;
+  overflow-y: auto;
 }
 </style>

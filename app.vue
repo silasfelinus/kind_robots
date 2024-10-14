75fd7996c09c4d5113d31b2b18cfc7a05d20ef2d
Silas M Knight <silasfelinus@gmail.com>
2024-10-13 17:03:11 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -7,13 +7,7 @@
     <!-- Header -->
     <header
       class="fixed w-full z-30 flex items-center justify-center bg-primary box-border overflow-hidden transition-all duration-500 ease-in-out"
-      :style="{
-        height: headerHeight,
-        width: calc(100vw - ${sectionPadding} * 2),
-        top: sectionPadding,
-        left: sectionPadding,
-        right: sectionPadding,
-      }"
+      :style="headerStyle"
     >
       <header-upgrade class="flex-grow text-center" />
     </header>
@@ -21,28 +15,17 @@
     <!-- Left Sidebar -->
     <aside
       class="fixed z-20 box-border transition-all duration-300 ease-in-out"
-      :style="{
-        width: sidebarLeftWidth,
-        left: sectionPadding,
-        top: calc(${headerHeight} + ${sectionPadding} * 2),
-        height: calc(${mainHeight} - (${sectionPadding} * (${footerMultiplier} + 2))),
-      }"
+      :style="leftSidebarStyle"
     >
       <!-- Left toggle button (top-left corner of the sidebar) -->
       <left-toggle
         class="fixed z-40 transition-all duration-600 ease-in-out"
-        :style="{
-          top: calc(${headerHeight} + ${sectionPadding} * 3),
-          left: calc(${sidebarLeftWidth} + ${sectionPadding} * (${sidebarLeftMultiplier} + 1)),
-        }"
+        :style="leftToggleStyle"
       />
       <!-- Sidefoot toggle button (bottom-right corner of the sidebar) -->
       <sidefoot-toggle
         class="fixed z-40 transition-all duration-600 ease-in-out"
-        :style="{
-          bottom: calc(${footerHeight} + (${sectionPadding} * (${footerMultiplier} + 2)),
-          left: calc(${sidebarLeftWidth} + (${sectionPadding} * (${sidebarLeftMultiplier} + 2)),
-        }"
+        :style="sidefootToggleStyle"
       />
       <kind-sidebar-simple />
     </aside>
@@ -50,17 +33,7 @@
     <!-- Main Content -->
     <main
       class="fixed z-10 box-border transition-all duration-600 ease-in-out"
-      :style="{
-        top: calc(${headerHeight} + ${sectionPadding} * 2),
-        height: calc(${mainHeight} - (${sectionPadding} * (${footerMultiplier} + 2))),
-        right: sidebarRightOpen
-          ? calc(${sidebarRightWidth} + (${sectionPadding} * ${sidebarRightMultiplier}))
-          : sectionPadding,
-        left: sidebarLeftOpen
-          ? calc(${sidebarLeftWidth} + (${sectionPadding} * ${sidebarLeftMultiplier}))
-          : sectionPadding,
-        width: calc(100vw - (${sectionPadding} * (${sidebarLeftMultiplier} + ${sidebarRightMultiplier}) + ${sidebarLeftOpen ? sidebarLeftWidth : 0} + ${sidebarRightOpen ? sidebarRightWidth : 0})),
-      }"
+      :style="mainContentStyle"
     >
       <main-content />
     </main>
@@ -68,20 +41,12 @@
     <!-- Right Sidebar -->
     <aside
       class="fixed z-20 box-border transition-all duration-600 ease-in-out"
-      :style="{
-        width: sidebarRightWidth,
-        right: sectionPadding,
-        top: calc(${headerHeight} + ${sectionPadding} * 2),
-        height: calc(${mainHeight} - (${sectionPadding} * (${footerMultiplier} + 2))),
-      }"
+      :style="rightSidebarStyle"
     >
       <!-- Right toggle button (top-right corner of the sidebar) -->
       <right-toggle
         class="fixed z-40 transition-all duration-600 ease-in-out"
-        :style="{
-          top: calc(${headerHeight} + ${sectionPadding} * 2),
-          right: calc(${sidebarRightWidth} + (${sectionPadding} * ${sidebarRightMultiplier})),
-        }"
+        :style="rightToggleStyle"
       />
       <splash-tutorial v-if="showTutorial" class="h-full w-full" />
     </aside>
@@ -89,21 +54,12 @@
     <!-- Footer -->
     <footer
       class="fixed z-30 box-border overflow-hidden transition-all duration-600 ease-in-out"
-      :style="{
-        height: footerHeight,
-        width: calc(100vw - ${sectionPadding} * 2),
-        left: sectionPadding,
-        right: sectionPadding,
-        bottom: sectionPadding,
-      }"
+      :style="footerStyle"
     >
       <!-- Footer toggle button (bottom-left corner of the footer) -->
       <footer-toggle
         class="fixed z-40 transition-all duration-600 ease-in-out"
-        :style="{
-          bottom: calc(${footerHeight} + ${sectionPadding} * ${footerMultiplier}),
-          right: calc(${sidebarRightWidth} + ${sectionPadding} * (${sidebarRightMultiplier} + 1)),
-        }"
+        :style="footerToggleStyle"
       />
       <horizontal-nav />
     </footer>
@@ -118,45 +74,80 @@ import { useDisplayStore } from '@/stores/displayStore'
 const displayStore = useDisplayStore()
 
 // Compute header height using custom vh
-const headerHeight = computed(
-  () => calc(var(--vh) * ${displayStore.headerVh}),
-)
-
-// Main content dimensions
+const headerHeight = computed(() => `calc(var(--vh) * ${displayStore.headerVh})`)
 const mainHeight = computed(() => displayStore.mainHeight)
-
-// Sidebar widths for left and right
 const sidebarLeftWidth = computed(() => displayStore.sidebarLeftWidth)
 const sidebarRightWidth = computed(() => displayStore.sidebarRightWidth)
-const showTutorial = computed(() => displayStore.showTutorial)
-
-// Footer height
-const footerHeight = computed(
-  () => calc(var(--vh) * ${displayStore.footerVh}),
-)
-
-// Padding for all sections (consistent)
+const footerHeight = computed(() => `calc(var(--vh) * ${displayStore.footerVh})`)
 const sectionPadding = '16px'
-
-// Check if the footer is open
+const showTutorial = computed(() => displayStore.showTutorial)
 const footerOpen = computed(() => displayStore.footerState === 'open')
-
-// Check if the left sidebar is open (and not hidden/disabled)
-const sidebarLeftOpen = computed(
-  () =>
-    displayStore.sidebarLeftState !== 'hidden' &&
-    displayStore.sidebarLeftState !== 'disabled',
-)
-
-// Check if the right sidebar is open (and not hidden/disabled)
-const sidebarRightOpen = computed(
-  () =>
-    displayStore.sidebarRightState !== 'hidden' &&
-    displayStore.sidebarRightState !== 'disabled',
-)
-
-// Calculate sidebar and main content multipliers based on footer and sidebar state
+const sidebarLeftOpen = computed(() => displayStore.sidebarLeftState !== 'hidden' && displayStore.sidebarLeftState !== 'disabled')
+const sidebarRightOpen = computed(() => displayStore.sidebarRightState !== 'hidden' && displayStore.sidebarRightState !== 'disabled')
 const footerMultiplier = computed(() => (footerOpen.value ? 2 : 1))
 const sidebarLeftMultiplier = computed(() => (sidebarLeftOpen.value ? 2 : 1))
 const sidebarRightMultiplier = computed(() => (sidebarRightOpen.value ? 2 : 1))
+
+// Computed styles
+const headerStyle = computed(() => ({
+  height: headerHeight.value,
+  width: `calc(100vw - ${sectionPadding} * 2)`,
+  top: sectionPadding,
+  left: sectionPadding,
+  right: sectionPadding,
+}))
+
+const leftSidebarStyle = computed(() => ({
+  width: sidebarLeftWidth.value,
+  left: sectionPadding,
+  top: `calc(${headerHeight.value} + ${sectionPadding} * 2)`,
+  height: `calc(${mainHeight.value} - (${sectionPadding} * (${footerMultiplier.value} + 2)))`,
+}))
+
+const leftToggleStyle = computed(() => ({
+  top: `calc(${headerHeight.value} + ${sectionPadding} * 3)`,
+  left: `calc(${sidebarLeftWidth.value} + ${sectionPadding} * (${sidebarLeftMultiplier.value} + 1))`,
+}))
+
+const sidefootToggleStyle = computed(() => ({
+  bottom: `calc(${footerHeight.value} + (${sectionPadding} * (${footerMultiplier.value} + 2)))`,
+  left: `calc(${sidebarLeftWidth.value} + (${sectionPadding} * (${sidebarLeftMultiplier.value} + 2)))`,
+}))
+
+const mainContentStyle = computed(() => ({
+  top: `calc(${headerHeight.value} + ${sectionPadding} * 2)`,
+  height: `calc(${mainHeight.value} - (${sectionPadding} * (${footerMultiplier.value} + 2)))`,
+  right: sidebarRightOpen.value
+    ? `calc(${sidebarRightWidth.value} + (${sectionPadding} * ${sidebarRightMultiplier.value}))`
+    : sectionPadding,
+  left: sidebarLeftOpen.value
+    ? `calc(${sidebarLeftWidth.value} + (${sectionPadding} * ${sidebarLeftMultiplier.value}))`
+    : sectionPadding,
+  width: `calc(100vw - (${sectionPadding} * (${sidebarLeftMultiplier.value} + ${sidebarRightMultiplier.value}) + ${sidebarLeftOpen.value ? sidebarLeftWidth.value : 0} + ${sidebarRightOpen.value ? sidebarRightWidth.value : 0}))`,
+}))
+
+const rightSidebarStyle = computed(() => ({
+  width: sidebarRightWidth.value,
+  right: sectionPadding,
+  top: `calc(${headerHeight.value} + ${sectionPadding} * 2)`,
+  height: `calc(${mainHeight.value} - (${sectionPadding} * (${footerMultiplier.value} + 2)))`,
+}))
+
+const rightToggleStyle = computed(() => ({
+  top: `calc(${headerHeight.value} + ${sectionPadding} * 2)`,
+  right: `calc(${sidebarRightWidth.value} + (${sectionPadding} * ${sidebarRightMultiplier.value}))`,
+}))
+
+const footerStyle = computed(() => ({
+  height: footerHeight.value,
+  width: `calc(100vw - ${sectionPadding} * 2)`,
+  left: sectionPadding,
+  right: sectionPadding,
+  bottom: sectionPadding,
+}))
+
+const footerToggleStyle = computed(() => ({
+  bottom: `calc(${footerHeight.value} + ${sectionPadding} * ${footerMultiplier.value})`,
+  right: `calc(${sidebarRightWidth.value} + ${sectionPadding} * (${sidebarRightMultiplier.value} + 1))`,
+}))
 </script>

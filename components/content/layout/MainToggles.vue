e6aa6e69582500d062d4028fae708e62d79cc45e
Silas M Knight <silasfelinus@gmail.com>
2024-10-14 16:43:31 -0700

    Update MainToggles.vue

--- components/content/layout/MainToggles.vue
+++ components/content/layout/MainToggles.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="pointer-events-auto">
     <!-- Left Toggle -->
-    <div class="w-16 h-16 fixed z-50" :style="leftToggleStyle">
+    <div class="w-5 h-5 fixed z-50 transition-all duration-600 ease-in-out" :style="leftToggleStyle">
       <left-toggle />
     </div>
 
     <!-- Sidefoot Toggle -->
-    <div class="w-16 h-16 fixed z-50" :style="sidefootToggleStyle">
+    <div class="ww-5 h-5 fixed z-50 transition-all duration-600 ease-in-out" :style="sidefootToggleStyle">
       <sidefoot-toggle />
     </div>
 
     <!-- Right Toggle -->
-    <div class="w-16 h-16 fixed z-50" :style="rightToggleStyle">
+    <div class="w-5 h-5 fixed z-50 transition-all duration-600 ease-in-out" :style="rightToggleStyle">
       <right-toggle />
     </div>
 
     <!-- Footer Toggle -->
-    <div class="w-16 h-16 fixed z-50" :style="footerToggleStyle">
+    <div class="w-5 h-5 fixed z-50 transition-all duration-600 ease-in-out" :style="footerToggleStyle">
       <footer-toggle />
     </div>
   </div>

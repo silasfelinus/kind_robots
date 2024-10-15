9a2bbfed863e419855fba84e74ddf05ad0d200d4
Silas M Knight <silasfelinus@gmail.com>
2024-10-14 20:33:29 -0700

    Update sidefoot-toggle.vue

--- components/content/layout/sidefoot-toggle.vue
+++ components/content/layout/sidefoot-toggle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative z-50">
     <button
-      class="rounded-tr-2xl rounded-bl-2xl shadow-lg bg-gradient-to-tl from-accent to-accent-dark hover:bg-secondary text-2xl font-semibold text-white flex items-center justify-center"
+      class="w-16 h-16 rounded-2xl bg-gradient-to-br from-pink-400 to-yellow-400 text-2xl font-semibold text-white flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleState"
     >
       <!-- Simple up and down caret characters -->

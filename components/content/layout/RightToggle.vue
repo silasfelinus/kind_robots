f3d1a9acab454f4ca2638c5d493279d1387d5339
Silas Knight <silasfelinus@gmail.com>
2024-10-14 22:57:07 -0700

    tweaking right toggle

--- components/content/layout/RightToggle.vue
+++ components/content/layout/RightToggle.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="relative z-50 p-1">
+  <div class="relative z-50">
     <!-- Arc-style toggle button for right sidebar and tutorial -->
     <button
-      class="w-16 h-16 rounded-2xl bg-gradient-to-br from-pink-400 to-yellow-400 text-2xl font-semibold text-white flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
+      class="p-1 fixed top-0 right-0 w-10 h-10 rounded-2xl bg-gradient-to-br from-pink-400 to-yellow-400 text-2xl font-semibold text-white flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleTutorialSidebar"
     >
       <span

30ff9e3976282f10d54cdc7a5cf863462b2c33a3
Silas Knight <silasfelinus@gmail.com>
2024-10-14 23:46:59 -0700

    moved right toggle spacing into right toggle.

--- components/content/layout/RightToggle.vue
+++ components/content/layout/RightToggle.vue
@@ -2,7 +2,8 @@
   <div class="relative z-50">
     <!-- Arc-style toggle button for right sidebar and tutorial -->
     <button
-      class="p-1 fixed top-0 right-0 w-10 h-10 rounded-2xl bg-gradient-to-br from-pink-400 to-yellow-400 text-2xl font-semibold text-white flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
+      class="p-1 fixed w-10 h-10 rounded-2xl bg-gradient-to-br from-pink-400 to-yellow-400 text-2xl font-semibold text-white flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
+      :style="rightToggleStyle"
       @click="toggleTutorialSidebar"
     >
       <span
@@ -19,10 +20,17 @@
 import { computed } from 'vue'
 import { useDisplayStore } from '@/stores/displayStore'
 
+// Access the displayStore for managing the layout state
 const displayStore = useDisplayStore()
 
-// Determine if the viewport is mobile
-const isMobile = computed(() => displayStore.isMobileViewport)
+// Right toggle style, dynamic based on main-toggles positioning
+const rightToggleStyle = computed(() => ({
+  top: `calc(${displayStore.headerHeight} + (${displayStore.sectionPadding} * 2 ))`,
+  right:
+    displayStore.sidebarRightState !== 'hidden'
+      ? `calc(${displayStore.sidebarRightWidth} + (${displayStore.sectionPadding} * 2 ))`
+      : displayStore.sectionPadding,
+}))
 
 // Determine the icon text based on the right sidebar's state
 const rightIconText = computed(() => {
@@ -33,7 +41,7 @@ const rightIconText = computed(() => {
 const toggleTutorialSidebar = () => {
   displayStore.toggleTutorial()
 
-  if (!isMobile.value) {
+  if (!displayStore.isMobileViewport) {
     // Toggle between 'open' and 'hidden' state
     if (displayStore.sidebarRightState === 'hidden') {
       displayStore.setSidebarRight(true) // Open the sidebar

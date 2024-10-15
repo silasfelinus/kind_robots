123fc42bfc156efdf96a2aa48cc1823c5c86bf61
Silas Knight <silasfelinus@gmail.com>
2024-10-15 01:55:53 -0700

    changed right padding

--- components/content/layout/RightToggle.vue
+++ components/content/layout/RightToggle.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="fixed z-50 p-1">
+  <div class="fixed z-50 p-1" :style="rightToggleStyle">
     <button
       class="w-10 h-10 rounded-2xl bg-gradient-to-br from-pink-400 to-yellow-400 text-2xl font-semibold text-white flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
-      :style="rightToggleStyle"
       @click="toggleTutorialSidebar"
     >
       <span
@@ -23,17 +22,19 @@ const displayStore = useDisplayStore()
 
 // Right toggle style, based on whether the right sidebar is open or hidden
 const rightToggleStyle = computed(() => {
+  const consistentPadding = '20px' // Define consistent padding for placement
+
   if (displayStore.sidebarRightState === 'hidden') {
-    // When the sidebar is hidden, place the toggle outside with padding
+    // When the sidebar is hidden, place the toggle outside with consistent padding
     return {
       top: `calc(${displayStore.headerHeight} + (${displayStore.sectionPadding} * 2))`,
-      right: `${displayStore.sectionPadding}`,
+      right: `${displayStore.sectionPadding}`, // Position outside
     }
   } else {
     // When the sidebar is open, place the toggle inside the sidebar
     return {
       top: `calc(${displayStore.headerHeight} + (${displayStore.sectionPadding} * 2))`,
-      right: `calc(${displayStore.sidebarRightWidth} - (${displayStore.sectionPadding} * 2))`,
+      right: `calc(${displayStore.sidebarRightWidth} - ${consistentPadding})`, // Inside the sidebar
     }
   }
 })

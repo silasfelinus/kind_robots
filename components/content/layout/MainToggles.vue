64d5add0adb54eded068af30ecaf27ca50f1b1de
Silas Knight <silasfelinus@gmail.com>
2024-10-16 00:13:08 -0700

    added fixed

--- components/content/layout/MainToggles.vue
+++ components/content/layout/MainToggles.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="pointer-events-auto">
     <!-- Left Toggle -->
-    <div class="w-7 h-7 z-50 transition-all duration-600 ease-in-out">
+    <div class="w-7 h-7 z-50 fixed transition-all duration-600 ease-in-out">
       <left-toggle />
     </div>
 
     <!-- Right Toggle -->
-    <div class="w-7 h-7 z-50 transition-all duration-600 ease-in-out">
+    <div class="w-7 h-7 z-50 fixed transition-all duration-600 ease-in-out">
       <right-toggle />
     </div>
 
     <!-- Footer Toggle (Centered at Bottom) -->
-    <div class="w-7 h-7 z-50 transition-all duration-600 ease-in-out">
+    <div class="w-7 h-7 z-50 fixed transition-all duration-600 ease-in-out">
       <footer-toggle />
     </div>
   </div>

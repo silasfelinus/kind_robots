06696ca4a638fba0b5c7b0fae73b3a1c77d561e0
Silas M Knight <silasfelinus@gmail.com>
2024-10-17 13:45:30 -0700

    Update ButterflyFlip.vue

---
+++ components/content/butterfly/ButterflyFlip.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flip-card" @click="toggleFlip">
+    <div class="flip-card-inner" :class="{ 'flipped': isFlipped }">
+      <div class="flip-card-front">
+        <slot name="front"></slot>
+      </div>
+      <div class="flip-card-back">
+        <slot name="back"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isFlipped = ref(false)
+
+const toggleFlip = () => {
+  isFlipped.value = !isFlipped.value
+}
+</script>
+
+<style scoped>
+.flip-card {
+  perspective: 1000px;
+  width: 200px;
+  height: 300px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+}
+
+.flipped {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+</style>

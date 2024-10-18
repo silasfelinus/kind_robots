431d6e6f843be26bbedfe9b2ba9f0881ba19b8fa
Silas M Knight <silasfelinus@gmail.com>
2024-10-17 21:09:59 -0700

    Update ButterflyFlip.vue

--- components/content/butterfly/ButterflyFlip.vue
+++ components/content/butterfly/ButterflyFlip.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flip-card">
+  <div class="flip-card relative w-52 h-72">
     <!-- Non-clickable area (front/back content) -->
-    <div class="flip-card-inner" :class="{ flipped: isFlipped }">
-      <div class="flip-card-front">
+    <div class="flip-card-inner absolute w-full h-full transition-transform duration-600 ease-in-out" :class="{ flipped: isFlipped }">
+      <div class="flip-card-front absolute w-full h-full backface-hidden">
         <slot name="front"></slot>
       </div>
-      <div class="flip-card-back">
+      <div class="flip-card-back absolute w-full h-full backface-hidden transform rotate-y-180">
         <slot name="back"></slot>
       </div>
     </div>
@@ -13,12 +13,12 @@
     <!-- Clickable triangle areas -->
     <div
       v-if="isFlipped"
-      class="triangle top-left"
+      class="absolute top-0 left-0 w-0 h-0 border-r-[30px] border-b-[30px] border-transparent border-r-[var(--bg-accent,#ff9800)] cursor-pointer z-10"
       @click="setTab('front')"
     ></div>
     <div
       v-if="!isFlipped"
-      class="triangle top-right"
+      class="absolute top-0 right-0 w-0 h-0 border-l-[30px] border-b-[30px] border-transparent border-l-[var(--bg-accent,#ff9800)] cursor-pointer z-10"
       @click="setTab('back')"
     ></div>
   </div>
@@ -37,60 +37,15 @@ const setTab = (tab) => {
 </script>
 
 <style scoped>
-.flip-card {
-  perspective: 1000px;
-  width: 200px;
-  height: 300px;
-  position: relative;
-}
-
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  transition: transform 0.6s;
-  transform-style: preserve-3d;
-}
-
-.flipped {
-  transform: rotateY(180deg);
-}
-
-.flip-card-front,
-.flip-card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
+.backface-hidden {
   backface-visibility: hidden;
 }
 
-.flip-card-back {
+.flip-card-inner.flipped {
   transform: rotateY(180deg);
 }
 
-/* Triangle CSS for flipping */
-.triangle {
-  position: absolute;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  cursor: pointer;
-  z-index: 10;
-}
-
-/* Top-left triangle */
-.top-left {
-  top: 0;
-  left: 0;
-  border-width: 0 40px 40px 0;
-  border-color: transparent var(--bg-accent) transparent transparent;
-}
-
-/* Bottom-right triangle */
-.top-right {
-  top: 0;
-  right: 0;
-  border-width: 40px 0 0 40px;
-  border-color: transparent transparent transparent var(--bg-accent);
+.rotate-y-180 {
+  transform: rotateY(180deg);
 }
 </style>

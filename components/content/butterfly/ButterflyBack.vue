de40040b5b6cb0a03fad230f896ef6de0d6e189a
Silas Knight <silasfelinus@gmail.com>
2024-10-18 03:18:37 -0700

    butterflies remodel

--- components/content/butterfly/ButterflyBack.vue
+++ components/content/butterfly/ButterflyBack.vue
@@ -6,9 +6,19 @@
 
       <!-- Butterfly Selection -->
       <div v-if="butterflies.length > 0" class="mb-6">
-        <label for="selectedButterfly" class="block mb-2">Select Butterfly:</label>
-        <select v-model="selectedButterflyId" class="w-full p-2 rounded" @change="selectButterfly">
-          <option v-for="butterfly in butterflies" :key="butterfly.id" :value="butterfly.id">
+        <label for="selectedButterfly" class="block mb-2"
+          >Select Butterfly:</label
+        >
+        <select
+          v-model="selectedButterflyId"
+          class="w-full p-2 rounded"
+          @change="selectButterfly"
+        >
+          <option
+            v-for="butterfly in butterflies"
+            :key="butterfly.id"
+            :value="butterfly.id"
+          >
             {{ butterfly.id }}
           </option>
         </select>
@@ -16,7 +26,9 @@
 
       <!-- Butterfly ID Editing -->
       <div v-if="selectedButterfly" class="mb-6">
-        <label for="editButterflyId" class="block mb-2">Edit Butterfly ID:</label>
+        <label for="editButterflyId" class="block mb-2"
+          >Edit Butterfly ID:</label
+        >
         <input
           id="editButterflyId"
           v-model="newButterflyId"
@@ -29,16 +41,46 @@
       <!-- Size range slider -->
       <butterfly-slider
         v-if="selectedButterfly"
+        v-model="selectedButterflySize"
         label="Size"
         :min="0.5"
         :max="2"
         :step="0.1"
-        v-model="selectedButterflySize"
         slider-id="sizeSlider"
       />
 
-      <!-- Other sliders like Speed, Wing Speed, etc... -->
+      <!-- Speed range slider -->
+      <butterfly-slider
+        v-if="selectedButterfly"
+        v-model="selectedButterflySpeed"
+        label="Speed"
+        :min="0.5"
+        :max="5"
+        :step="0.1"
+        slider-id="speedSlider"
+      />
 
+      <!-- Wing Speed range slider -->
+      <butterfly-slider
+        v-if="selectedButterfly"
+        v-model="selectedButterflyWingSpeed"
+        label="Wing Speed"
+        :min="0.5"
+        :max="5"
+        :step="0.1"
+        slider-id="wingSpeedSlider"
+      />
+
+      <!-- Sway range slider -->
+      <butterfly-slider
+        v-if="selectedButterfly"
+        v-model="selectedButterflySway"
+        label="Sway"
+        :min="0"
+        :max="2"
+        :step="0.1"
+        slider-id="swaySlider"
+      />
     </div>
 
     <!-- Butterfly Demo always visible at the bottom -->
@@ -47,18 +89,23 @@
     </div>
   </div>
 </template>
-
 <script setup>
 import { computed, ref } from 'vue'
 import { useButterflyStore } from '@/stores/butterflyStore'
 
 // Access the butterfly store
 const butterflyStore = useButterflyStore()
-const butterflies = computed(() => butterflyStore.butterflies)
+const butterflies = computed(() => butterflyStore.getAllButterflies)
 
 // Selected butterfly ID and reactive object for selected butterfly
-const selectedButterflyId = ref(butterflies.value.length ? butterflies.value[0].id : null)
-const selectedButterfly = computed(() => butterflies.value.find(butterfly => butterfly.id === selectedButterflyId.value))
+const selectedButterflyId = ref(
+  butterflies.value.length ? butterflies.value[0].id : null,
+)
+const selectedButterfly = computed(() =>
+  butterflies.value.find(
+    (butterfly) => butterfly.id === selectedButterflyId.value,
+  ),
+)
 
 // Create a reactive property for the new butterfly ID (used for editing)
 const newButterflyId = ref(selectedButterfly.value?.id || '')
@@ -66,7 +113,10 @@ const newButterflyId = ref(selectedButterfly.value?.id || '')
 // Update the selected butterfly's ID
 const updateButterflyId = () => {
   if (selectedButterfly.value) {
-    butterflyStore.updateButterflyId(selectedButterfly.value.id, newButterflyId.value)
+    butterflyStore.updateButterflyId(
+      selectedButterfly.value.id,
+      newButterflyId.value,
+    )
   }
 }
 
@@ -77,11 +127,38 @@ const selectButterfly = () => {
 
 // Compute and bind individual butterfly properties like size, speed, etc.
 const selectedButterflySize = computed({
-  get: () => selectedButterfly.value ? selectedButterfly.value.scale : 0,
-  set: (val) => { if (selectedButterfly.value) selectedButterfly.value.scale = val }
+  get: () => selectedButterfly.value?.scale || 0.5,
+  set: (val) => {
+    if (selectedButterfly.value) {
+      selectedButterfly.value.scale = val
+    }
+  },
+})
+
+const selectedButterflySpeed = computed({
+  get: () => selectedButterfly.value?.speed || 1,
+  set: (val) => {
+    if (selectedButterfly.value) {
+      selectedButterfly.value.speed = val
+    }
+  },
+})
+
+const selectedButterflyWingSpeed = computed({
+  get: () => selectedButterfly.value?.wingSpeed || 1,
+  set: (val) => {
+    if (selectedButterfly.value) {
+      selectedButterfly.value.wingSpeed = val
+    }
+  },
+})
+
+const selectedButterflySway = computed({
+  get: () => selectedButterfly.value?.sway || 0.1,
+  set: (val) => {
+    if (selectedButterfly.value) {
+      selectedButterfly.value.sway = val
+    }
+  },
 })
 </script>
-
-<style scoped>
-/* Style adjustments can go here if needed */
-</style>

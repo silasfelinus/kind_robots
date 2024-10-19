674bcea787978ca0a01cccd3faa5ab6f69ad8376
Silas Knight <silasfelinus@gmail.com>
2024-10-18 18:10:38 -0700

    removed sway, tried to fix second slider button, using butterflyStore for color for main butterfly

--- components/content/butterfly/AmiButterfly.vue
+++ components/content/butterfly/AmiButterfly.vue
@@ -1,31 +1,13 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, reactive } from 'vue'
 import { makeNoise2D } from 'open-simplex-noise'
+import {
+  randomColor,
+  analogousColor,
+  complementaryColor,
+} from '@/stores/butterflyStore'
 
 const hydrated = ref(false)
-
-const randomColor = (): string => {
-  const h = Math.floor(Math.random() * 360)
-  const s = Math.floor(Math.random() * 50 + 50) // keep saturation between 50 and 100
-  const l = Math.floor(Math.random() * 40 + 30) // keep lightness between 30 and 70
-  return `hsl(${h},${s}%,${l}%)`
-}
-
-const analogousColor = (hsl: string): string => {
-  const hslMatch = hsl.match(/\d+/g)
-  if (!hslMatch) {
-    throw new Error('Invalid color format')
-  }
-  const [h, s, l] = hslMatch.map(Number)
-  const newH = (h + 30) % 360
-  return `hsl(${newH},${s}%,${l}%)`
-}
-
-const complementaryColor = (color: string): string => {
-  const [h, s, l] = color.replace('hsl(', '').replace(')', '').split(',')
-  const newH = (parseInt(h) + 180) % 360
-  return `hsl(${newH},${s},${l})`
-}
 
 interface Butterfly {
   wingTopColor: string
@@ -35,7 +17,6 @@ interface Butterfly {
   status: 'random' | 'float' | 'mouse' | 'spaz' | 'flock' | 'clear'
   goal: { x: number; y: number }
   hasReachedGoal: boolean
-  sway: boolean
   wingSpeed: number
   scale: number
   countdown: number
@@ -71,7 +52,6 @@ const butterfly = reactive<Butterfly>({
     y: Math.random() * windowSize.height,
   },
   hasReachedGoal: false,
-  sway: false,
   wingSpeed: Math.floor(Math.random() * 3), // random initial wing speed
   scale: Math.random() * 0.5 + 0.75, // random initial scale between 0.75 and 1.25
   countdown: 0,

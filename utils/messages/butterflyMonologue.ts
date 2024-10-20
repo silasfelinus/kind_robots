4873b61ee1d47713e492d2371612ba297d523505
Silas M Knight <silasfelinus@gmail.com>
2024-10-19 20:45:04 -0700

    Update butterflyMonologue.ts

---
+++ utils/messages/butterflyMonologue.ts
@@ -0,0 +1,7 @@
+export const butterflyMonologue = [
+  "I wonder if anyone notices how fabulous I look today.",
+  "These wings are like magic, right?",
+  "What's the point of flying if no one watches?",
+  "Do you think the flowers appreciate my visit?",
+  "I hope my colors are on point today!"
+]

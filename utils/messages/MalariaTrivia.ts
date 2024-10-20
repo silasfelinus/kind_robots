c39a0562d47edc27583b3f4fda36cb21aafb87c7
Silas M Knight <silasfelinus@gmail.com>
2024-10-19 20:47:36 -0700

    Update MalariaTrivia.ts

---
+++ utils/messages/MalariaTrivia.ts
@@ -0,0 +1,7 @@
+export const malariaTrivia = [
+  "Malaria kills hundreds of thousands of people each year.",
+  "Did you know malaria is transmitted by mosquitoes, not butterflies?",
+  "Malaria is preventable with proper care and resources.",
+  "Mosquitoes are responsible for spreading malaria in tropical regions.",
+  "Malaria prevention efforts can save countless lives each year."
+]

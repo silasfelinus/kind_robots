6f90075098596dff02799f9b1b63c817562f749a
Silas M Knight <silasfelinus@gmail.com>
2024-10-19 20:48:32 -0700

    Update sponsorPlea.ts

---
+++ utils/messages/sponsorPlea.ts
@@ -0,0 +1,7 @@
+export const sponsorPlea = [
+  `Let's make a difference together! Visit [our sponsor](https://againstmalaria.com/amibot)`,
+  `Check out how you can help prevent malaria at [our sponsor](https://againstmalaria.com/amibot)`,
+  `Want to save lives? Support [our sponsor](https://againstmalaria.com/amibot)`,
+  `Help fight malaria by donating through [our sponsor](https://againstmalaria.com/amibot)`,
+  `Together, we can beat malaria! Learn more at [our sponsor](https://againstmalaria.com/amibot)`
+]

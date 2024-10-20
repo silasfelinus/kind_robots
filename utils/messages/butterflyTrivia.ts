b142891f8d63a1d99e9d056df73cb687144e3c0b
Silas M Knight <silasfelinus@gmail.com>
2024-10-19 20:42:40 -0700

    Update butterflyTrivia.ts

---
+++ utils/messages/butterflyTrivia.ts
@@ -0,0 +1,5 @@
+export const butterflyTrivia = [
+  "Did you know butterflies taste with their feet?",
+  "Some butterflies can fly 37 miles per hour!",
+  "A day without butterflies is a day without sunshine!"
+]

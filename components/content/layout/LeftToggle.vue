9252742f67aac1bca2eb7e7204b7bad1bec767e0
Silas M Knight <silasfelinus@gmail.com>
2024-10-21 05:54:10 -0700

    Update LeftToggle.vue

--- components/content/layout/LeftToggle.vue
+++ components/content/layout/LeftToggle.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="z-50 p-1">
     <button
-      class="w-6 h-6 rounded-2xl font-semibold text-transparent flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
+      class="w-8 h-8 rounded-2xl font-semibold text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleSidebarLeft"
     >
       <span
-        class="toggle-character"
+        class="toggle-character text-secondary"
         style="
           background: linear-gradient(to bottom right, #f472b6, #fbbf24);
           background-clip: text;
           -webkit-background-clip: text;
-          color: transparent;
+          
           text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);
         "
       >

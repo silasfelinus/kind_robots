d114983676df6c7328a500fc959f365a8f017786
Silas Knight <silasfelinus@gmail.com>
2024-10-21 16:14:03 -0700

    better viewablee toggle

--- components/content/layout/RightToggle.vue
+++ components/content/layout/RightToggle.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="z-50 p-1">
     <button
-      class="w-8 h-8 rounded-2xl flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
+      class="w-8 h-8 rounded-2xl font-semibold text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleTutorialSidebar"
     >
       <span
-        class="toggle-character text-primary font-bold text-info "
+        class="toggle-character text-secondary"
         style="
           background: linear-gradient(to bottom right, #f472b6, #fbbf24);
           background-clip: text;

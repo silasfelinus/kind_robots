7f9c85d102ce9a75d77aa625a0ee715b33861b9d
Silas Knight <silasfelinus@gmail.com>
2024-10-20 18:28:54 -0700

    tighter login

--- components/content/admin/LoginPath.vue
+++ components/content/admin/LoginPath.vue
@@ -10,39 +10,15 @@
           {{ username }}!
         </router-link>
         <jellybean-count />
-
-        <!-- Login Overlay (Hidden until hover) -->
-        <div
-          class="absolute left-0 top-full mt-1 p-2 bg-primary text-white rounded-lg shadow-lg transition-all opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-2 duration-300 ease-in-out"
-        >
-          <router-link to="/logout">
-            <button class="flex items-center justify-center space-x-2">
-              <Icon name="bi:box-arrow-in-right" class="w-5 h-5" />
-              <span>Logout</span>
-            </button>
-          </router-link>
-        </div>
       </div>
       <div v-else class="relative group">
         <!-- Default Guest Layout -->
-        <router-link
-          to="/dashboard"
-          class="cursor-pointer text-primary text-lg w-full box-border"
-        >
-          Kind Guest
+        <router-link to="/dashboard">
+          <button class="flex items-center justify-center space-x-2">
+            <Icon name="bi:person-fill" class="w-5 h-5" />
+            <span>Login</span>
+          </button>
         </router-link>
-
-        <!-- Login Overlay (Hidden until hover) -->
-        <div
-          class="absolute left-0 top-full mt-1 p-2 bg-primary text-white rounded-lg shadow-lg transition-all opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-2 duration-300 ease-in-out"
-        >
-          <router-link to="/login">
-            <button class="flex items-center justify-center space-x-2">
-              <Icon name="bi:person-fill" class="w-5 h-5" />
-              <span>Login</span>
-            </button>
-          </router-link>
-        </div>
       </div>
     </div>
   </div>

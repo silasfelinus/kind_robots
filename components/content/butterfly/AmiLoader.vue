3ec24c33f481456dfeb7bc2128ca070a152c84af
Silas M Knight <silasfelinus@gmail.com>
2024-10-21 18:22:36 -0700

    Update AmiLoader.vue

--- components/content/butterfly/AmiLoader.vue
+++ components/content/butterfly/AmiLoader.vue
@@ -7,7 +7,7 @@
   >
     <!-- Dynamic Loading Message -->
     <div class="loading-message">{{ currentMessage }}</div>
-<Icon name="kind-icon:loading-bubble"
+<Icon name="kind-icon:bubble-loading"
 
     <!-- Multiple Butterflies with Animation Delay -->
     <ami-butterfly v-for="i in butterflyCount" :key="i" :class="{ 'butterfly-fade-out': butterflyFadeOut }" />

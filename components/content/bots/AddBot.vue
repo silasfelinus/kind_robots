428fe0b1e79307e28172446688a4387578747ffe
Silas M Knight <silasfelinus@gmail.com>
2024-10-25 20:16:33 -0700

    Update AddBot.vue

--- components/content/bots/AddBot.vue
+++ components/content/bots/AddBot.vue
@@ -1,156 +1,137 @@
 <template>
-  <div
-    class="rounded-2xl border p-4 m-4 mx-auto bg-base-200 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-  >
+  <div class="rounded-2xl border p-4 m-4 mx-auto bg-base-200 grid gap-4 grid-cols-1">
     <h1 class="text-4xl text-center col-span-full">Create or Edit a Bot</h1>
 
-    <bot-selector />
+    <!-- Top section with Bot Selector, Designer, and Toggles -->
+    <div class="flex flex-wrap justify-between items-center col-span-full gap-4">
+      <div class="w-full lg:w-auto">
+        <bot-selector />
+      </div>
 
-    <!-- Toggles for Public Visibility and Under Construction -->
-    <div class="col-span-full flex justify-between items-center mb-4">
-      <div>
-        <label class="block text-lg font-medium">Public Visibility:</label>
-        <div class="flex space-x-2">
-          <button
-            type="button"
-            :class="{
-              'btn btn-primary': botStore.botForm.isPublic,
-              'btn btn-grey-200': !botStore.botForm.isPublic,
-            }"
-            @click="toggleVisibility(true)"
+      <!-- Designer Field -->
+      <div class="flex-grow lg:w-1/4">
+        <label for="designer" class="block text-lg font-medium">Designer:</label>
+        <input
+          v-if="canEditDesigner"
+          id="designer"
+          v-model="botStore.botForm.designer"
+          type="text"
+          class="w-full p-3 rounded-lg border"
+        />
+        <p v-else>{{ botStore.botForm.designer }}</p>
+      </div>
+
+      <!-- Toggles for Public Visibility and Under Construction -->
+      <div class="flex gap-4">
+        <div>
+          <label class="block text-lg font-medium">Public Visibility:</label>
+          <div class="flex space-x-2">
+            <button
+              type="button"
+              :class="{
+                'btn btn-primary': botStore.botForm.isPublic,
+                'btn btn-grey-200': !botStore.botForm.isPublic,
+              }"
+              @click="toggleVisibility(true)"
+            >
+              Public
+            </button>
+            <button
+              type="button"
+              :class="{
+                'btn btn-primary': !botStore.botForm.isPublic,
+                'btn btn-grey-200': botStore.botForm.isPublic,
+              }"
+              @click="toggleVisibility(false)"
+            >
+              Private
+            </button>
+          </div>
+        </div>
+
+        <div class="flex items-center">
+          <input
+            id="underConstruction"
+            v-model="botStore.botForm.underConstruction"
+            type="checkbox"
+            class="mr-2"
+          />
+          <label for="underConstruction" class="block text-lg font-medium"
+            >Mark as under construction</label
           >
-            Public
-          </button>
-          <button
-            type="button"
-            :class="{
-              'btn btn-primary': !botStore.botForm.isPublic,
-              'btn btn-grey-200': botStore.botForm.isPublic,
-            }"
-            @click="toggleVisibility(false)"
-          >
-            Private
-          </button>
         </div>
       </div>
-      <div class="flex items-center">
-        <input
-          id="underConstruction"
-          v-model="botStore.botForm.underConstruction"
-          type="checkbox"
-          class="mr-2"
-        />
-        <label for="underConstruction" class="block text-lg font-medium"
-          >Mark as under construction</label
-        >
+    </div>
+
+    <!-- Middle section - Avatar Generator on the left, Bot Name & Details on the right -->
+    <div class="flex flex-wrap w-full mt-4">
+      <!-- Avatar Image and Generation Component -->
+      <div class="w-full md:w-1/2 lg:w-1/2 p-4">
+        <generate-avatar />
+      </div>
+
+      <!-- Bot Details: Name, Subtitle, Description, and Bot Intro -->
+      <div class="w-full md:w-1/2 lg:w-1/2 p-4">
+        <div class="grid gap-4">
+          <!-- Name Field -->
+          <div :class="highlightIfChanged('name')">
+            <label for="name" class="block text-lg font-medium">Name:</label>
+            <input
+              id="name"
+              v-model="botStore.botForm.name"
+              type="text"
+              class="w-full p-3 rounded-lg border"
+              required
+            />
+          </div>
+
+          <!-- Subtitle Field -->
+          <div :class="highlightIfChanged('subtitle')">
+            <label for="subtitle" class="block text-lg font-medium">Subtitle:</label>
+            <input
+              id="subtitle"
+              v-model="botStore.botForm.subtitle"
+              type="text"
+              class="w-full p-3 rounded-lg border"
+            />
+          </div>
+
+          <!-- Description Field -->
+          <div :class="highlightIfChanged('description')">
+            <label for="description" class="block text-lg font-medium">Description:</label>
+            <textarea
+              id="description"
+              v-model="botStore.botForm.description"
+              rows="4"
+              class="w-full p-3 rounded-lg border"
+              placeholder="Enter a short description for the bot"
+            ></textarea>
+          </div>
+
+          <!-- Bot Intro Field -->
+          <div :class="highlightIfChanged('botIntro')">
+            <label for="botIntro" class="block text-lg font-medium">Bot Intro:</label>
+            <textarea
+              id="botIntro"
+              v-model="botStore.botForm.botIntro"
+              rows="4"
+              class="w-full p-3 rounded-lg border"
+              placeholder="Enter the introduction sent to the language modeller"
+            ></textarea>
+          </div>
+        </div>
       </div>
     </div>
 
-    <!-- Feedback Message -->
-    <div
-      v-if="botFeedbackMessage"
-      :class="botFeedbackClass"
-      class="p-3 rounded-lg w-full text-center col-span-full"
-    >
-      {{ botFeedbackMessage }}
-    </div>
-
-    <!-- Designer Field -->
-    <div class="col-span-full sm:col-span-1">
-      <label for="designer" class="block text-lg font-medium">Designer:</label>
-      <input
-        v-if="canEditDesigner"
-        id="designer"
-        v-model="botStore.botForm.designer"
-        type="text"
-        class="w-full p-3 rounded-lg border"
-      />
-      <p v-else>{{ botStore.botForm.designer }}</p>
-    </div>
-
-    <!-- Avatar Image and Generation Component -->
-    <div class="col-span-full sm:col-span-1 lg:col-span-1">
-      <generate-avatar />
-    </div>
-
-    <!-- Form for Bot Details -->
+    <!-- Form for User Prompts and Submit Button -->
     <form
-      class="bg-white shadow-md rounded-xl p-6 w-full col-span-full"
+      class="bg-white shadow-md rounded-xl p-6 w-full mt-4"
       @submit.prevent="handleSubmit"
     >
-      <div
-        class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-      >
-        <!-- Name Field -->
-        <div
-          :class="highlightIfChanged('name')"
-          class="col-span-full sm:col-span-1"
-        >
-          <label for="name" class="block text-lg font-medium">Name:</label>
-          <input
-            id="name"
-            v-model="botStore.botForm.name"
-            type="text"
-            class="w-full p-3 rounded-lg border"
-            required
-          />
-        </div>
+      <label for="userIntro" class="block text-lg font-medium">User Prompts:</label>
+      <prompt-creator />
 
-        <!-- Subtitle Field -->
-        <div
-          :class="highlightIfChanged('subtitle')"
-          class="col-span-full sm:col-span-1"
-        >
-          <label for="subtitle" class="block text-lg font-medium"
-            >Subtitle:</label
-          >
-          <input
-            id="subtitle"
-            v-model="botStore.botForm.subtitle"
-            type="text"
-            class="w-full p-3 rounded-lg border"
-          />
-        </div>
-
-        <!-- Description Field -->
-        <div :class="highlightIfChanged('description')" class="col-span-full">
-          <label for="description" class="block text-lg font-medium"
-            >Description:</label
-          >
-          <textarea
-            id="description"
-            v-model="botStore.botForm.description"
-            rows="4"
-            class="w-full p-3 rounded-lg border"
-            placeholder="Enter a short description for the bot"
-          ></textarea>
-        </div>
-
-        <!-- Bot Intro Field -->
-        <div :class="highlightIfChanged('botIntro')" class="col-span-full">
-          <label for="botIntro" class="block text-lg font-medium"
-            >Bot Intro:</label
-          >
-          <textarea
-            id="botIntro"
-            v-model="botStore.botForm.botIntro"
-            rows="4"
-            class="w-full p-3 rounded-lg border"
-            placeholder="Enter the introduction sent to the language modeller"
-          ></textarea>
-        </div>
-
-        <!-- Prompt Creator Component -->
-        <div class="col-span-full">
-          <label for="userIntro" class="block text-lg font-medium"
-            >User Prompts:</label
-          >
-          <prompt-creator />
-        </div>
-      </div>
-
-      <!-- Form Feedback & Submit Buttons -->
-      <div v-if="isLoading" class="loading loading-ring loading-lg"></div>
+      <div v-if="isLoading" class="loading loading-ring loading-lg mt-4"></div>
       <div v-if="errorMessage" class="text-red-500 mt-2">
         {{ errorMessage }}
       </div>
@@ -158,7 +139,6 @@
         {{ successMessage }}
       </div>
 
-      <!-- Submit Button -->
       <div class="flex flex-col md:flex-row md:space-x-4 mt-6">
         <button
           type="submit"
@@ -166,14 +146,13 @@
           :disabled="isLoading"
         >
           <span v-if="isLoading">Saving...</span>
-          <span v-else>{{
-            botStore.selectedBotId ? 'Update Bot' : 'Create New Bot'
-          }}</span>
+          <span v-else>{{ botStore.selectedBotId ? 'Update Bot' : 'Create New Bot' }}</span>
         </button>
       </div>
     </form>
   </div>
 </template>
+
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue'

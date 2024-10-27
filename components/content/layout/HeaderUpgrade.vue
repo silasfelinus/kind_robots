8f33b13223c779d489bf188647e2679953908040
Silas Knight <silasfelinus@gmail.com>
2024-10-27 04:43:17 -0700

    translucent background

--- components/content/layout/HeaderUpgrade.vue
+++ components/content/layout/HeaderUpgrade.vue
@@ -1,53 +1,45 @@
 <template>
   <header
-    class="flex items-center h-full justify-between bg-base-300 rounded-2xl border-4 p-1 z-30 max-w-full box-border"
-    :style="{
-      height: displayStore.headerHeight,
-    }"
+    class="flex items-center justify-between bg-base-300 rounded-2xl border-4 p-2 max-w-full box-border"
+    :style="{ height: displayStore.headerHeight }"
   >
     <!-- Left Section: Avatar -->
-    <div class="flex items-center justify-center w-[15%] h-full rounded-2xl relative">
+    <div
+      class="flex items-center justify-center w-[15%] h-full relative rounded-2xl"
+    >
       <avatar-image
         alt="User Avatar"
         class="w-full h-full rounded-2xl object-cover"
       />
-      <!-- Special Overlay: Shows viewportSize from displayStore, positioned inside avatar-image -->
+      <!-- Special Overlay: Shows viewportSize, positioned inside avatar-image -->
       <div
-        class="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-white bg-primary rounded-lg text-xs md:text-sm lg:text-base p-1"
+        class="absolute bottom-2 right-2 text-white bg-primary rounded-md text-xs md:text-sm lg:text-base p-1"
       >
         {{ displayStore.viewportSize }}
       </div>
     </div>
 
-    <!-- Second Section: Title and Subtitle -->
+    <!-- Middle Section: Title and Subtitle -->
     <div
-      class="max-w-[40%] flex-grow flex flex-col items-center justify-center text-center mx-2 box-border"
+      class="flex flex-col justify-center items-center text-center mx-2 flex-grow max-w-[50%]"
     >
       <h1
-        class="text-xs md:text-sm lg:text-md xl:text-xl font-semibold text-ellipsis overflow-hidden whitespace-nowrap max-w-full"
+        class="text-lg md:text-xl lg:text-2xl xl:text-3xl font-semibold max-w-full text-ellipsis overflow-hidden whitespace-nowrap"
       >
         The {{ page.title || 'Room' }} Room
       </h1>
       <h2
-        class="text-xs md:text-sm lg:text-lg xl:text-xl italic text-ellipsis overflow-hidden whitespace-nowrap max-w-full"
+        class="text-md md:text-lg lg:text-xl xl:text-2xl italic max-w-full text-ellipsis overflow-hidden whitespace-nowrap"
       >
         {{ subtitle }}
       </h2>
     </div>
 
     <!-- Right Section: Icons and Login -->
-    <div
-      class="flex items-center justify-end space-x-1 box-border"
-      :style="{
-        width: '45%', // This section takes 45% for three elements each taking 15%
-      }"
-    >
-      <!-- Login Button -->
-      <login-path class="w-1/3 box-border" />
-      <!-- Theme Icon -->
-      <theme-icon class="w-1/3 box-border" />
-      <!-- Butterfly Toggle Icon -->
-      <swarm-icon class="w-1/3 box-border" />
+    <div class="flex items-center justify-end space-x-2 w-[30%]">
+      <login-path class="w-1/3" />
+      <theme-icon class="w-1/3" />
+      <swarm-icon class="w-1/3" />
     </div>
   </header>
 </template>

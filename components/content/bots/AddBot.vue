83182c09ce45b1a0f6b59bbc633ee38a5e988ce8
Silas M Knight <silasfelinus@gmail.com>
2024-10-27 23:52:40 -0700

    Update AddBot.vue

--- components/content/bots/AddBot.vue
+++ components/content/bots/AddBot.vue
@@ -1,151 +1,255 @@
 <template>
   <div
-    class="bg-primary bg-opacity-30 border border-accent rounded-2xl p-2 m-2 transition-all duration-300 ease-in-out hover:shadow-lg cursor-pointer flex flex-col justify-between h-full"
-    @click="selectArt"
+    class="rounded-2xl border p-4 m-4 mx-auto bg-base-200 grid gap-4 grid-cols-1"
   >
-    <!-- Art Information -->
-    <h3 class="text-lg font-semibold mb-2 truncate text-center" title="Prompt">
-      {{ art?.promptString || 'No prompt available' }}
-    </h3>
+    <h1 class="text-4xl text-center col-span-full">Create or Edit a Bot</h1>
 
-    <!-- Image Section -->
+    <!-- Top section with Bot Selector, Designer, and Toggles -->
     <div
-      class="relative flex-grow flex justify-center items-center overflow-hidden"
-      :class="fullscreenMode ? 'h-screen' : 'max-h-[50vh]'"
+      class="flex flex-wrap justify-between items-center col-span-full gap-4"
     >
-      <!-- Display artImage.imageData (base64) or art.path -->
-      <img
-        :src="computedArtImage"
-        alt="Artwork"
-        class="rounded-2xl transition-transform ease-in-out hover:scale-105 w-full object-cover cursor-pointer"
-        :class="
-          fullscreenMode ? 'h-auto w-auto max-h-screen max-w-screen' : 'h-auto'
-        "
-        loading="lazy"
-        @click.stop="toggleFullscreenMode"
-      />
-    </div>
+      <div class="w-full lg:w-auto">
+        <bot-selector />
+      </div>
 
-    <!-- Display if showing art-image or art-path -->
-    <div class="text-center mt-2">
-      <span class="text-sm text-info">
-        Displaying: {{ showArtImage.value ? 'Art Image (Base64)' : 'Art Path' }}
-      </span>
-    </div>
+      <!-- Add Bot Icon (only shows if currentBot exists) -->
+      <div v-if="botStore.currentBot" class="flex items-center">
+        <button class="btn btn-icon" @click="deselectCurrentBot">
+          <icon name="kind-icon:addbot" class="text-3xl" />
+        </button>
+        <p class="ml-2">Create new bot</p>
+      </div>
 
-    <!-- Art Metadata -->
-    <div class="mt-2 flex flex-col items-center">
-      <p class="text-base truncate" title="Pitch">
-        {{ art?.pitchId || 'No pitch available' }}
-      </p>
-      <div class="flex justify-between items-center w-full mt-2 px-4">
-        <p class="text-base">Claps: {{ reactions.length || 0 }}</p>
-        <p class="text-base">
-          isPublic?:
-          <span class="font-semibold">{{ art.isPublic ? 'Yes' : 'No' }}</span>
-        </p>
+      <!-- Designer Field -->
+      <div class="flex-grow w-1/4">
+        <label for="designer" class="block text-lg font-medium"
+          >Designer:</label
+        >
+        <input
+          v-if="canEditDesigner"
+          id="designer"
+          v-model="botStore.botForm.designer"
+          type="text"
+          class="w-full p-3 rounded-lg border"
+        />
+        <p v-else>{{ botStore.botForm.designer }}</p>
+      </div>
+
+      <!-- Toggles for Public Visibility and Under Construction -->
+      <div class="flex gap-4">
+        <div>
+          <label class="block text-lg font-medium">Public Visibility:</label>
+          <div class="flex space-x-2">
+            <button
+              type="button"
+              :class="{
+                'btn btn-primary': botStore.botForm.isPublic,
+                'btn btn-grey-200': !botStore.botForm.isPublic,
+              }"
+              @click="toggleVisibility(true)"
+            >
+              Public
+            </button>
+            <button
+              type="button"
+              :class="{
+                'btn btn-primary': !botStore.botForm.isPublic,
+                'btn btn-grey-200': botStore.botForm.isPublic,
+              }"
+              @click="toggleVisibility(false)"
+            >
+              Private
+            </button>
+          </div>
+        </div>
+
+        <div class="flex items-center">
+          <input
+            id="underConstruction"
+            v-model="botStore.botForm.underConstruction"
+            type="checkbox"
+            class="mr-2"
+          />
+          <label for="underConstruction" class="block text-lg font-medium">
+            Under construction
+          </label>
+        </div>
       </div>
     </div>
 
-    <!-- Toggle Button for Art Details -->
-    <div class="mt-4 flex justify-center">
-      <button
-        class="bg-secondary text-white rounded-lg px-4 py-2"
-        @click="toggleDetails"
-      >
-        {{ showDetails ? 'Hide' : 'Show' }} Details
-      </button>
+    <!-- Middle section - Avatar Generator on the left, Bot Name & Details on the right -->
+    <div class="flex flex-wrap w-full mt-4">
+      <!-- Avatar Image and Generation Component -->
+      <div class="w-full md:w-1/2 lg:w-1/2 p-4">
+        <generate-avatar />
+      </div>
+
+      <!-- Bot Details: Name, Subtitle, Description, and Bot Intro -->
+      <div class="w-full md:w-1/2 lg:w-1/2 p-4">
+        <div class="grid gap-4">
+          <!-- Name Field -->
+          <div :class="highlightIfChanged('name')">
+            <label for="name" class="block text-lg font-medium">Name:</label>
+            <input
+              id="name"
+              v-model="botStore.botForm.name"
+              type="text"
+              class="w-full p-3 rounded-lg border"
+              required
+            />
+          </div>
+
+          <!-- Subtitle Field -->
+          <div :class="highlightIfChanged('subtitle')">
+            <label for="subtitle" class="block text-lg font-medium"
+              >Subtitle:</label
+            >
+            <input
+              id="subtitle"
+              v-model="botStore.botForm.subtitle"
+              type="text"
+              class="w-full p-3 rounded-lg border"
+            />
+          </div>
+
+          <!-- Description Field -->
+          <div :class="highlightIfChanged('description')">
+            <label for="description" class="block text-lg font-medium"
+              >Description:</label
+            >
+            <input
+              id="description"
+              v-model="botStore.botForm.description"
+              rows="4"
+              class="w-full p-3 rounded-lg border"
+              placeholder="Enter a short description for the bot"
+            />
+          </div>
+
+          <!-- Bot Intro Field -->
+          <div :class="highlightIfChanged('botIntro')">
+            <label for="botIntro" class="block text-lg font-medium"
+              >Bot Intro:</label
+            >
+            <textarea
+              id="botIntro"
+              v-model="botStore.botForm.botIntro"
+              rows="4"
+              class="w-full p-3 rounded-lg border"
+              placeholder="Enter the introduction sent to the language modeller"
+            ></textarea>
+          </div>
+
+          <label for="userIntro" class="block text-lg font-medium"
+            >User Prompts:</label
+          >
+          <prompt-creator />
+        </div>
+      </div>
     </div>
 
-    <!-- Art Details Toggle Section -->
-    <div
-      v-if="showDetails"
-      class="mt-4 p-4 bg-base-200 overflow-y-auto rounded-xl"
+    <!-- Form for User Prompts and Submit Button -->
+    <form
+      class="bg-white shadow-md rounded-xl p-6 w-full mt-4"
+      @submit.prevent="handleSubmit"
     >
-      <pre class="text-sm whitespace-pre-wrap">{{ artData }}</pre>
-      <!-- Show artImage details if available -->
-      <pre v-if="hasImage" class="text-sm whitespace-pre-wrap">{{ localArtImage }}</pre>
-    </div>
+      <div v-if="isLoading" class="loading loading-ring loading-lg mt-4"></div>
+      <div v-if="errorMessage" class="text-red-500 mt-2">
+        {{ errorMessage }}
+      </div>
+      <div v-if="successMessage" class="text-green-500 mt-2">
+        {{ successMessage }}
+      </div>
+
+      <div class="flex flex-col md:flex-row md:space-x-4 mt-6">
+        <button
+          type="submit"
+          class="btn btn-primary w-full md:w-auto"
+          :disabled="isLoading"
+        >
+          <span v-if="isLoading">Saving...</span>
+          <span v-else>{{
+            botStore.selectedBotId ? 'Update Bot' : 'Create New Bot'
+          }}</span>
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue'
-import { useArtStore } from '@/stores/artStore'
-import { usePromptStore } from '@/stores/promptStore'
-import { useReactionStore } from '@/stores/reactionStore'
+import { useBotStore } from './../../../stores/botStore'
+import { useGalleryStore } from './../../../stores/galleryStore'
+import { useUserStore } from './../../../stores/userStore'
 
-const props = defineProps<{
-  art: Art
-  artImage?: ArtImage
-}>()
+const botStore = useBotStore()
+const galleryStore = useGalleryStore()
+const userStore = useUserStore()
 
-const localArtImage = ref<ArtImage | null>(null)
-const showArtImage = ref(false)
-const fullscreenMode = ref(false)
-const showDetails = ref(false)
+const isLoading = ref(false)
+const errorMessage = ref<string | null>(null)
+const successMessage = ref<string | null>(null)
+const botFeedbackMessage = ref<string | null>(null)
 
-const hasImage = computed(() => !!(localArtImage.value || props.artImage?.imageData))
-
-const artStore = useArtStore()
-const promptStore = usePromptStore()
-const reactionStore = useReactionStore()
-
-const artData = computed(() => props.art)
-
-const reactions = computed(() =>
-  reactionStore.reactions.filter((r) => r.artId === props.art.id),
+// Determine if the designer field can be edited
+const canEditDesigner = computed(
+  () => !botStore.selectedBotId || botStore.botForm.userId === userStore.userId,
 )
 
-// Computed for artImage or fallback to path
-const computedArtImage = computed(() => {
-  if (showArtImage.value && localArtImage.value?.imageData) {
-    return `data:image/${localArtImage.value.fileType};base64,${localArtImage.value.imageData}`  // Use dynamic file type
-  } else if (props.artImage?.imageData) {
-    return `data:image/${props.artImage.fileType};base64,${props.artImage.imageData}`  // Use dynamic file type
-  } else if (props.art.path) {
-    return props.art.path
+// Highlight any fields that have changed compared to the original bot
+function highlightIfChanged(field: string) {
+  const original = botStore.currentBot || {}
+  if (
+    botStore.botForm[field as keyof typeof botStore.botForm] !==
+    original[field as keyof typeof original]
+  ) {
+    return 'border border-info' // Highlight changed fields
   }
-  return '/images/backtree.webp'
-})
+  return ''
+}
 
-onMounted(() => {
-  if (props.artImage?.imageData || props.art.artImageId) {
-    showArtImage.value = true
-    if (!props.artImage?.imageData) {
-      fetchArtImage()
-    }
-  }
-})
+function toggleVisibility(value: boolean) {
+  botStore.botForm.isPublic = value
+}
 
-const fetchArtImage = async () => {
+async function handleSubmit() {
+  isLoading.value = true
+  errorMessage.value = null
+  successMessage.value = null
+
   try {
-    const artImage = await artStore.fetchArtImageById(props.art.artImageId)
-    if (artImage) {
-      localArtImage.value = artImage
+    if (botStore.selectedBotId) {
+      // Update an existing bot
+      await botStore.updateBot(botStore.selectedBotId)
+      successMessage.value = 'Bot updated successfully!'
+      botFeedbackMessage.value = 'Bot saved successfully!'
+    } else {
+      // Create a new bot
+      const newBot = await botStore.addBot(botStore.botForm)
+      if (newBot) {
+        botStore.currentBot = newBot // Set the currentBot to the newly created bot
+        botStore.botForm = { ...newBot } // Also set botForm to the newly created bot
+        successMessage.value = 'New bot created successfully!'
+        botFeedbackMessage.value = 'New bot saved successfully!'
+      } else {
+        errorMessage.value = 'Failed to create a new bot.'
+      }
     }
   } catch (error) {
-    console.error('Error fetching art image:', error)
+    console.error('Error editing bot:', error)
+    errorMessage.value = Error editing bot: ${error}
+  } finally {
+    isLoading.value = false
   }
 }
 
-const toggleFullscreenMode = () => {
-  fullscreenMode.value = !fullscreenMode.value
+function deselectCurrentBot() {
+  botStore.deselectBot() // Deselects the current bot via store action
 }
 
-const toggleDetails = () => {
-  showDetails.value = !showDetails.value
-}
-
-const selectArt = () => {
-  artStore.selectArt(props.art.id)
-}
+onMounted(async () => {
+  await botStore.loadStore()
+  await galleryStore.initializeStore()
+})
 </script>
-
-<style scoped>
-.fullscreen-img {
-  max-width: 100vw;
-  max-height: 100vh;
-  object-fit: contain;
-}
-</style>

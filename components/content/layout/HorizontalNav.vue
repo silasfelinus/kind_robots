5bfe2682b9b7a2605012ba743f23b7c891589947
Silas Knight <silasfelinus@gmail.com>
2024-11-03 19:28:37 -0800

    tweaking sizes to fit better in large displays

--- components/content/layout/HorizontalNav.vue
+++ components/content/layout/HorizontalNav.vue
@@ -17,7 +17,7 @@
         >
           <icon
             :name="item.icon"
-            class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-accent transition-transform group-hover:scale-110"
+            class="w-8 h-8 md:w-10 md:h-10 xl:w-12 xl:h-12 text-accent transition-transform group-hover:scale-110"
           />
 
           <span

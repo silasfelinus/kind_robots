2ee3c48006bf242497d66805bd0953035edeae6f
Silas M Knight <silasfelinus@gmail.com>
2024-11-06 09:25:07 -0800

    Update count.get.ts

--- server/api/galleries/count.get.ts
+++ server/api/galleries/count.get.ts
@@ -3,20 +3,30 @@ import { defineEventHandler } from 'h3'
 import { countGalleries } from '.'
 import { errorHandler } from '../utils/error'
 
-export default defineEventHandler(async () => {
+type CountResponse = {
+  success: boolean
+  message?: string
+  count?: number
+  statusCode?: number
+}
+
+export default defineEventHandler(async (): Promise<CountResponse> => {
+  let response: CountResponse
   try {
     // Get the count of galleries
     const count = await countGalleries()
 
     // Return the success response with count
-    return { success: true, count }
+    response = { success: true, count, message: 'Galleries count retrieved successfully.', statusCode: 200 }
   } catch (error: unknown) {
     // Use the errorHandler to handle and format the error
     const handledError = errorHandler(error)
-    return {
+    response = {
       success: false,
       message: handledError.message || 'Failed to get galleries count.',
       statusCode: handledError.statusCode || 500,
     }
   }
+
+  return response
 })

bbb1cff17c28ffb2423583cdd8da223d90bda196
Silas M Knight <silasfelinus@gmail.com>
2024-11-06 08:51:29 -0800

    Update index.get.ts

--- server/api/chats/public/index.get.ts
+++ server/api/chats/public/index.get.ts
@@ -1,16 +1,33 @@
-//server/api/chats/public/index.get.ts
+// /server/api/chats/public/index.get.ts
 import { defineEventHandler } from 'h3'
 import type { ChatExchange } from '@prisma/client'
 import { errorHandler } from '../../utils/error'
 import prisma from '../../utils/prisma'
 
 export default defineEventHandler(async (event) => {
+  let response
+
   try {
     const publicChatExchanges = await fetchPublicChatExchanges()
-    return { success: true, chatExchanges: publicChatExchanges }
+    response = {
+      success: true,
+      data: {
+        chatExchanges: publicChatExchanges,
+      },
+      statusCode: 200,
+    }
+    event.node.res.statusCode = 200
   } catch (error: unknown) {
-    return errorHandler(error)
+    const handledError = errorHandler(error)
+    event.node.res.statusCode = handledError.statusCode || 500
+    response = {
+      success: false,
+      message: handledError.message || 'Failed to retrieve public chats.',
+      statusCode: event.node.res.statusCode,
+    }
   }
+
+  return response
 })
 
 // Function to fetch all public Chat Exchanges

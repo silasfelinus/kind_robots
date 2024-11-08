022b4d9476a0678d072cced4d2f4c00b3e61ac92
Silas Knight <silasfelinus@gmail.com>
2024-11-08 14:35:09 -0800

    art collections returns proper object

--- cypress/e2e/api/art.cy.ts
+++ cypress/e2e/api/art.cy.ts
@@ -122,8 +122,8 @@ describe('Art Management API Tests', () => {
     }).then((response) => {
       expect(response.status).to.eq(201)
       expect(response.body.success).to.be.true
-      expect(response.body.data).to.be.an('object').that.is.not.empty
-      artId = response.body.data.id
+      expect(response.body.data.newArt).to.be.an('object').that.is.not.empty
+      artId = response.body.data.newArtid
     })
   })
 

5a4c2a0efe1127ba2fb7c7f95564e1fee666c82e
Silas Knight <silasfelinus@gmail.com>
2024-11-09 13:45:24 -0800

    refactoring auth

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -4,37 +4,33 @@ import { validateUserCredentials } from '.'
 
 export default defineEventHandler(async (event) => {
   try {
-    // Read and destructure the body from the event
+    // Extract and destructure the username and password from the request body
     const { username, password } = await readBody<{
       username: string
       password: string
     }>(event)
 
-    // Validate the user credentials
+    // Validate the provided credentials
     const result = await validateUserCredentials(username, password)
     if (result && result.user) {
-      // Extract the user data and token directly into the data object
+      // Create the response payload with user details and token
       const data = {
         ...result.user,
         token: result.token,
       }
 
-      // Return success response with data containing user info and token
+      // Return success response
       return { success: true, data }
     } else {
-      // Throw an error if credentials are invalid
-      event.node.res.statusCode = 401 // Set status to 401 Unauthorized
+      // Set response status and return invalid credentials message
+      event.node.res.statusCode = 401
       return { success: false, message: 'Invalid credentials' }
     }
   } catch (error: unknown) {
-    // Log the error and return a failure response
+    // Log the error and use sendError to manage unexpected issues
     console.error('Error during login:', error)
-
-    // Safely handle unknown errors
     const errorMessage =
       error instanceof Error ? error.message : 'Unknown error occurred'
-
-    // Use sendError to set an appropriate status code if not already set
     return sendError(event, new Error(errorMessage))
   }
 })

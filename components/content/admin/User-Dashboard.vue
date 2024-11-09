7dd4e6c6979b43174f34f31437b6b27d674052c5
Silas M Knight <silasfelinus@gmail.com>
2024-11-08 18:56:45 -0800

    Update User-Dashboard.vue

--- components/content/admin/User-Dashboard.vue
+++ components/content/admin/User-Dashboard.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    :class="['bg-base-300 p-2 rounded-2xl relative', { 'h-32': isMinimized }]"
-  >
+  <div :class="['bg-base-300 p-2 rounded-2xl relative', { 'h-32': isMinimized }]">
     <button
       v-if="!isMinimized"
       class="absolute top-1 left-1 z-10"
@@ -9,20 +7,14 @@
     >
       <Icon name="kind-icon:expand" class="text-lg" />
     </button>
-    <span class="absolute top-2 right-2"
-      >Role: {{ user?.Role || 'Guest' }}</span
-    >
+    <span class="absolute top-2 right-2">Role: {{ user?.Role || 'Guest' }}</span>
 
     <div v-if="!isMinimized">
       <h1 class="text-2xl font-semibold ml-6">User Dashboard</h1>
       <div class="relative flex justify-center items-center">
         <user-avatar />
-        <div
-          class="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2"
-        >
-          <h2
-            class="text-lg font-semibold bg-base-300 border-accent rounded-2xl border p-2 pt-1 pb-1"
-          >
+        <div class="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2">
+          <h2 class="text-lg font-semibold bg-base-300 border-accent rounded-2xl border p-2 pt-1 pb-1">
             {{ user?.username || 'Kind Guest' }}
           </h2>
         </div>
@@ -31,9 +23,7 @@
         <div>
           <p class="text-lg font-medium m-2 mt-4 p-1 pb-0">
             Welcome, {{ user?.username || 'Guest' }}
-            <span v-if="!isLoggedIn" class="text-sm text-gray-500 ml-2"
-              >(Not logged in)</span
-            >
+            <span v-if="!isLoggedIn" class="text-sm text-gray-500 ml-2">(Not logged in)</span>
           </p>
           <div class="flex space-x-4 mt-2">
             <div class="flex items-center space-x-2">
@@ -50,8 +40,17 @@
             Logout
           </button>
         </div>
+        <div v-else>
+          <button
+            class="bg-primary p-2 rounded-lg text-white text-lg"
+            @click="showLogin = true"
+          >
+            Login
+          </button>
+        </div>
       </div>
       <login-form v-if="showLogin" @close="showLogin = false" />
+      <user-panel v-if="isLoggedIn" />
       <div class="flex flex-row">
         <theme-toggle class="flex flex-row" />
       </div>
@@ -71,17 +70,20 @@
           <span>{{ user?.mana || 0 }}</span>
         </div>
         <button
-          v-if="isLoggedIn"
-          class="bg-warning rounded-lg text-white text-lg"
+          :class="[
+            'rounded-lg text-white text-lg',
+            isLoggedIn ? 'bg-warning' : 'bg-primary',
+          ]"
           @click.stop="handleButtonClick"
         >
-          Logout
+          {{ isLoggedIn ? 'Logout' : 'Login' }}
         </button>
         <theme-toggle class="flex flex-row" />
       </div>
     </div>
   </div>
 </template>
+
 
 <script lang="ts" setup>
 import { ref, computed, watch } from 'vue'

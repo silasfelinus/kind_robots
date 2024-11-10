ece6d47b6dfffa5c5ea5349fb016dbcd2dcd7371
Silas Knight <silasfelinus@gmail.com>
2024-11-09 16:59:04 -0800

    remodelling milestone display

--- components/content/milestones/milestone-reward.vue
+++ components/content/milestones/milestone-reward.vue
@@ -6,20 +6,28 @@
       class="bg-primary text-white rounded-2xl p-4 m-4 border"
       @click="togglePopup"
     >
-      <Icon :name="milestone?.icon || 'kind-icon:map'" class="h-16 w-16" />
+      <Icon
+        :name="milestone?.icon ?? 'kind-icon:default-icon'"
+        class="h-16 w-16"
+      />
     </button>
 
     <!-- Popup Content -->
     <div
       v-if="showPopup"
-      class="flex justify-center rounded-2xl items-center z-40 bg-base-300 p-2 m-2 border"
+      class="flex justify-center rounded-2xl items-center z-50 bg-base-300 p-2 m-2 border shadow-xl"
     >
-      <div class="bg-base-400 rounded-2xl p-6 m-6 text-center relative">
+      <div
+        class="bg-base-400 rounded-2xl p-6 m-6 text-center relative max-w-sm"
+      >
         <h2 class="text-2xl font-semibold mb-4">
           Congratulations, {{ userStore.username }}!
         </h2>
-        <div v-if="milestone?.icon">
-          <Icon :name="milestone.icon" class="h-16 w-16 mx-auto mb-4" />
+        <div v-if="milestone">
+          <Icon
+            :name="milestone.icon ?? 'kind-icon:default-icon'"
+            class="h-16 w-16 mx-auto mb-4"
+          />
           <p class="text-xl font-medium">
             🌟 You earned the {{ milestone.label }} milestone! 🌟
           </p>
@@ -36,7 +44,7 @@
           </div>
           <button
             class="bg-primary text-white rounded-2xl border px-4 py-2 mt-4"
-            @click="closePopup"
+            @click="togglePopup"
           >
             Yay! (Close)
           </button>
@@ -45,31 +53,24 @@
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { useUserStore } from './../../../stores/userStore'
-import {
-  useMilestoneStore,
-  type Milestone,
-} from './../../../stores/milestoneStore'
-import { useErrorStore } from './../../../stores/errorStore'
-import { useConfetti } from './../../../utils/useConfetti'
+import { useUserStore } from '@/stores/userStore'
+import { useMilestoneStore, type Milestone } from '@/stores/milestoneStore'
+import { useErrorStore } from '@/stores/errorStore'
+import { useConfetti } from '@/utils/useConfetti'
 
 const props = defineProps<{ id: number }>()
 const { triggerConfetti } = useConfetti()
 const userStore = useUserStore()
 const milestoneStore = useMilestoneStore()
-const errorStore = useErrorStore() // Initialize errorStore
+const errorStore = useErrorStore()
 const milestone = ref<Milestone | null>(null)
-
 const showPopup = ref(false)
 
 const togglePopup = () => {
   showPopup.value = !showPopup.value
-}
-
-const closePopup = () => {
-  showPopup.value = false
 }
 
 const validateMilestoneRecord = async () => {
@@ -79,7 +80,7 @@ const validateMilestoneRecord = async () => {
       milestoneStore.hasMilestone(userStore.userId, milestone.value.id)
     ) {
       console.log('Milestone already rewarded, closing popup.')
-      showPopup.value = false // Close the popup if milestone already rewarded
+      showPopup.value = false
       return 'success'
     }
     if (milestone.value) {
@@ -88,14 +89,10 @@ const validateMilestoneRecord = async () => {
         userStore.userId,
         milestone.value.id,
       )
-      if (result.success) {
-        console.log('Successfully validated milestone', result)
-      } else {
-        throw new Error(result.message)
-      }
+      if (!result.success) throw new Error(result.message)
     }
   } catch (error: unknown) {
-    errorStore.setError(ErrorType.GENERAL_ERROR, error) // Use errorStore for error handling
+    errorStore.setError(ErrorType.GENERAL_ERROR, error)
     console.error('Failed to validate milestone', errorStore.message)
   }
 }
@@ -103,19 +100,23 @@ const validateMilestoneRecord = async () => {
 const validateUserRecord = async () => {
   try {
     const result = await userStore.updateKarmaAndMana()
-    if (result.success) {
-      console.log('Successfully validated user', result)
-    } else {
-      throw new Error(result.message)
-    }
+    if (!result.success) throw new Error(result.message)
   } catch (error: unknown) {
-    errorStore.setError(ErrorType.GENERAL_ERROR, error) // Use errorStore for error handling
+    errorStore.setError(ErrorType.GENERAL_ERROR, error)
     console.error('Failed to validate user record', errorStore.message)
   }
 }
 
 onMounted(async () => {
-  await milestoneStore.fetchMilestoneById(props.id || 10)
+  const response = await milestoneStore.fetchMilestoneById(props.id || 10)
+
+  if (response.success && response.data) {
+    milestone.value = response.data.milestone
+  } else {
+    milestone.value = null
+    errorStore.setError(ErrorType.GENERAL_ERROR, response.message)
+  }
+
   showPopup.value = true
   if (userStore.userId !== 0) {
     await Promise.all([validateMilestoneRecord(), validateUserRecord()])

8b267de3a1b8e2847fdaf063231ba7807f13e012
Silas M Knight <silasfelinus@gmail.com>
2024-11-12 14:13:37 -0800

    Update title-menu.vue

---
+++ components/content/prompts/title-menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Select a Title</h2>
+    <ul>
+      <li
+        v-for="title in pitchStore.titles"
+        :key="title.id"
+        @click="setSelectedTitle(title.id)"
+        :class="{ 'selected': pitchStore.selectedTitle?.id === title.id }"
+      >
+        {{ title.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { usePitchStore } from '~/stores/pitchStore'
+const pitchStore = usePitchStore()
+
+onMounted(() => {
+  pitchStore.fetchTitles()
+})
+
+function setSelectedTitle(titleId: number) {
+  pitchStore.setSelectedTitle(titleId)
+}
+</script>

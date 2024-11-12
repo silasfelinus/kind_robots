24e5dcdf718f83b5f691097a620bc06fd35b50e6
Silas M Knight <silasfelinus@gmail.com>
2024-11-12 14:11:03 -0800

    Update brainstorm-view.vue

---
+++ components/content/prompts/brainstorm-view.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <TitleMenu />
+    <TitleMaker />
+    <PitchDisplay />
+    <section v-if="pitchStore.brainstormResponse.success">
+      <h3>New Pitches</h3>
+      <ul>
+        <li
+          v-for="pitch in pitchStore.brainstormResponse.data"
+          :key="pitch.id"
+          class="highlighted-pitch"
+        >
+          {{ pitch.pitch }}
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TitleMenu from './TitleMenu.vue'
+import TitleMaker from './TitleMaker.vue'
+import PitchDisplay from './PitchDisplay.vue'
+import { usePitchStore } from '~/stores/pitchStore'
+
+const pitchStore = usePitchStore()
+</script>
+
+<style scoped>
+.highlighted-pitch {
+  color: #ff9800; /* Example color for highlighted pitches */
+  font-weight: bold;
+}
+</style>

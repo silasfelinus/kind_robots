721290649da22b87356d418391397ef1ff320717
Silas M Knight <silasfelinus@gmail.com>
2024-11-12 21:55:34 -0800

    Update brainstorm-image.vue

---
+++ components/content/prompts/brainstorm-image.vue
@@ -0,0 +1,15 @@
+<template>
+  <nuxt-img
+    src="/images/brainstorm.webp"
+    alt="Brainstorm"
+    class="w-full h-auto object-cover rounded-lg"
+  />
+</template>
+
+<script setup lang="ts">
+// This component simply displays the brainstorm image using nuxt-img
+</script>
+
+<style scoped>
+/* Optional styling adjustments */
+</style>

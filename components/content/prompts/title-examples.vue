07bb30459cc00fa39e458a7450e64be80334a5c4
Silas Knight <silasfelinus@gmail.com>
2024-11-13 16:21:50 -0800

    removed import of defineProps

--- components/content/prompts/title-examples.vue
+++ components/content/prompts/title-examples.vue
@@ -62,7 +62,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps } from 'vue'
+import { ref } from 'vue'
 import { usePitchStore, type Pitch } from '~/stores/pitchStore'
 
 const props = defineProps<{ pitch?: Partial<Pitch>; isEditing: boolean }>()

6178d2c7a3c7209ab37fa89b3cea59ef07a7a62e
Silas Knight <silasfelinus@gmail.com>
2024-11-15 13:00:44 -0800

    revised header orientation for consistent spacing

--- components/content/layout/HeaderUpgrade.vue
+++ components/content/layout/HeaderUpgrade.vue
@@ -3,10 +3,15 @@
     class="flex items-center justify-between bg-base-300 rounded-2xl border-1 md:border-2 lg:border-3 xl:border-4 max-w-full box-border"
     :style="{ height: displayStore.headerHeight }"
   >
-    <!-- Left Section: Avatar -->
-    <div class="flex items-center justify-center w-1/4 h-full relative rounded-2xl">
-      <avatar-image alt="User Avatar" class="w-full h-full rounded-2xl object-cover" />
-      <!-- Special Overlay: Shows viewportSize, positioned inside avatar-image -->
+    <!-- Section 1: Avatar -->
+    <div
+      class="flex items-center justify-center w-1/5 h-full relative rounded-2xl"
+    >
+      <avatar-image
+        alt="User Avatar"
+        class="w-full h-full rounded-2xl object-cover"
+      />
+      <!-- Overlay: Viewport size -->
       <div
         v-if="isAdmin"
         class="absolute bottom-2 right-2 text-white bg-primary rounded-md text-xs md:text-sm p-1"
@@ -15,33 +20,30 @@
       </div>
     </div>
 
-    <!-- Title and subtitle  -->
+    <!-- Section 2-4: Title and Subtitle -->
     <div
-      class="flex flex-col justify-center items-center text-center h-full w-1/2"
+      class="flex flex-col justify-center items-center text-center h-full w-3/5"
     >
       <h1
-        class="flex text-md md:text-lg lg:text-xl xl:text-2xl font-semibold w-full h-1/2 text-ellipsis sm:order-first"
+        class="text-md md:text-lg lg:text-xl xl:text-2xl font-semibold w-full text-ellipsis"
       >
         The {{ page.title || 'Room' }} Room
       </h1>
-        <h2
-          class="text-sm md:text-md flex lg:text-lg xl:text-xl italic text-ellipsis w-full h-1/2"
-        >
-          {{ subtitle }}
-        </h2>
+      <h2
+        class="text-sm md:text-md lg:text-lg xl:text-xl italic text-ellipsis w-full"
+      >
+        {{ subtitle }}
+      </h2>
     </div>
-<!-- Icons Section -->
-      <div class="flex flex-row justify-between h-full w-1/4">
-        
 
-        
-          <login-path class="flex w-1/3" />
-          <theme-icon class="flex w-1/3" />
-          <swarm-icon class="flex w-1/3" />
-        </div>
+    <!-- Section 5: Icons -->
+    <div class="flex justify-around items-center h-full w-1/5">
+      <login-path class="flex" />
+      <theme-icon class="flex" />
+      <swarm-icon class="flex" />
+    </div>
   </header>
 </template>
-
 
 <script setup lang="ts">
 import { computed } from 'vue'
@@ -55,5 +57,7 @@ const isAdmin = computed(() => userStore.user?.Role === 'ADMIN')
 
 // Access page content and subtitle
 const { page } = useContent()
-const subtitle = computed(() => page.value?.subtitle ?? 'Welcome to Kind Robots')
+const subtitle = computed(
+  () => page.value?.subtitle ?? 'Welcome to Kind Robots',
+)
 </script>

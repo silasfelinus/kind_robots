1e59df4a3fa395b34124c8ac56d376cd0c134769
Silas Knight <silasfelinus@gmail.com>
2024-11-15 14:00:22 -0800

    trying stacked small display

--- components/content/layout/HeaderUpgrade.vue
+++ components/content/layout/HeaderUpgrade.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="flex items-center justify-between bg-base-300 rounded-2xl border-1 md:border-2 lg:border-3 xl:border-4 max-w-full box-border"
+    class="flex flex-col md:flex-row items-center justify-between bg-base-300 rounded-2xl border-1 md:border-2 lg:border-3 xl:border-4 max-w-full box-border"
     :style="{ height: displayStore.headerHeight }"
   >
     <!-- Section 1: Avatar -->
@@ -19,28 +19,35 @@
         {{ displayStore.viewportSize }}
       </div>
     </div>
-
-    <!-- Section 2-4: Title and Subtitle -->
+    <!-- Title Section -->
     <div
-      class="flex flex-col justify-center items-center text-center h-full w-3/5"
+      class="flex flex-col md:w-3/5 w-full justify-center items-center text-center order-1 md:order-none"
     >
       <h1
         class="text-md md:text-lg lg:text-xl xl:text-2xl font-semibold w-full text-ellipsis"
       >
         The {{ page.title || 'Room' }} Room
       </h1>
+    </div>
+
+    <!-- Icons Section -->
+    <div
+      class="flex justify-around items-center w-full md:w-1/5 h-auto order-2 md:order-none my-2 md:my-0"
+    >
+      <login-path class="flex" />
+      <theme-icon class="flex" />
+      <swarm-icon class="flex" />
+    </div>
+
+    <!-- Subtitle Section -->
+    <div
+      class="flex flex-col md:w-3/5 w-full justify-center items-center text-center order-3 md:order-none"
+    >
       <h2
         class="text-sm md:text-md lg:text-lg xl:text-xl italic text-ellipsis w-full"
       >
         {{ subtitle }}
       </h2>
-    </div>
-
-    <!-- Section 5: Icons -->
-    <div class="flex justify-around items-center h-full w-1/5">
-      <login-path class="flex" />
-      <theme-icon class="flex" />
-      <swarm-icon class="flex" />
     </div>
   </header>
 </template>

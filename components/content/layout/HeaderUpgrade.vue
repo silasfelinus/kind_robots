40377d008a138ca691793b664ed243790e74efed
Silas Knight <silasfelinus@gmail.com>
2024-11-15 15:53:55 -0800

    adjustments for md and higher

--- components/content/layout/HeaderUpgrade.vue
+++ components/content/layout/HeaderUpgrade.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="relative flex md:flex-row flex-wrap sm:flex-nowrap items-center justify-between bg-base-300 rounded-2xl border-1 md:border-2 lg:border-3 xl:border-4 max-w-full box-border"
+    class="relative flex items-center justify-between bg-base-300 rounded-2xl border-1 md:border-2 lg:border-3 xl:border-4 max-w-full box-border"
     :style="{ height: displayStore.headerHeight }"
   >
     <!-- Avatar and Viewport Size -->
@@ -20,38 +20,12 @@
     </div>
 
     <!-- Responsive Layout -->
-    <div class="relative flex-1 h-full">
-      <!-- Small Screens -->
+    <div
+      class="flex-1 h-full flex flex-col md:flex-row items-center justify-between space-y-2 md:space-y-0"
+    >
+      <!-- Medium Screens: Title and Subtitle Stacked -->
       <div
-        class="absolute inset-0 flex flex-col justify-between sm:hidden space-y-2 p-2"
-      >
-        <!-- Title (Top) -->
-        <h1
-          class="absolute top-2 left-1/5 text-md font-semibold w-full text-ellipsis text-left"
-        >
-          The {{ page.title || 'Room' }} Room
-        </h1>
-
-        <!-- Subtitle (Bottom) -->
-        <h2
-          class="absolute bottom-2 left-1/5 text-sm italic text-ellipsis w-full text-left"
-        >
-          {{ subtitle }}
-        </h2>
-
-        <!-- Icons (Middle) -->
-        <div
-          class="absolute inset-y-1/2 left-1/5 transform -translate-y-1/2 flex justify-start items-center space-x-4"
-        >
-          <login-path class="flex" />
-          <theme-icon class="flex" />
-          <swarm-icon class="flex" />
-        </div>
-      </div>
-
-      <!-- Medium Screens -->
-      <div
-        class="hidden sm:flex md:flex-col md:justify-between items-center text-center space-y-2"
+        class="flex flex-col items-center md:items-start text-center md:text-left"
       >
         <!-- Title -->
         <h1
@@ -67,28 +41,11 @@
         </h2>
       </div>
 
-      <!-- Large Screens -->
-      <div
-        class="hidden lg:flex lg:flex-row lg:items-center lg:justify-between w-full space-x-4"
-      >
-        <!-- Title -->
-        <h1
-          class="text-lg lg:text-xl xl:text-2xl font-semibold text-ellipsis flex-grow text-left"
-        >
-          The {{ page.title || 'Room' }} Room
-        </h1>
-        <!-- Subtitle -->
-        <h2
-          class="text-md lg:text-lg xl:text-xl italic text-ellipsis flex-grow text-left"
-        >
-          {{ subtitle }}
-        </h2>
-        <!-- Icons -->
-        <div class="flex justify-end items-center space-x-4">
-          <login-path class="flex" />
-          <theme-icon class="flex" />
-          <swarm-icon class="flex" />
-        </div>
+      <!-- Icons -->
+      <div class="flex justify-end items-center space-x-4">
+        <login-path class="flex" />
+        <theme-icon class="flex" />
+        <swarm-icon class="flex" />
       </div>
     </div>
   </header>

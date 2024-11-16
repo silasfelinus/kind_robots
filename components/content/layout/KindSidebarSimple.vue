a30fbdcf68495e4a25c0b2e9dc235bdcea316666
Silas M Knight <silasfelinus@gmail.com>
2024-11-16 13:02:35 -0800

    Update KindSidebarSimple.vue

--- components/content/layout/KindSidebarSimple.vue
+++ components/content/layout/KindSidebarSimple.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Left Sidebar -->
   <div
-    class="transition-all duration-300 bg-base-300 border-4 rounded-2xl left-0 h-full overflow-y-auto no-scrollbar flex flex-col justify-between flex-grow"
+    class="transition-all duration-300 bg-base-300 border-1 rounded-2xl left-0 h-full overflow-y-auto no-scrollbar flex flex-col justify-between flex-grow"
   >
     <!-- Sidebar Links with Icons and Titles -->
     <div class="flex flex-col justify-between flex-grow">

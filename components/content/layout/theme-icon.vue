3e9a76360892d76a37becc60f06c127a231ac3a7
Silas M Knight <silasfelinus@gmail.com>
2024-11-17 00:52:39 -0800

    Update theme-icon.vue

--- components/content/layout/theme-icon.vue
+++ components/content/layout/theme-icon.vue
@@ -9,7 +9,7 @@
     <!-- Icon with Scale Hover Effect -->
     <Icon
       name="kind-icon:paintbrush"
-      class="h-8 w-8 transition-transform transform hover:scale-110 duration-300 ease-in-out text-accent"
+      class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out text-accent"
     />
 
     <!-- Theme Name (Visible on md and higher) -->

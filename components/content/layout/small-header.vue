17d510180246ade79e4d48fb8397873cc3fb0198
Silas M Knight <silasfelinus@gmail.com>
2024-11-17 19:57:02 -0800

    Update small-header.vue

--- components/content/layout/small-header.vue
+++ components/content/layout/small-header.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col h-full px-4">
-    <!-- Title Section -->
-    <div class="flex flex-col items-start justify-start h-1/3">
+  <div class="flex h-full px-4">
+    <!-- Title Section (1/3 Width) -->
+    <div class="flex flex-col justify-start w-1/3">
       <h1 class="text-lg font-semibold text-left leading-none">
         The
       </h1>
@@ -13,19 +13,22 @@
       </h1>
     </div>
 
-    <!-- Subtitle Section -->
-    <div class="relative h-1/3">
-      <h2 class="absolute top-0 w-full text-sm italic text-right text-ellipsis">
-        {{ subtitle }}
-      </h2>
-    </div>
+    <!-- Right Side (2/3 Width) -->
+    <div class="flex flex-col w-2/3">
+      <!-- Subtitle Section (Top-Aligned) -->
+      <div class="relative h-1/3">
+        <h2 class="absolute top-0 w-full text-sm italic text-right text-ellipsis">
+          {{ subtitle }}
+        </h2>
+      </div>
 
-    <!-- Icons Section -->
-    <div class="relative h-2/3">
-      <div class="absolute bottom-0 right-0 flex gap-2">
-        <login-path />
-        <theme-icon />
-        <swarm-icon />
+      <!-- Icons Section (Bottom-Aligned) -->
+      <div class="relative flex-grow">
+        <div class="absolute bottom-0 right-0 flex gap-2">
+          <login-path />
+          <theme-icon />
+          <swarm-icon />
+        </div>
       </div>
     </div>
   </div>

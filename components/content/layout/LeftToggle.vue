a5dd86733ba332cb14fc9305d9255f4ca7186198
Silas M Knight <silasfelinus@gmail.com>
2024-11-18 05:42:24 -0800

    Update LeftToggle.vue

--- components/content/layout/LeftToggle.vue
+++ components/content/layout/LeftToggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="z-50 p-1">
+  <div class="relative z-50 p-1">
     <button
       class="w-8 h-8 rounded-2xl font-semibold text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleSidebarLeft"

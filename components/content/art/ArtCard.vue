42b4a372df2599774825b8d2836de24973b4b3d8
Silas Knight <silasfelinus@gmail.com>
2024-11-19 13:04:02 -0800

    remove path ref for troubleshooting

--- components/content/art/ArtCard.vue
+++ components/content/art/ArtCard.vue
@@ -165,9 +165,6 @@ const computedArtImage = computed(() => {
   } else if (props.artImage?.imageData) {
     console.log('returning props image data')
     return `data:image/${props.artImage.fileType};base64,${props.artImage.imageData}`
-  } else if (props.art?.path) {
-    console.log('returning path')
-    return props.art.path
   }
   return '/images/backtree.webp'
 })

bb60c1b1c487d0a3732e2c831dcad9902aa3fb17
Silas M Knight <silasfelinus@gmail.com>
2024-11-20 18:48:23 -0800

    Update AmiLoader.vue

--- components/content/butterfly/AmiLoader.vue
+++ components/content/butterfly/AmiLoader.vue
@@ -45,7 +45,7 @@ let intervalId: NodeJS.Timeout
 onMounted(() => {
   setTimeout(() => {
     currentMessage.value = randomLoadMessage()
-    intervalId = setInterval(updateMessage, 1700)
+    intervalId = setInterval(updateMessage, 2500)
   }, 100)
   
   setTimeout(startFadeOut, 1300) // Fade out the message after 2 seconds

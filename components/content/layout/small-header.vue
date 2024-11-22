e37b8afa573f07527e92aed322616b6360369474
Silas Knight <silasfelinus@gmail.com>
2024-11-22 04:40:00 -0800

    better header divisions

--- components/content/layout/small-header.vue
+++ components/content/layout/small-header.vue
@@ -2,22 +2,20 @@
   <div class="flex h-full px-2">
     <!-- Title Section (1/3 Width) -->
     <div class="flex flex-col justify-start w-1/3">
-      <h1 class="text-lg font-semibold text-left leading-none">
-        The
-      </h1>
+      <h1 class="text-lg font-semibold text-left leading-none">The</h1>
       <h1 class="text-lg font-semibold text-left leading-none">
         {{ page.title || 'Room' }}
       </h1>
-      <h1 class="text-lg font-semibold text-left leading-none">
-        Room
-      </h1>
+      <h1 class="text-lg font-semibold text-left leading-none">Room</h1>
     </div>
 
     <!-- Right Side (2/3 Width) -->
     <div class="flex flex-col w-2/3">
       <!-- Subtitle Section (Top-Aligned) -->
       <div class="relative h-1/3">
-        <h2 class="absolute top-0 w-full text-sm italic text-right text-ellipsis">
+        <h2
+          class="absolute top-0 w-full text-sm italic text-right text-ellipsis"
+        >
           {{ subtitle }}
         </h2>
       </div>
@@ -26,6 +24,7 @@
       <div class="relative flex-grow">
         <div class="absolute bottom-0 right-0 flex gap-2">
           <login-path />
+          <jellybean-count />
           <theme-icon />
           <swarm-icon />
         </div>
@@ -35,9 +34,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed } from 'vue'
 
 // Page and subtitle
-const { page } = useContent();
-const subtitle = computed(() => page.value?.subtitle ?? 'Welcome to Kind Robots');
+const { page } = useContent()
+const subtitle = computed(
+  () => page.value?.subtitle ?? 'Welcome to Kind Robots',
+)
 </script>

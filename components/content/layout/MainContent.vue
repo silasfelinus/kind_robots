2955b4716ae6c76c8aac95a9ac5d94c4725f853e
Silas Knight <silasfelinus@gmail.com>
2024-11-22 15:33:42 -0800

    readded flip effect

--- components/content/layout/MainContent.vue
+++ components/content/layout/MainContent.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Main container -->
-  <div class="h-full flex flex-col relative z-10 bg-base-300">
+  <div class="h-full flex flex-col relative z-10">
     <!-- Transition Wrapper -->
     <transition name="flip" mode="out-in">
       <!-- Mobile View (no flip card) -->
@@ -14,7 +14,10 @@
           v-if="showTutorial"
           class="h-full w-full z-10 rounded-2xl border-4"
         />
-        <NuxtPage v-else class="h-full w-full z-10 no-scrollbar rounded-2xl" />
+        <NuxtPage
+          v-else
+          class="h-full w-full z-10 no-scrollbar rounded-2xl box-border bg-base-300 border-1 border-accent"
+        />
       </div>
 
       <!-- Fullscreen Mode (Desktop) -->
@@ -24,7 +27,9 @@
         class="h-full w-full overflow-y-auto no-scrollbar z-10 flex-grow"
         :style="desktopViewStyle"
       >
-        <NuxtPage class="h-full w-full rounded-2xl" />
+        <NuxtPage
+          class="h-full w-full rounded-2xl box-border bg-base-300 border-1 border-accent"
+        />
       </div>
     </transition>
   </div>

5f8863e6e1c53c2504b4f7551367ad09af786bdb
Silas Knight <silasfelinus@gmail.com>
2024-11-22 03:47:54 -0800

    toggle backgrounds, minor text edit

--- components/content/layout/LeftToggle.vue
+++ components/content/layout/LeftToggle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative z-50 p-1">
     <button
-      class="w-8 h-8 rounded-2xl font-semibold text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
+      class="w-8 h-8 rounded-2xl font-semibold bg-info text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleSidebarLeft"
     >
       <span
@@ -10,7 +10,7 @@
           background: linear-gradient(to bottom right, #f472b6, #fbbf24);
           background-clip: text;
           -webkit-background-clip: text;
-          
+
           text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);
         "
       >
--- components/content/layout/RightToggle.vue
+++ components/content/layout/RightToggle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative z-50 p-1">
     <button
-      class="w-8 h-8 rounded-2xl font-semibold text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
+      class="w-8 h-8 rounded-2xl font-semibold bg-info text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleTutorialSidebar"
     >
       <span

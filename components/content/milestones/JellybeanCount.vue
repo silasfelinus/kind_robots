6e094f6d4186a2e82e36c6f070ab685e9258a1e2
Silas Knight <silasfelinus@gmail.com>
2024-11-24 13:18:10 -0800

    changed minimum height width

--- components/content/milestones/JellybeanCount.vue
+++ components/content/milestones/JellybeanCount.vue
@@ -1,13 +1,14 @@
 <template>
   <router-link
     to="/milestones"
-    class="flex flex-col items-center no-underline text-info w-full h-full"
+    class="flex flex-col items-center justify-center no-underline text-info w-full h-full"
   >
-    <!-- Always Show Jellybean Icon -->
+    <!-- Centered Jellybean Icon -->
     <Icon
       name="kind-icon:jellybean"
       class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out text-info"
     />
+
     <!-- Conditionally Show Jellybean Count -->
     <div v-if="!isSmallDisplay" class="mt-2 text-center">
       <span class="whitespace-nowrap"> {{ beanCount || 0 }} /11 </span>

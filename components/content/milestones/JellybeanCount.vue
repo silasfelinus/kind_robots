cbff48fb60c956616c4b7f3189315ae3f8dc79b9
Silas Knight <silasfelinus@gmail.com>
2024-11-24 03:47:22 -0800

    column not row

--- components/content/milestones/JellybeanCount.vue
+++ components/content/milestones/JellybeanCount.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     to="/milestones"
-    class="flex items-center space-x-2 no-underline text-info w-full h-full"
+    class="flex flex-col items-center no-underline text-info w-full h-full"
   >
     <!-- Always Show Jellybean Icon -->
     <Icon
@@ -9,8 +9,8 @@
       class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out text-info"
     />
     <!-- Conditionally Show Jellybean Count -->
-    <div v-if="!isSmallDisplay" class="flex items-center">
-      <span class="whitespace-nowrap mr-1"> {{ beanCount || 0 }} /11 </span>
+    <div v-if="!isSmallDisplay" class="mt-2 text-center">
+      <span class="whitespace-nowrap"> {{ beanCount || 0 }} /11 </span>
     </div>
   </router-link>
 </template>

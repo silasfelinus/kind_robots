ce56b14a4b759c84292d7d90967c4321529fe3aa
Silas Knight <silasfelinus@gmail.com>
2024-11-24 16:01:46 -0800

    capping max height

--- components/content/forum/User-Dashboard.vue
+++ components/content/forum/User-Dashboard.vue
@@ -17,7 +17,9 @@
       <h1 class="text-2xl font-semibold ml-6">User Dashboard</h1>
       <div class="relative flex flex-col justify-center items-center">
         <!-- Rounded Avatar -->
-        <user-avatar class="w-24 h-24 rounded-full border-2 border-accent" />
+        <user-avatar
+          class="w-auto max-h-50 rounded-full border-2 border-accent"
+        />
 
         <!-- Smaller Avatar Upload -->
         <avatar-upload class="w-16 h-16 mt-2" />

3f35f390b110a9121e6a6f970781469966446b10
Silas M Knight <silasfelinus@gmail.com>
2024-11-29 02:21:59 -0800

    Update [id].get.ts

---
+++ server/api/characters/[id].get.ts
@@ -0,0 +1,56 @@
+import { defineEventHandler } from 'h3'
+import type { Character } from '@prisma/client'
+import { errorHandler } from '../utils/error'
+import prisma from '../utils/prisma'
+
+export default defineEventHandler(async (event) => {
+  let response
+
+  try {
+    const id = Number(event.context.params?.id)
+
+    if (isNaN(id)) {
+      return {
+        success: false,
+        message: 'Invalid ID',
+        statusCode: 400,
+      }
+    }
+
+    // Fetch the character by ID
+    const data = await fetchCharacterById(id)
+    if (!data) {
+      return {
+        success: false,
+        message: 'Character not found',
+        statusCode: 404,
+      }
+    }
+
+    response = {
+      success: true,
+      message: 'Character details fetched successfully.',
+      data, // Return the character details under data
+      statusCode: 200,
+    }
+    event.node.res.statusCode = response.statusCode
+  } catch (error: unknown) {
+    return errorHandler(error)
+  }
+
+  return response
+})
+
+export async function fetchCharacterById(id: number): Promise<Character | null> {
+  try {
+    // Fetch the character by ID without including related data
+    const character = await prisma.character.findUnique({
+      where: { id },
+    })
+
+    return character || null
+  } catch (error: unknown) {
+    const handledError = errorHandler(error)
+    throw new Error(handledError.message)
+  }
+}

f0ae86d983420466992846b807f3112442239f7f
Silas M Knight <silasfelinus@gmail.com>
2024-11-29 00:47:44 -0800

    Update character-creator.vue

--- components/content/weird/character-creator.vue
+++ components/content/weird/character-creator.vue
@@ -1,7 +1,7 @@
 	<template>
   <div class="h-screen w-full bg-base-300 flex flex-col overflow-y-auto">
     <!-- Top Section: Name, Alignment, Level, and Save/Load/Delete -->
-    <div class="flex items-center justify-between h-[10%] bg-accent rounded-lg shadow-md px-4 py-2">
+    <div class="flex items-center justify-between h-[10%] bg-accent rounded-lg shadow-md">
       <!-- Character Name and Class -->
       <div class="flex flex-col flex-grow">
         <h1 class="text-4xl font-bold text-white truncate">

fc5e287cd245d7e02dc62672238076e05d254a17
Silas Knight <silasfelinus@gmail.com>
2024-12-07 14:44:32 -0800

    checkbox toggle

---
+++ components/content/weird/character-selector.vue
@@ -0,0 +1,17 @@
+<template>
+  <select v-model="characterStore.selectedCharacter">
+    <option
+      v-for="character in characterStore.characters"
+      :key="character.id"
+      :value="character"
+    >
+      {{ character.name }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+import { useCharacterStore } from '@/stores/characterStore'
+
+const characterStore = useCharacterStore()
+</script>

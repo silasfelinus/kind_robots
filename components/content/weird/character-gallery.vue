3edbd5b324977011dbd7708aa7339c4e58633fc2
Silas M Knight <silasfelinus@gmail.com>
2024-12-07 02:30:17 -0800

    Update character-gallery.vue

--- components/content/weird/character-gallery.vue
+++ components/content/weird/character-gallery.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen w-full bg-base-300 p-4 flex flex-col overflow-y-auto">
+  <div class="h-screen w-full bg-base-300 p-4 flex flex-col">
     <h1 class="text-3xl font-bold text-gray-700 mb-4">Character Gallery</h1>
 
     <!-- Filter and Search -->

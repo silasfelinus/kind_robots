1ae1c63cf509e235200458e5ab1ed09824d45998
Silas Knight <silasfelinus@gmail.com>
2024-12-07 17:26:16 -0800

    bug fixes

--- components/content/weird/character-designer.vue
+++ components/content/weird/character-designer.vue
@@ -10,89 +10,109 @@
     <div
       class="flex flex-wrap justify-between items-center col-span-full gap-4"
     >
-      <div class="w-full lg:w-auto">
-        <character-selector />
-      </div>
-
-      <div v-if="characterStore.selectedCharacter" class="flex items-center">
-        <button class="btn btn-icon" @click="deselectCurrentCharacter">
-          <icon name="kind-icon:addcharacter" class="text-3xl" />
-        </button>
-        <p class="ml-2">Create new character</p>
-      </div>
-
-      <div class="flex-grow w-1/4">
-        <label for="designer" class="block text-lg font-medium"
-          >Designer:</label
-        >
-        <input
-          v-if="canEditDesigner"
-          id="designer"
-          v-model="characterStore.characterForm.designer"
-          type="text"
-          class="w-full p-3 rounded-lg border"
-        />
-        <p v-else>{{ characterStore.characterForm.designer }}</p>
-      </div>
-
-      <div class="flex gap-4">
-        <div>
-          <label class="block text-lg font-medium">Public Visibility:</label>
-          <div class="flex space-x-2">
-            <button
-              type="button"
-              :class="{
-                'btn btn-primary': characterStore.characterForm.isPublic,
-                'btn btn-grey-200': !characterStore.characterForm.isPublic,
-              }"
-              @click="toggleVisibility(true)"
-            >
-              Public
-            </button>
-            <button
-              type="button"
-              :class="{
-                'btn btn-primary': !characterStore.characterForm.isPublic,
-                'btn btn-grey-200': characterStore.characterForm.isPublic,
-              }"
-              @click="toggleVisibility(false)"
-            >
-              Private
-            </button>
-          </div>
+      <!-- Title, Honorific, and Class -->
+      <div class="w-full lg:w-1/2 space-y-2">
+        <h2 class="text-lg font-medium">Character Info</h2>
+        <div class="flex items-center space-x-2">
+          <CheckboxToggle
+            v-model="characterStore.keepField.name"
+            label="Freeze Name"
+            title="Prevent changes to name"
+          />
+          <input
+            v-model="characterStore.characterForm.name"
+            type="text"
+            class="w-full p-3 rounded-lg border"
+            placeholder="Name"
+            :disabled="characterStore.keepField.name"
+          />
+          <span>The</span>
+          <CheckboxToggle
+            v-model="characterStore.keepField.honorific"
+            label="Freeze Honorific"
+            title="Prevent changes to honorific"
+          />
+          <input
+            v-model="characterStore.characterForm.honorific"
+            type="text"
+            class="w-full p-3 rounded-lg border"
+            placeholder="Honorific"
+            :disabled="characterStore.keepField.honorific"
+          />
         </div>
+        <div class="flex items-center space-x-2">
+          <CheckboxToggle
+            v-model="characterStore.keepField.class"
+            label="Freeze Class"
+            title="Prevent changes to class"
+          />
+          <input
+            v-model="characterStore.characterForm.class"
+            type="text"
+            class="w-full p-3 rounded-lg border"
+            placeholder="Class"
+            :disabled="characterStore.keepField.class"
+          />
+        </div>
+      </div>
+
+      <!-- Mode and Public Toggle -->
+      <div class="w-full lg:w-1/2 flex items-center space-x-4">
+        <CheckboxToggle
+          v-model="characterStore.generationMode"
+          label="Generation Mode"
+          title="Enable or disable generation mode"
+        />
+        <button
+          class="btn btn-primary"
+          :class="{
+            'btn-active': characterStore.characterForm.isPublic,
+          }"
+          @click="toggleVisibility(!characterStore.characterForm.isPublic)"
+        >
+          {{ characterStore.characterForm.isPublic ? 'Public' : 'Private' }}
+        </button>
+      </div>
+
+      <!-- Designer -->
+      <div class="w-full lg:w-auto">
+        <label class="block text-sm font-medium">Designer:</label>
+        <p>{{ designerName }}</p>
       </div>
     </div>
 
     <!-- Middle Section -->
     <div class="flex flex-wrap w-full mt-4">
-      <!-- Left: Art and Prompt -->
-      <div class="w-full md:w-1/2 lg:w-1/2 p-4">
+      <!-- Left: Art Section -->
+      <div class="w-full md:w-1/2 p-4">
+        <h2 class="text-lg font-medium">Character Art</h2>
         <div class="grid gap-4">
           <CheckboxToggle
             v-model="characterStore.keepField.artPrompt"
             label="Freeze Art Prompt"
             title="Prevent changes to art prompt"
           />
-          <img
-            v-if="characterStore.artImagePath"
-            :src="characterStore.artImagePath"
-            alt="Character Art"
-            class="object-cover w-full h-60 rounded-lg"
-          />
-          <img
-            v-else
-            src="/images/bot.webp"
-            alt="Default Art"
-            class="object-cover w-full h-60 rounded-lg"
-          />
+          <div class="flex justify-center">
+            <img
+              v-if="characterStore.artImagePath"
+              :src="characterStore.artImagePath"
+              alt="Character Portrait"
+              class="object-cover w-48 h-64 rounded-lg"
+            />
+            <img
+              v-else
+              src="/images/placeholder-portrait.webp"
+              alt="Default Portrait"
+              class="object-cover w-48 h-64 rounded-lg"
+            />
+          </div>
           <textarea
             v-model="characterStore.characterForm.artPrompt"
             placeholder="Describe your character's appearance or a scene..."
             class="w-full p-3 rounded-lg border"
             :disabled="characterStore.keepField.artPrompt"
           ></textarea>
-          <div class="flex w-full space-x-2">
+          <div class="flex space-x-2">
             <button
               class="btn btn-primary w-1/2"
               :disabled="characterStore.isGeneratingArt"
@@ -110,86 +130,54 @@
       </div>
 
       <!-- Right: Stats -->
-      <div class="w-full md:w-1/2 lg:w-1/2 p-4">
-        <div class="grid gap-4">
-          <h2 class="text-lg font-medium">Stats</h2>
-          <div class="grid grid-cols-3 gap-4">
-            <template v-for="i in 6" :key="'stat-' + i">
-              <div class="relative">
-                <CheckboxToggle
-                  v-model="characterStore.keepField[`statName${i}`]"
-                  :label="'Freeze Stat ' + i"
-                  :title="'Prevent changes to Stat ' + i"
-                />
-                <label
-                  :for="'statName' + i"
-                  class="block text-sm font-bold uppercase"
-                >
-                  {{
-                    characterStore.characterForm[
-                      `statName${i}` as keyof Character
-                    ] || 'Stat'
-                  }}
-                </label>
-                <input
-                  :id="'statValue' + i"
-                  v-model="
-                    characterStore.characterForm[
-                      `statValue${i}` as keyof Character
-                    ]
-                  "
-                  type="number"
-                  class="w-full p-2 rounded-lg border text-center"
-                />
-              </div>
-            </template>
-          </div>
-          <button
-            class="btn btn-secondary mt-2"
-            @click="characterStore.rerollStats"
-          >
-            Reroll Stats
-          </button>
+      <div class="w-full md:w-1/2 p-4">
+        <h2 class="text-lg font-medium">Stats</h2>
+        <div class="grid grid-cols-2 gap-4">
+          <template v-for="i in 6" :key="'stat-' + i">
+            <div class="relative">
+              <CheckboxToggle
+                v-model="characterStore.keepField[`statName${i}`]"
+                :label="'Freeze Stat ' + i"
+                :title="'Prevent changes to Stat ' + i"
+              />
+              <label
+                :for="'statName' + i"
+                class="block text-sm font-bold uppercase"
+              >
+                {{
+                  characterStore.characterForm[
+                    `statName${i}` as keyof Character
+                  ] || 'Stat'
+                }}
+              </label>
+              <input
+                :id="'statValue' + i"
+                v-model="
+                  characterStore.characterForm[
+                    `statValue${i}` as keyof Character
+                  ]
+                "
+                type="number"
+                class="w-full p-2 rounded-lg border text-center"
+              />
+            </div>
+          </template>
         </div>
+        <button
+          class="btn btn-secondary mt-2"
+          @click="characterStore.rerollStats"
+        >
+          Reroll Stats
+        </button>
       </div>
     </div>
 
     <!-- Bottom Section -->
     <div class="grid gap-4 mt-4">
-      <!-- Title -->
+      <!-- Rewards -->
       <div>
-        <CheckboxToggle
-          v-model="characterStore.keepField.name"
-          label="Freeze Name"
-          title="Prevent changes to character name"
-        />
-        <CheckboxToggle
-          v-model="characterStore.keepField.honorific"
-          label="Freeze Honorific"
-          title="Prevent changes to character honorific"
-        />
-        <h2 class="text-lg font-medium">Title</h2>
-        <div class="flex items-center space-x-2">
-          <input
-            v-model="characterStore.characterForm.name"
-            type="text"
-            class="w-full p-3 rounded-lg border"
-            placeholder="Enter character's name"
-            :disabled="characterStore.keepField.name"
-          />
-          <span>The</span>
-          <input
-            v-model="characterStore.characterForm.honorific"
-            type="text"
-            class="w-full p-3 rounded-lg border"
-            placeholder="Honorific (e.g., Brave)"
-            :disabled="characterStore.keepField.honorific"
-          />
-        </div>
-        <p class="text-sm text-gray-500 mt-2">
-          Preview: {{ characterStore.characterForm.name || 'Unnamed' }} the
-          {{ characterStore.characterForm.honorific || 'Unknown' }}
-        </p>
+        <h2 class="text-lg font-medium">Choose Reward:</h2>
+        <character-rewards />
       </div>
 
       <!-- Backstory -->
@@ -209,10 +197,10 @@
         ></textarea>
       </div>
 
-      <!-- Rewards -->
+      <!-- Additional Fields -->
       <div>
-        <h2 class="text-lg font-medium">Rewards</h2>
-        <character-rewards />
+        <h2 class="text-lg font-medium">Other Details</h2>
+        <p>Quirks, Inventory, Skills</p>
       </div>
     </div>
 
@@ -250,20 +238,22 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { useCharacterStore } from '@/stores/characterStore'
+import { useUserStore } from '@/stores/userStore'
 
 const characterStore = useCharacterStore()
+const userStore = useUserStore()
 
 const isLoading = ref(false)
 const errorMessage = ref<string | null>(null)
 const successMessage = ref<string | null>(null)
 
+const designerName = computed(() =>
+  userStore.getUsernameByUserId(characterStore.selectedCharacter?.userId || 10),
+)
+
 function toggleVisibility(value: boolean) {
   characterStore.characterForm.isPublic = value
 }
-
-const canEditDesigner = computed(() =>
-  characterStore.characterForm.designer ? true : false,
-)
 
 function setArtImageId(id: number) {
   characterStore.setArtImageId(id)
@@ -271,10 +261,6 @@ function setArtImageId(id: number) {
 
 async function generateArtImage() {
   await characterStore.generateArtImage()
-}
-
-function deselectCurrentCharacter() {
-  characterStore.deselectCharacter()
 }
 
 async function handleSubmit() {

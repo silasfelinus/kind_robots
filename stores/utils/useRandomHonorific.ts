f9c932a2e4208210fe67a9e5ad2a6343521eba3f
Silas M Knight <silasfelinus@gmail.com>
2024-12-08 00:27:03 -0800

    Update useRandomHonorific.ts

--- stores/utils/useRandomHonorific.ts
+++ stores/utils/useRandomHonorific.ts
@@ -1,7 +1,112 @@
-export function useRandomHonorific() {
+export function useRandomHonorifics() {
   const honorifics = [
-    'Brave', 'Wise', 'Swift', 'Cunning',
-    'Reckless', 'Undying', 'Forgotten',
+    // Noble and Regal
+    'Brave',
+    'Wise',
+    'Swift',
+    'Cunning',
+    'Noble',
+    'Magnanimous',
+    'Honorable',
+    'Just',
+    'Radiant',
+    'Virtuous',
+
+    // Mystical and Magical
+    'Enchanted',
+    'Arcane',
+    'Ethereal',
+    'Shadowed',
+    'Blazing',
+    'Frozen',
+    'Seer',
+    'Invoker',
+    'Moonlit',
+    'Starborn',
+
+    // Adventurous
+    'Explorer',
+    'Wanderer',
+    'Trailblazer',
+    'Pioneer',
+    'Pathfinder',
+    'Voyager',
+    'Conqueror',
+    'Champion',
+    'Guardian',
+    'Vagabond',
+
+    // Quirky and Fun
+    'Perpetually Confused',
+    'Undaunted',
+    'Glorious',
+    'Defenestrator',
+    'Keeper of Cats',
+    'Master of Cheese',
+    'Unstoppable',
+    'Sassy',
+    'Indomitable',
+    'Unpredictable',
+
+    // Animal Inspired
+    'Eagle-Eyed',
+    'Lionhearted',
+    'Foxlike',
+    'Stagborne',
+    'Wolfkin',
+    'Bearclaw',
+    'Tigerfang',
+    'Owlsighted',
+    'Pantherclaw',
+    'Ravenwinged',
+
+    // Historical and Cultural
+    'Templar',
+    'Ronin',
+    'Viking',
+    'Centurion',
+    'Samurai',
+    'Khaleesi',
+    'Shogun',
+    'Knight Errant',
+    'Baron',
+    'Emissary',
+
+    // Dark and Mysterious
+    'Fallen',
+    'Twilight-Touched',
+    'Veiled',
+    'Silent',
+    'Eclipse',
+    'Wraith',
+    'Phantom',
+    'Hollow',
+    'Blooded',
+    'Unseen',
+
+    // Elemental Inspired
+    'Stormbringer',
+    'Flameborn',
+    'Earthshaker',
+    'Tidecaller',
+    'Skydancer',
+    'Frostmarked',
+    'Thunderborn',
+    'Dawnbreaker',
+    'Ashen',
+    'Tempest',
+
+    // Titles of Power
+    'Warden',
+    'Overseer',
+    'Chancellor',
+    'High Priest',
+    'Archmage',
+    'Commander',
+    'Marshal',
+    'Herald',
+    'Emperor',
+    'Oracle',
   ]
 
   function randomHonorific() {

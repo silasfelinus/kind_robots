5e68f1031275ec88f46ab87b276075add17c96ee
Silas M Knight <silasfelinus@gmail.com>
2024-12-08 14:20:51 -0800

    Update character-designer.vue

--- components/content/weird/character-designer.vue
+++ components/content/weird/character-designer.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="rounded-2xl border p-4 m-4 mx-auto bg-base-200 grid gap-4 grid-cols-1"
+    class="rounded-2xl border p-2 md:p-4 mx-auto bg-base-200 grid gap-4 grid-cols-1"
   >
-    <h1 class="text-4xl text-center col-span-full">
-      Create or Edit a Character
+    <h1 class="text-lg md:text-2xl lg:text-3xl xl:4xl text-center col-span-full">
+      Character Designer
     </h1>
 
     <!-- Top Section -->
@@ -16,19 +16,19 @@
     <!-- Middle Section -->
     <div class="flex flex-wrap w-full mt-4">
       <!-- Left: Art Section -->
-      <div class="w-full md:w-1/2 p-4">
+      <div class="w-full md:w-1/2 p-1 md:p-4">
         <character-art />
       </div>
 
       <!-- Right: Stats Section -->
-      <div class="w-full md:w-1/2 p-4">
+      <div class="w-full md:w-1/2 p-1 md:p-4">
         <character-stats />
         <character-rewards />
       </div>
     </div>
 
     <!-- Bottom Section -->
-    <div class="grid gap-4 mt-4">
+    <div class="grid gap-4 mt-1 md:mt-2 lg:mt-3">
       <character-bottom />
     </div>
 

0668705479b6f670755c3cb69e4be0511b6c7b77
Silas M Knight <silasfelinus@gmail.com>
2024-12-11 19:13:14 -0800

    Create scenario-selector.vue

---
+++ components/content/weird/scenario-selector.vue
@@ -0,0 +1,21 @@
+<template>
+  <select
+    v-model="scenarioStore.selectedScenario"
+    class="bg-base-200 border border-gray-400 rounded-lg p-2 w-full"
+  >
+    <option disabled value="">Select a Scenario</option>
+    <option
+      v-for="scenario in scenarioStore.scenarios"
+      :key="scenario.id"
+      :value="scenario"
+    >
+      {{ scenario.title || 'Untitled Scenario' }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+import { useScenarioStore } from '@/stores/scenarioStore';
+
+const scenarioStore = useScenarioStore();
+</script>

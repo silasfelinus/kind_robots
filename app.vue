8daaf462c1e22606b80c0f2073bea5b0e0d19131
Silas Knight <silasfelinus@gmail.com>
2024-12-17 06:22:05 -0800

    layout

--- app.vue
+++ app.vue
@@ -1,15 +1,8 @@
 <template>
-  <NuxtLayout :name="layoutStore.currentLayout">
-    <NuxtPage />
-    <div class="fixed top-2 right-2 z-50">
-      <layout-selector />
-    </div>
-  </NuxtLayout>
+  <NuxtPage />
+  <div class="fixed top-2 right-2 z-50">
+    <layout-selector />
+  </div>
 </template>
 
-<script setup lang="ts">
-import { useLayoutStore } from '@/stores/layoutStore'
-import LayoutToggle from '@/components/LayoutToggle.vue'
-
-const layoutStore = useLayoutStore()
-</script>
+<script setup lang="ts"></script>

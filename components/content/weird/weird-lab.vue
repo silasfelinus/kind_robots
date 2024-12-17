a1ee00f76de211242d200f1f649a8562369e2309
Silas Knight <silasfelinus@gmail.com>
2024-12-17 14:32:11 -0800

    essentialized wonderlab and renamed

--- components/content/weird/weird-lab.vue
+++ components/content/weird/weird-lab.vue
@@ -35,14 +35,13 @@
 
     <!-- Section Content -->
     <div class="flex-grow w-full overflow-y-auto h-full">
-
       <lazy-model-manager v-if="activeTab === 'model-manager'" />
-      
+
       <lazy-scenario-manager v-if="activeTab === 'scenario-manager'" />
-      <lazy-character-designer v-if="activeTab === 'character-designer'" />
-      
+      <lazy-add-character v-if="activeTab === 'add-character'" />
+
       <lazy-character-gallery v-if="activeTab === 'character-gallery'" />
-  
+
       <lazy-rewards-gallery v-if="activeTab === 'reward-gallery'" />
     </div>
   </div>
@@ -58,12 +57,12 @@ const displayStore = useDisplayStore()
 // Tabs setup for Weirdlandia
 const tabs = [
   { name: 'model-manager', label: ' Model Manager' },
-{ name: 'scenario-manager', label: 'Scenario Manager' },
+  { name: 'scenario-manager', label: 'Scenario Manager' },
 
-  { name: 'character-designer', label: 'Character Designer' },
-  
+  { name: 'add-character', label: 'Character Designer' },
+
   { name: 'character-gallery', label: 'Character Gallery' },
-  
+
   { name: 'reward-gallery', label: 'Reward Gallery' },
 ]
 

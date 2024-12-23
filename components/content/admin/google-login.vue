74022ceedffe19decd616979e15266b46fe1a2cc
Silas M Knight <silasfelinus@gmail.com>
2024-12-22 18:54:36 -0800

    Create google-login.vue

---
+++ components/content/admin/google-login.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    class="btn btn-primary"
+    @click="loginWithGoogle"
+  >
+    Sign in with Google
+  </button>
+</template>
+
+<script setup>
+import { navigateTo } from 'nuxt/app';
+
+function loginWithGoogle() {
+  navigateTo('/api/auth/google');
+}
+</script>

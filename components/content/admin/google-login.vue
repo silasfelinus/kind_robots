728e35ad0a5fa388ab57c7ba1ef84b30582ba70f
Silas M Knight <silasfelinus@gmail.com>
2024-12-22 23:38:35 -0800

    Update google-login.vue

--- components/content/admin/google-login.vue
+++ components/content/admin/google-login.vue
@@ -1,9 +1,14 @@
 <template>
   <button
-    class="btn btn-primary"
+    class="btn btn-outline btn-primary flex items-center space-x-2 px-6 py-3 rounded-lg shadow-md hover:shadow-lg transition-all"
     @click="loginWithGoogle"
   >
-    Sign in with Google
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+      alt="Google"
+      class="w-5 h-5"
+    />
+    <span>Sign in with Google</span>
   </button>
 </template>
 

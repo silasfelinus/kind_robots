66a7c16900a39174ffd7835dba376f72826b44ac
Silas M Knight <silasfelinus@gmail.com>
2024-12-24 18:28:07 -0800

    Update index.ts

--- server/api/auth/google/index.ts
+++ server/api/auth/google/index.ts
@@ -4,7 +4,8 @@ import { defineEventHandler, setResponseStatus, setHeaders } from 'h3'
 
 export default defineEventHandler((event) => {
   const clientId = process.env.GOOGLE_ID
-  const redirectUri = 'https://kind-robots.vercel.app/dashboard'
+  const redirectUri = 'https://kind-robots.vercel.app/api/auth/google/callback';
+
 
   const googleOAuthURL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=email%20profile`
 

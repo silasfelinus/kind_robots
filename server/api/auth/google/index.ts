e44afcd24fd610974a120ad11fee86b67b3bd04e
Silas Knight <silasfelinus@gmail.com>
2024-12-24 16:38:04 -0800

    troubleshooting login

--- server/api/auth/google/index.ts
+++ server/api/auth/google/index.ts
@@ -1,11 +1,13 @@
-import { defineEventHandler, setResponseStatus, setHeaders } from 'h3';
+//server/api/auth/google/index.ts
+
+import { defineEventHandler, setResponseStatus, setHeaders } from 'h3'
 
 export default defineEventHandler((event) => {
-  const clientId = process.env.GOOGLE_ID;
-  const redirectUri = 'https://kind-robots.vercel.app/dashboard';
+  const clientId = process.env.GOOGLE_ID
+  const redirectUri = 'https://kind-robots.vercel.app/dashboard'
 
-  const googleOAuthURL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=email%20profile`;
+  const googleOAuthURL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=email%20profile`
 
-  setResponseStatus(event, 302);
-  setHeaders(event, { Location: googleOAuthURL });
-});
+  setResponseStatus(event, 302)
+  setHeaders(event, { Location: googleOAuthURL })
+})

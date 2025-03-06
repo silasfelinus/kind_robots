7ed89d39544815f2d8ae65fa953a9144e276a98a
Silas Knight <silasfelinus@gmail.com>
2025-03-05 16:39:15 -0800

    explicit pages schema

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,25 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: 'page',
-      source: '**/*.md'
-    })
-  }
+      source: '**/*.md',
+      schema: z.object({
+        title: z.string(),
+        subtitle: z.string(),
+        description: z.string(),
+        layout: z.string(),
+        icon: z.string(),
+        image: z.string(),
+        gallery: z.string(),
+        tags: z.array(z.string()),
+        category: z.string(),
+        tooltip: z.string(),
+        dottitip: z.string(),
+        amitip: z.string(),
+        sort: z.string(),
+      }),
+    }),
+  },
 })

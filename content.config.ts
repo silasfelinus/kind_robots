c24688f217691bc01771acc3cc9515d6de061db4
Silas Knight <silasfelinus@gmail.com>
2025-03-10 19:23:26 -0700

    better page data

--- content.config.ts
+++ content.config.ts
@@ -7,8 +7,36 @@ export default defineContentConfig({
       source: '**/*.md',
       schema: z.object({
         title: z.string(),
-        subtitle: z.string(),
+        path: z.string(),
         description: z.string(),
+        seo: z
+          .intersection(
+            z.object({
+              title: z.string().optional(),
+              description: z.string().optional(),
+              meta: z.array(z.record(z.string(), z.any())).optional(),
+              link: z.array(z.record(z.string(), z.any())).optional(),
+            }),
+            z.record(z.string(), z.any()),
+          )
+          .optional()
+          .default({}),
+        body: z.object({
+          type: z.string(),
+          children: z.any(),
+          toc: z.any(),
+        }),
+        navigation: z
+          .union([
+            z.boolean(),
+            z.object({
+              title: z.string(),
+              description: z.string(),
+              icon: z.string(),
+            }),
+          ])
+          .default(true),
+        subtitle: z.string(),
         layout: z.string(),
         icon: z.string(),
         image: z.string(),

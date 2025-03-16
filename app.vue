e012165d7f1d6d49764d8f122939f4ec6a9bdcb0
Silas Knight <silasfelinus@gmail.com>
2025-03-16 16:10:14 -0700

    proper style names

--- app.vue
+++ app.vue
@@ -5,8 +5,8 @@
       <kind-loader />
       <Analytics />
       <animation-loader class="fixed z-50" />
-<screen-debug />
-    <milestone-popup />
+      <screen-debug />
+      <milestone-popup />
     </div>
 
     <!-- Header -->
@@ -22,10 +22,7 @@
       class="grid grid-cols-[auto_1fr_auto] grid-rows-[auto_1fr_auto] flex-grow w-full"
     >
       <!-- Left Sidebar -->
-      <aside
-        class="z-10 transition-all"
-        :style="displayStore.sidebarLeftStyle"
-      >
+      <aside class="z-10 transition-all" :style="displayStore.leftSidebarStyle">
         <kind-sidebar-simple class="h-full w-full" />
       </aside>
 
@@ -38,16 +35,14 @@
           <mode-row />
         </div>
 
-        <main class="flex-grow p-4 w-full transition-all"
-:style="mainContentStyle"
->
+        <main
+          class="flex-grow p-4 w-full transition-all"
+          :style="displayStore.mainContentStyle"
+        >
           <NuxtPage :key="$route.fullPath" />
         </main>
 
-        <footer
-          class="z-10 transition-all"
-          :style="displayStore.footerStyle"
-        >
+        <footer class="z-10 transition-all" :style="displayStore.footerStyle">
           <horizontal-nav class="h-full w-full" />
         </footer>
       </div>
@@ -55,7 +50,7 @@
       <!-- Right Sidebar -->
       <aside
         class="z-10 transition-all"
-        :style="displayStore.sidebarRightStyle"
+        :style="displayStore.rightSidebarStyle"
       >
         <splash-tutorial class="h-full w-full" />
       </aside>
@@ -65,16 +60,11 @@
     <div class="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50">
       <footer-toggle />
     </div>
-
-    
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import { useDisplayStore } from '@/stores/displayStore'
 
 const displayStore = useDisplayStore()
-
-
 </script>

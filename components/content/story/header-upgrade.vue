e6f512a94352d94eccc80081bcc5c2702bb03421
Silas M Knight <silasfelinus@gmail.com>
2025-03-17 05:36:02 -0700

    Update header-upgrade.vue

--- components/content/story/header-upgrade.vue
+++ components/content/story/header-upgrade.vue
@@ -6,33 +6,28 @@
     <!-- Top Section: Avatar and Viewport -->
     <div class="flex items-center justify-between w-full h-full">
       <!-- Avatar Section -->
-      <div
-        class="flex items-center justify-left w-1/5 sm:w-1/6 h-full relative rounded-2xl"
-      >
-        <avatar-image
-          alt="User Avatar"
-          class="w-full h-full rounded-2xl object-cover"
-        />
-        <div
-          v-if="isAdmin"
-          class="absolute bottom-2 right-2 text-white bg-primary rounded-md text-xs md:text-sm p-1"
-        >
+      <div class="flex items-center justify-left w-1/5 sm:w-1/6 h-full relative rounded-2xl">
+        <avatar-image alt="User Avatar" class="w-full h-full rounded-2xl object-cover" />
+        <div v-if="isAdmin" class="absolute bottom-2 right-2 text-white bg-primary rounded-md text-xs md:text-sm p-1">
           {{ displayStore.viewportSize }}
         </div>
       </div>
 
       <!-- Conditional Header Content -->
-      <div class="flex-1 h-full">
+      <div class="flex flex-col flex-1 h-full">
         <SmallHeader v-if="isSmallDisplay" />
         <LargeHeader v-else />
+        <ModeRow class="w-full mt-2" /> 
       </div>
     </div>
   </header>
 </template>
+
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useDisplayStore } from '@/stores/displayStore'
 import { useUserStore } from '@/stores/userStore'
+import ModeRow from '@/components/ModeRow.vue'
 
 // Access stores
 const displayStore = useDisplayStore()

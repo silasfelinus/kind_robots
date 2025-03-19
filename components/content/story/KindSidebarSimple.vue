c7a22604313988a42fecb9415a1f2ec711e9ae1f
Silas Knight <silasfelinus@gmail.com>
2025-03-18 23:44:00 -0700

    adding titles on small display

--- components/content/story/KindSidebarSimple.vue
+++ components/content/story/KindSidebarSimple.vue
@@ -28,7 +28,6 @@
 
           <!-- Show the link title below the icon in compact state -->
           <span
-            v-if="displayStore.sidebarLeftState === 'compact' && !isMobile"
             class="text-xs md:text-md lg:text-lg font-semibold text-center px-2 rounded-lg"
           >
             {{ link.title }}
@@ -89,8 +88,6 @@ const displayStore = useDisplayStore()
 const navigate = (path: string) => {
   router.push(path)
 }
-
-const isMobile = computed(() => displayStore.isMobileViewport)
 </script>
 
 <style scoped>

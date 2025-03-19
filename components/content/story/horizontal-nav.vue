d0852857cb9e751ecacccc0c41f79a386a0807a4
Silas Knight <silasfelinus@gmail.com>
2025-03-18 21:35:47 -0700

    changed horizontal gap

--- components/content/story/horizontal-nav.vue
+++ components/content/story/horizontal-nav.vue
@@ -3,7 +3,7 @@
     class="relative flex justify-center bg-base-300 border border-accent rounded-2xl max-w-full overflow-x-auto"
   >
     <div
-      class="flex flex-wrap justify-center w-full items-center gap-2 md:gap-4"
+      class="flex flex-wrap justify-center w-full items-center gap-1 md:gap-2 lg:gap-3 xl:gap-4"
     >
       <div
         v-for="(item, index) in hardcodedLinks"
@@ -12,7 +12,7 @@
       >
         <button
           :aria-label="item.title"
-          class="group flex flex-col items-center justify-center w-full space-y-1 text-center transition-all transform p-2 hover:scale-105 hover:bg-accent hover:text-info focus:outline-none focus:ring-2 focus:ring-accent focus:ring-offset-2"
+          class="group flex flex-col items-center justify-center w-full space-y-1 text-center transition-all transform hover:scale-105 hover:bg-accent hover:text-info focus:outline-none focus:ring-2 focus:ring-accent focus:ring-offset-2"
           @click="navigateTo(item.path)"
         >
           <icon
@@ -20,7 +20,7 @@
             class="w-6 h-6 transition-transform group-hover:scale-110"
           />
           <span
-            class="text-xs md:text-sm font-semibold mt-1 text-center bg-secondary px-2 rounded-lg"
+            class="text-xs md:text-sm font-semibold text-center bg-secondary rounded-lg"
           >
             {{ item.title }}
           </span>

ee4ecf58a84cf2ce3713438f1034a69081291aec
Silas M Knight <silasfelinus@gmail.com>
2025-03-21 04:00:05 -0700

    Update RoomMeta.vue

--- components/content/tooltips/RoomMeta.vue
+++ components/content/tooltips/RoomMeta.vue
@@ -3,36 +3,25 @@
     <!-- Image -->
     <div class="flex justify-center items-center m-1">
       <img
-        v-if="page?.image"
-        :src="'/images/' + page.image"
+        v-if="image"
+        :src="'/images/' + image"
         alt="Main Image"
         class="rounded-2xl border shadow-md medium"
       />
     </div>
+
     <!-- Title and Subtitle -->
     <div class="flex flex-col justify-center items-center">
       <room-title class="text-lg font-semibold m-2" />
-      <h2 v-if="page?.subtitle" class="text-md font-medium">
-        {{ page.subtitle }}
+      <h2 v-if="subtitle" class="text-md font-medium">
+        {{ subtitle }}
       </h2>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
-import { useAsyncData } from '#app'
-import type { ContentType } from '~/content.config'
+import { usePageStore } from '@/stores/pageStore'
 
-// Get the route params
-const route = useRoute()
-
-const page = computed(() => {
-  const { data } = useAsyncData(route.path, async () => {
-    return (await queryCollection('content')
-      .path(route.path)
-      .first()) as ContentType | null
-  })
-  return data.value
-})
+const { image, subtitle } = storeToRefs(usePageStore())
 </script>

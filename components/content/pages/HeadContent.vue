297f1f4dfa987f6ba3ca7fe86e696d8c56d72b40
Silas M Knight <silasfelinus@gmail.com>
2025-03-21 03:41:05 -0700

    Update HeadContent.vue

--- components/content/pages/HeadContent.vue
+++ components/content/pages/HeadContent.vue
@@ -4,29 +4,7 @@
 </template>
 
 <script setup lang="ts">
-import { useAsyncData } from '#app'
+import { usePageStore } from '@/stores/pageStore'
 
-// Get the route params
-const route = useRoute()
-const name = route.params.name as string
-
-// Define expected content structure
-interface PageData {
-  title?: string
-  description?: string
-  subtitle?: string
-  image?: string
-  icon?: string
-  underConstruction?: boolean
-  dottitip?: string
-  amitip?: string
-  tooltip?: string
-  message?: string
-}
-
-// Fetch the page data using Nuxt Content v3
-const { data: page } = await useAsyncData<PageData>(`${name}`, async () => {
-  const result = await queryCollection('content').path(`${name}`).first()
-  return result || {}
-})
+const { page } = storeToRefs(usePageStore())
 </script>

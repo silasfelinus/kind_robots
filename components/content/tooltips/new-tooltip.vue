b4140ba758d237e86558d7789c16312ed4a4f90b
Silas M Knight <silasfelinus@gmail.com>
2025-03-21 03:36:34 -0700

    Update new-tooltip.vue

--- components/content/tooltips/new-tooltip.vue
+++ components/content/tooltips/new-tooltip.vue
@@ -1,36 +1,14 @@
 <template>
   <streaming-tooltip
-    v-if="page?.tooltip && page?.amitip"
-    :tooltip="page.tooltip"
-    :amitip="page.amitip"
+    v-if="tooltip && amitip"
+    :tooltip="tooltip"
+    :amitip="amitip"
     class="w-full h-full overflow-visible"
   />
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
-import { useAsyncData } from '#app'
+import { usePageStore } from '@/stores/pageStore'
 
-// Get the route params
-const route = useRoute()
-const name = route.params.name as string
-
-// Define expected content structure
-interface PageData {
-  title?: string
-  description?: string
-  subtitle?: string
-  image?: string
-  icon?: string
-  underConstruction?: boolean
-  dottitip?: string
-  amitip?: string
-  tooltip?: string
-}
-
-// Fetch the page data using Nuxt Content v3
-const { data: page } = await useAsyncData<PageData>(`${name}`, async () => {
-  const result = await queryCollection('content').path(`${name}`).first()
-  return result || {}
-})
+const { tooltip, amitip } = storeToRefs(usePageStore())
 </script>

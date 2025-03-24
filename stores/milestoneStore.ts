c9e88aa0d4d14861cca60cbe78153b232404224a
Silas Knight <silasfelinus@gmail.com>
2025-03-24 07:31:33 -0700

    essentializing award milestone

--- stores/milestoneStore.ts
+++ stores/milestoneStore.ts
@@ -398,6 +398,7 @@ export const useMilestoneStore = defineStore('milestoneStore', () => {
     updateMilestone,
     updateMilestonesFromData,
     deactivateMilestone,
+    rewardMilestone,
 
     // Milestone record logic
     addMilestoneRecord,

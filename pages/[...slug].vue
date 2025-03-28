53b7b98b58889962963b8a73fed974d671504a4f
Silas M Knight <silasfelinus@gmail.com>
2025-03-27 20:22:25 -0700

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -45,7 +45,11 @@ const pitchStore = usePitchStore()
 const promptStore = usePromptStore()
 const pageStore = usePageStore()
 
-const { page, layout } = storeToRefs(pageStore)
+const { layout } = storeToRefs(pageStore)
+
+const { page } = await useAsyncData(page-${resolvedPath}, () =>
+        queryCollection('content').path(resolvedPath).first(),
+      )
 
 const handleRouteChange = async () => {
   await pageStore.loadPage(route.path)

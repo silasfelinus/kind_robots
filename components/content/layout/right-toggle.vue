cb658ea1fac37a2da00db3d8c342c13cad4fae03
Silas Knight <silasfelinus@gmail.com>
2025-03-28 04:49:54 -0700

    question icon

---
+++ components/content/layout/right-toggle.vue
@@ -0,0 +1,31 @@
+// /components/rightToggle.vue
+<template>
+  <div class="relative z-50 p-1">
+    <button
+      class="w-8 h-8 rounded-2xl flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 duration-300 ease-in-out"
+      :class="
+        isHighlighted ? 'bg-warning text-primary' : 'bg-primary text-secondary'
+      "
+      @click="toggleHighlight"
+    >
+      <Icon
+        :name="
+          isHighlighted
+            ? 'kind-icon:question-glow'
+            : 'kind-icon:question-hollow'
+        "
+        class="w-6 h-6"
+      />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isHighlighted = ref(false)
+
+const toggleHighlight = () => {
+  isHighlighted.value = !isHighlighted.value
+}
+</script>

910b854c1036a181ed1bde4cdcdd72939d965192
Silas M Knight <silasfelinus@gmail.com>
2025-03-28 20:15:52 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -28,11 +28,11 @@
     >
       <kind-sidebar v-if="sidebarLeftOpen" class="h-full w-full z-10" />
     </aside>
+ <div>
+    <right-toggle :style="displayStore.rightToggleStyle" class="fixed z-20"/>
+<big-toggle :style="displayStore.leftToggleStyle" class="fixed z-20"/>
 
-    <right-toggle :style="displayStore.rightToggleStyle" />
-<big-toggle :style="displayStore.leftToggleStyle" />
-
-
+</div>
     <!-- Center Column -->
     <div class="flex flex-col w-full h-full">
       <main

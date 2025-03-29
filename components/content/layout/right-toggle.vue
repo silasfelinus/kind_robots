535dcf4d9aa6f1ba3851dde99f0be5a2685c518f
Silas M Knight <silasfelinus@gmail.com>
2025-03-28 20:24:23 -0700

    Update right-toggle.vue

--- components/content/layout/right-toggle.vue
+++ components/content/layout/right-toggle.vue
@@ -1,6 +1,6 @@
 <!-- /components/TutorialToggle.vue -->
 <template>
-  <div class="z-50 p-1">
+  <div class="z-30 p-1">
     <button
       class="w-8 h-8 rounded-2xl flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleTutorialSidebar"

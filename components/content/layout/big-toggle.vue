c59bc63a5d3fc3f8a6721103f084197a6fb85da0
Silas Knight <silasfelinus@gmail.com>
2025-03-29 02:37:48 -0700

    changed to absolute

--- components/content/layout/big-toggle.vue
+++ components/content/layout/big-toggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative z-50 p-1">
+  <div class="absolute z-50 p-1">
     <button
       class="w-8 h-8 rounded-2xl font-semibold bg-primary text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleBigMode"

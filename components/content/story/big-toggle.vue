420a5f0151c4c14289c38c44a355937342451699
Silas Knight <silasfelinus@gmail.com>
2025-03-30 02:12:21 -0700

    removed z index for toggles

--- components/content/story/big-toggle.vue
+++ components/content/story/big-toggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-30 p-1">
+  <div class="fixed p-1">
     <button
       class="w-8 h-8 rounded-2xl font-semibold bg-primary text-secondary flex items-center justify-center shadow-lg transition-transform transform hover:scale-110 hover:rotate-12 duration-300 ease-in-out"
       @click="toggleBigMode"

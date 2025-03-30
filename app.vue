d4de8244ec44e051431493b2c65b5d14b8f9a477
Silas Knight <silasfelinus@gmail.com>
2025-03-30 01:56:05 -0700

    removd z tinkering

--- app.vue
+++ app.vue
@@ -6,17 +6,22 @@
     <!-- Loaders -->
     <div class="fixed z-50">
       <kind-loader />
-      <animation-loader class="fixed z-100" />
+      <animation-loader class="fixed z-50" />
       <milestone-popup />
       <screen-debug />
     </div>
 
     <!-- Header -->
     <header
-      class="fixed -z-40 border-3 flex items-center justify-center box-border border-1 border-black overflow-hidden transition-all duration-500 ease-in-out"
+      class="fixed z-40 border-3 flex items-center justify-center box-border border-1 border-black overflow-hidden transition-all duration-500 ease-in-out"
       :style="displayStore.headerStyle"
     >
-      <kind-header class="flex-grow text-center rounded-xl" />
+      <template v-if="displayStore.headerState === 'hidden'">
+        <mode-row class="flex-grow text-center rounded-xl" />
+      </template>
+      <template v-else>
+        <kind-header class="flex-grow text-center rounded-xl" />
+      </template>
     </header>
 
     <!-- Left Sidebar -->
@@ -30,7 +35,7 @@
     <!-- Center Column -->
     <div class="flex flex-col w-full h-full">
       <main
-        class="fixed -z-10 border-3 rounded-2xl overflow-auto bg-base-300 box-border transition-all duration-600 ease-in-out"
+        class="fixed z-10 border-3 rounded-2xl overflow-auto bg-base-300 box-border transition-all duration-600 ease-in-out"
         :style="displayStore.mainContentStyle"
       >
         <NuxtPage :key="$route.fullPath" />
@@ -48,7 +53,7 @@
 
   <!-- Footer -->
   <footer
-    class="fixed -z-30 box-border border-3 overflow-visible transition-all duration-600 ease-in-out"
+    class="fixed z-30 box-border border-3 overflow-visible transition-all duration-600 ease-in-out"
     :style="displayStore.footerStyle"
   >
     <mode-row v-if="footerOpen" class="h-full w-full z-5" />
@@ -56,7 +61,7 @@
 
   <!-- Footer Toggle -->
   <div
-    class="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-50"
+    class="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-40"
     :style="displayStore.footerToggleStyle"
   >
     <footer-toggle />

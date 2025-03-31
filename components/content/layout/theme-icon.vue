af1e1d9a755c09b5bec9b14a4f41fdd98b16f740
Silas Knight <silasfelinus@gmail.com>
2025-03-31 03:46:27 -0700

    unified icons

--- components/content/layout/theme-icon.vue
+++ components/content/layout/theme-icon.vue
@@ -1,20 +1,16 @@
+<!-- /components/content/layout/theme-icon.vue -->
 <template>
   <button
-    class="relative transition-all flex flex-col items-center w-auto space-y-0"
     @click="goToThemePage"
+    class="group flex flex-col items-center justify-center w-[80px] min-w-[72px] max-w-[90px] transition-all relative"
   >
-    <!-- Icon with Scale Hover Effect -->
     <Icon
       name="kind-icon:paintbrush"
       class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out"
     />
-
-    <!-- Theme Name (Visible on md and higher) -->
-    <span class="hidden md:block text-sm transition-colors">
+    <span class="mt-2 text-center text-sm md:block hidden">
       {{ currentTheme }}
     </span>
-
-    <!-- Popup for sm screens -->
     <div
       class="absolute bottom-12 left-1/2 transform -translate-x-1/2 bg-base-200 text-sm rounded-lg p-2 shadow-lg z-50 hidden group-hover:block md:hidden"
     >

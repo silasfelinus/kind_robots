9a48b59d6d403e92e07b571e1835c0bf411d6564
Silas M Knight <silasfelinus@gmail.com>
2025-03-31 01:48:33 -0700

    Update mode-row.vue

--- components/content/story/mode-row.vue
+++ components/content/story/mode-row.vue
@@ -8,7 +8,7 @@
     >
       <div
         @click="handleGalleryMode(mode.name)"
-        class="flex items-center px-3 py-1 gap-1"
+        class="flex items-center px-3 gap-1"
       >
         <span class="text-sm md:text-md font-semibold hidden md:inline">
           {{ mode.label }}
@@ -17,7 +17,7 @@
       <div class="w-px h-6 bg-base-300 rotate-[25deg] -ml-1 -mr-1"></div>
       <div
         @click.stop="handleAddMode(mode.name)"
-        class="flex items-center justify-center px-2 py-1 text-white rounded-r-full hover:scale-105 transition-transform"
+        class="flex items-center justify-center px-2 text-white rounded-r-full hover:scale-105 transition-transform"
       >
         <Icon name="kind-icon:add" class="w-5 h-5" />
       </div>

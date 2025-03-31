217bfc96d861a059bcbd51a9e931ebe771f8ec11
Silas Knight <silasfelinus@gmail.com>
2025-03-31 03:46:27 -0700

    unified icons

--- components/content/user/LoginPath.vue
+++ components/content/user/LoginPath.vue
@@ -1,37 +1,36 @@
+<!-- /components/content/story/login-path.vue -->
 <template>
-  <div class="relative flex flex-col items-start box-border">
-    <!-- User Avatar and Login Button -->
-    <router-link :to="routeToNavigate" class="flex flex-col items-center">
-      <template v-if="isLoggedIn">
-        <div class="flex flex-col items-center">
-          <user-avatar
-            class="h-14 w-14 md:h-16 md:w-16 lg:w-18 lg:h-18 xl:w-20 xl:h-20 rounded-full border border-1 border-base-300"
-          />
-        </div>
-      </template>
-      <template v-else>
-        <button class="flex flex-col items-center space-y-1">
-          <Icon name="kind-icon:person" class="w-10 h-10" />
-          <span v-if="displayStore.isLargeViewport" class="text-sm md:text-base"
-            >Login</span
-          >
-        </button>
-      </template>
-    </router-link>
-  </div>
+  <router-link
+    :to="routeToNavigate"
+    class="group flex flex-col items-center justify-center w-[80px] min-w-[72px] max-w-[90px] transition-all"
+  >
+    <div v-if="isLoggedIn" class="flex flex-col items-center">
+      <user-avatar
+        class="h-10 w-10 md:h-12 md:w-12 lg:h-14 lg:w-14 xl:h-16 xl:w-16 rounded-full border border-base-300 transition-transform transform hover:scale-110 duration-300 ease-in-out"
+      />
+    </div>
+    <div v-else class="flex flex-col items-center">
+      <Icon
+        name="kind-icon:person"
+        class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out"
+      />
+    </div>
+
+    <!-- Label below the icon -->
+    <span class="mt-2 text-center text-sm md:block hidden">
+      {{ isLoggedIn ? username : 'Login?' }}
+    </span>
+  </router-link>
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue'
-import { useUserStore } from '~/stores/userStore'
-import { useDisplayStore } from '@/stores/displayStore'
-
-const displayStore = useDisplayStore()
+import { useUserStore } from '@/stores/userStore'
 
 const userStore = useUserStore()
 const isLoggedIn = computed(() => userStore.isLoggedIn)
+const username = computed(() => userStore.user?.username || 'User')
 
-// Dynamically determine the route to navigate
 const routeToNavigate = computed(() =>
   isLoggedIn.value ? '/dashboard' : '/login',
 )

0fb6e1b183f51a77da1e119a9ce852fca7c093b6
Silas M Knight <silasfelinus@gmail.com>
2025-03-31 01:37:54 -0700

    Update mode-row.vue

--- components/content/story/mode-row.vue
+++ components/content/story/mode-row.vue
@@ -26,10 +26,7 @@
       </div>
     </div>
 
-    <!-- Center Toggle -->
-    <div>
-      <footer-toggle />
-    </div>
+
 
     <!-- Right Modes (last 4) -->
     <div class="flex flex-wrap gap-2">
@@ -49,7 +46,7 @@
         <div class="w-px h-6 bg-base-300 rotate-[25deg] -ml-1 -mr-1"></div>
         <div
           @click.stop="handleAddMode(mode.name)"
-          class="flex items-center justify-center px-2 py-1 text-white bg-accent rounded-r-full hover:scale-105 transition-transform"
+          class="flex items-center justify-center px-2 py-1 text-white rounded-r-full hover:scale-105 transition-transform"
         >
           <Icon name="kind-icon:add" class="w-5 h-5" />
         </div>

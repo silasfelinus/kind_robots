431447475e65b9668d71f5f66cb439cad7809b0d
Silas M Knight <silasfelinus@gmail.com>
2025-03-30 23:28:48 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -62,7 +62,7 @@
 
   <!-- Footer Toggle -->
   <div
-    class="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-40"
+    class="fixed z-40"
     :style="displayStore.footerToggleStyle"
   >
     <footer-toggle />

34b856b1276ba65eda25e79bdf6c7761bf2811a4
Silas Knight <silasfelinus@gmail.com>
2025-04-01 18:32:45 -0700

    better styling and centered

--- components/content/story/magic-container.vue
+++ components/content/story/magic-container.vue
@@ -1,19 +1,18 @@
 // /components/content/story/magic-container.vue
 <template>
-  <div :class="containerClass">
+  <div
+    class="transition-transform duration-500 ease-in-out origin-center"
+    :class="{
+      'scale-[1.5]': displayStore.bigMode,
+      'scale-100': !displayStore.bigMode,
+    }"
+  >
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import { useDisplayStore } from '~/stores/displayStore'
 
 const displayStore = useDisplayStore()
-
-const containerClass = computed(() =>
-  displayStore.bigMode
-    ? 'scale-[1.5] transition-transform duration-500 ease-in-out'
-    : 'scale-100 transition-transform duration-500 ease-in-out',
-)
 </script>

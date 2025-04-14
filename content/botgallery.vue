c7782d8ab6eac00090c7df28fb7abccbbdf39b7c
Silas M Knight <silasfelinus@gmail.com>
2025-04-14 02:09:02 -0700

    Create botgallery.vue

---
+++ content/botgallery.vue
@@ -0,0 +1,16 @@
+---
+title: 'Bot Gallery'
+subtitle: 'Meet the minds (and circuits) of our community'
+description: 'Browse user-made bots that power our chatrooms and creative chaos.'
+gallery: 'botgallery'
+image: 'wonderbot/wonderbot-1142.webp'
+tags: ['community']
+layout: 'default'
+icon: fa-solid:robot
+category: gallery
+tooltip: 'Explore and interact with the bots created by others.'
+dottitip: 'Every bot here started as an idea.'
+amitip: 'And some of them now have cult followings and theme songs!'
+sort: gallery
+---
+:botgallery

6ea5a38a110d22b8f273e600d707d7d3c1c7aece
Silas M Knight <silasfelinus@gmail.com>
2025-04-14 01:57:31 -0700

    Create addpitch.vue

---
+++ content/addpitch.vue
@@ -0,0 +1,16 @@
+---
+title: 'Add Pitch'
+subtitle: 'Submit an idea or a spark of genius'
+description: 'Pitches are the seeds that blossom into wild robot dreams.'
+gallery: 'pitchpool'
+image: 'pitch/pitch-ignite.webp'
+tags: ['brainstorm']
+layout: 'default'
+icon: fa-solid:lightbulb
+category: user
+tooltip: 'Share new ideas, pitches, or taglines for our bots to build from.'
+dottitip: 'Every great invention starts with a pitch.'
+amitip: 'Except me. I started with a swarm and three espresso shots.'
+sort: highlight
+---
+:add-pitch

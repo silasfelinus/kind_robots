d7d8be551a9bee08a24d08c73d45a45609b6b095
Silas Knight <silasfelinus@gmail.com>
2025-04-16 00:11:07 -0700

    tighter header and icons

--- components/content/story/kind-icons.vue
+++ components/content/story/kind-icons.vue
@@ -3,19 +3,15 @@
     v-for="link in navLinks"
     :key="link.path"
     :to="link.path"
-    class="flex flex-col items-center cursor-pointer max-w-[80px]"
-    :title="compact ? '' : link.title"
+    class="group flex flex-col items-center justify-center w-[80px] min-w-[72px] max-w-[90px] transition-all"
   >
     <Icon
       :name="link.icon"
-      class="h-6 w-6 md:h-8 md:w-8 transition-transform hover:scale-110"
+      class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out"
     />
-    <span
-      v-if="!compact"
-      class="text-xs md:text-sm font-semibold text-center mt-1"
-    >
+    <div v-if="!compact" class="mt-2 text-center text-sm md:block hidden">
       {{ link.title }}
-    </span>
+    </div>
   </NuxtLink>
 </template>
 

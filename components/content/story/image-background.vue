6e12f33246c5f23a2800a114ae8b304f0da1f437
Silas Knight <silasfelinus@gmail.com>
2025-04-17 04:56:32 -0700

    layout uses background of unage

---
+++ components/content/story/image-background.vue
@@ -0,0 +1,15 @@
+<!-- /components/content/story/image-background.vue -->
+<template>
+  <img
+    v-if="image"
+    :src="`/images/${image}`"
+    class="w-full h-full object-cover opacity-40"
+    alt="Ambient Background"
+  />
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { usePageStore } from '@/stores/pageStore'
+const { image } = storeToRefs(usePageStore())
+</script>

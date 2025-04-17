2d781530ca77bd9595bc3f9e7bee44e850e5373e
Silas M Knight <silasfelinus@gmail.com>
2025-04-16 19:58:56 -0700

    Update kind-icons.vue

--- components/content/story/kind-icons.vue
+++ components/content/story/kind-icons.vue
@@ -1,18 +1,30 @@
+<!-- /components/content/story/kind-icons.vue -->
 <template>
-  <NuxtLink
-    v-for="link in navLinks"
-    :key="link.path"
-    :to="link.path"
-    class="group flex flex-col items-center justify-center w-[80px] min-w-[72px] max-w-[90px] transition-all"
-  >
-    <Icon
-      :name="link.icon"
-      class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out"
-    />
-    <div v-if="!compact" class="mt-2 text-center text-sm md:block hidden">
-      {{ link.title }}
-    </div>
-  </NuxtLink>
+  <div class="flex gap-2 items-center justify-end w-full">
+    <NuxtLink
+      v-for="link in navLinks"
+      :key="link.path"
+      :to="link.path"
+      class="group flex flex-col items-center justify-center w-[80px] min-w-[72px] max-w-[90px] transition-all"
+    >
+      <Icon
+        :name="link.icon"
+        class="h-8 w-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 transition-transform transform hover:scale-110 duration-300 ease-in-out"
+      />
+      <div
+        v-if="!compact"
+        class="mt-2 text-center text-sm md:block hidden group-hover:block"
+      >
+        {{ link.title }}
+      </div>
+    </NuxtLink>
+
+    <!-- Utility Icons -->
+    <login-path :compact="compact" class="flex max-w-[80px]" />
+    <jellybean-count :compact="compact" class="flex max-w-[80px]" />
+    <theme-icon :compact="compact" class="flex max-w-[80px]" />
+    <swarm-icon :compact="compact" class="flex max-w-[80px]" />
+  </div>
 </template>
 
 <script setup lang="ts">

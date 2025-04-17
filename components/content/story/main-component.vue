217ad8552e49a67d23f7d61d2b9c72eee7b44f3b
Silas M Knight <silasfelinus@gmail.com>
2025-04-16 21:29:13 -0700

    Create main-component.vue

---
+++ components/content/story/main-component.vue
@@ -0,0 +1,21 @@
+<!-- /components/content/story/main-component.vue -->
+<template>
+  <component :is="resolvedComponent" v-if="resolvedComponent" />
+</template>
+
+<script setup lang="ts">
+import { computed, resolveComponent } from 'vue'
+import { useDisplayStore } from '~/stores/displayStore'
+
+const displayStore = useDisplayStore()
+
+const resolvedComponent = computed(() => {
+  const name = displayStore.mainComponent
+  if (!name) return null
+  try {
+    return resolveComponent(name)
+  } catch {
+    return null
+  }
+})
+</script>

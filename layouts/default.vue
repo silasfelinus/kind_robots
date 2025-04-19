4ffc83910a17e47412fea6804bbb4df2d74fff22
Silas Knight <silasfelinus@gmail.com>
2025-04-19 03:18:36 -0700

    removed background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,6 @@
 <!-- /layouts/default.vue -->
 <template>
   <div class="relative w-full h-full overflow-hidden">
-    <!-- Immersive Background Bleed (Right Side Stretch) -->
-    <div class="fixed inset-0 z-0 pointer-events-none overflow-hidden">
-      <div class="absolute top-0 right-0 h-full">
-        <image-background />
-      </div>
-    </div>
-
     <!-- Main App Slot Content (Header, Main, Splash, etc.) -->
     <main class="relative z-10">
       <slot />

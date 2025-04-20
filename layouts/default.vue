9398a8c296e6f06b66331e48a1a01d66eac07ccc
Silas Knight <silasfelinus@gmail.com>
2025-04-20 15:58:08 -0700

    moved image-background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <!-- /layouts/default.vue -->
 <template>
   <div class="relative w-full h-full overflow-hidden">
-    <image-background />
-
     <!-- Main App Slot Content (Header, Main, Splash, etc.) -->
     <main class="relative z-10">
       <slot />

dab1c41e15e5df2a58e6627b7da3f12db5493bda
Silas Knight <silasfelinus@gmail.com>
2025-04-29 02:34:58 -0700

    smart icions smarter

--- components/content/story/smart-icons.vue
+++ components/content/story/smart-icons.vue
@@ -1,36 +1,47 @@
 <!-- /components/content/story/smart-icons.vue -->
 <template>
   <div class="flex w-full items-center justify-end flex-nowrap gap-1 lg:gap-2">
-    <!-- SmartBar Nav Links -->
-    <div
-      v-for="link in filteredNavLinks"
-      :key="link.icon"
-      class="group flex-1 min-w-0 flex flex-col items-center justify-center transition-all"
-    >
-      <NuxtLink
-        v-if="link.path"
-        :to="link.path"
-        class="flex flex-col items-center justify-center w-full"
+    <!-- Normal SmartBar Icons -->
+    <template v-if="filteredNavLinks.length > 0">
+      <div
+        v-for="link in filteredNavLinks"
+        :key="link.icon"
+        class="group flex-1 min-w-0 flex flex-col items-center justify-center transition-all"
       >
-        <Icon
-          :name="link.icon"
-          class="text-sm md:text-md lg:text-4xl xl:text-5xl transition-transform hover:scale-110 duration-300 ease-in-out"
-        />
-        <div
-          v-if="!compact"
-          class="mt-1 text-center text-xs lg:text-sm hidden md:block w-full"
+        <NuxtLink
+          v-if="link.path"
+          :to="link.path"
+          class="flex flex-col items-center justify-center w-full"
         >
-          {{ link.title }}
-        </div>
-      </NuxtLink>
+          <Icon
+            :name="link.icon"
+            class="text-sm md:text-md lg:text-4xl xl:text-5xl transition-transform hover:scale-110 duration-300 ease-in-out"
+          />
+          <div
+            v-if="!compact"
+            class="mt-1 text-center text-xs lg:text-sm hidden md:block w-full"
+          >
+            {{ link.title }}
+          </div>
+        </NuxtLink>
 
-      <!-- Utility Links (without paths) -->
-      <component
-        v-else
-        :is="link.icon"
-        :compact="compact"
-        class="flex flex-col items-center justify-center w-full"
-      />
+        <!-- Utility Links (without paths) -->
+        <component
+          v-else
+          :is="link.icon"
+          :compact="compact"
+          class="flex flex-col items-center justify-center w-full"
+        />
+      </div>
+    </template>
+
+    <!-- Fallback when no icons -->
+    <div
+      v-else
+      class="flex-1 flex flex-col items-center justify-center p-4 text-center text-sm text-base-content/60"
+    >
+      <div>No SmartBar icons found.</div>
+      <div class="text-xs mt-2 break-all">[{{ smartBarItems.join(', ') }}]</div>
     </div>
   </div>
 </template>

91302c73c86a998e67dbdb055cb26a2600fff08f
Silas Knight <silasfelinus@gmail.com>
2025-04-28 19:53:30 -0700

    using art collections instead of gallery

---
+++ prisma/migrations/20250429014810_resonance_remove_image_p_ath/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imagePath` on the `Resonance` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Resonance` DROP COLUMN `imagePath`;

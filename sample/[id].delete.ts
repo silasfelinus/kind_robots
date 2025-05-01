4cb4456507cb52322b0b5ebf02c741fe479e45ad
Silas M Knight <silasfelinus@gmail.com>
2025-04-30 23:59:29 -0700

    Update [id].delete.ts

--- sample/[id].delete.ts
+++ sample/[id].delete.ts
@@ -1,3 +1,7 @@
+// @ts-nocheck
+/* eslint-disable */
+// test-ignore
+
 // /server/api/[model]/[id].delete.ts
 import { defineEventHandler, createError } from 'h3'
 import prisma from '@/server/utils/prisma'

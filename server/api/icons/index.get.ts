b91b36c546d0b6d436abe6cadda7effd0041e346
Silas Knight <silasfelinus@gmail.com>
2025-04-30 17:46:51 -0700

    proper get endpoint

--- server/api/icons/index.get.ts
+++ server/api/icons/index.get.ts
@@ -1,24 +1,24 @@
-// /server/api/resonance/index.get.ts
+// /server/api/icons/index.get.ts
 import { defineEventHandler } from 'h3'
 import prisma from './../utils/prisma'
 import { errorHandler } from './../utils/error'
 
 export default defineEventHandler(async (event) => {
   try {
-    console.log('Fetching all resonances from the database')
+    console.log('Fetching all SmartIcons from the database')
 
-    const data = await prisma.resonance.findMany()
+    const data = await prisma.smartIcon.findMany()
 
-    console.log('All resonances fetched successfully.')
+    console.log('All SmartIcons fetched successfully.')
 
     return {
       success: true,
-      message: 'All resonances fetched successfully.',
+      message: 'All SmartIcons fetched successfully.',
       data,
       statusCode: 200,
     }
   } catch (error: unknown) {
-    console.error('Error occurred while fetching resonances:', error)
+    console.error('Error occurred while fetching SmartIcons:', error)
 
     const { success, message, statusCode } = errorHandler(error)
 

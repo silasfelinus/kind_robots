ef98203616b3df201a0baf7b43ce89f616fb1720
Silas M Knight <silasfelinus@gmail.com>
2025-05-04 01:27:34 -0700

    Update smart-icons.vue

--- components/content/icons/smart-icons.vue
+++ components/content/icons/smart-icons.vue
@@ -34,7 +34,7 @@
             >
               <Icon
                 :name="icon.icon || 'lucide:help-circle'"
-                class="hover:scale-110 transition-transform text-3xl w-[3rem] h-[5rem]"
+                class="hover:scale-110 transition-transform text-3xl w-[3rem] h-[3rem]"
               />
               <span
                 v-if="!bigMode"
@@ -47,7 +47,7 @@
             <!-- Utility Component -->
             <div
               v-else-if="icon.type === 'utility'"
-              class="flex flex-col items-center justify-center text-3xl w-[3rem] h-[5rem]"
+              class="flex flex-col items-center justify-center text-3xl w-[3rem] h-[3rem]"
             >
               <component :is="icon.component" />
         
@@ -77,7 +77,7 @@
           <NuxtLink
             to="/icongallery"
             @click="isEditing && confirmEdit()"
-            class="group flex flex-col items-center justify-center text-3xl w-[3rem] h-[5rem] snap-start"
+            class="group flex flex-col items-center justify-center text-3xl w-[3rem] h-[3rem] snap-start"
           >
             <Icon name="lucide:plus-circle" class="hover:scale-110 transition-transform" />
             <span

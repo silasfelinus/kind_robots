039dc6865852ebc506b6d2384e53d901d0264985
Silas M Knight <silasfelinus@gmail.com>
2025-05-09 19:47:33 -0700

    Update login-icon.vue

--- components/content/icons/login-icon.vue
+++ components/content/icons/login-icon.vue
@@ -1,51 +1,38 @@
 <!-- /components/content/story/login-icon.vue -->
 <template>
-  <icon-shell>
-    <template #icon>
-      <router-link
-        :to="routeToNavigate"
-        class="w-full h-full flex items-center justify-center transition-transform hover:scale-110"
-      >
-        <user-avatar
-          v-if="isLoggedIn"
-          class="w-full h-full max-w-[3rem] max-h-[3rem] rounded-full border border-base-300"
-        />
-        <Icon
-          v-else
-          name="kind-icon:person"
-          class="w-full h-full max-w-[3rem] max-h-[3rem]"
-        />
-      </router-link>
-    </template>
-
-    <template #label>
-      <span
-        v-if="!isEditing && !bigMode"
-        class="absolute bottom-0 left-1/2 -translate-x-1/2 h-[1.25rem] w-full flex items-center justify-center pointer-events-none z-40 text-xs text-center leading-none"
-      >
-        {{ isLoggedIn ? username : 'Login?' }}
-      </span>
-    </template>
-  </icon-shell>
+  <router-link
+    :to="routeToNavigate"
+    class="w-full h-full flex items-center justify-center transition-transform hover:scale-110"
+  >
+    <user-avatar
+      v-if="isLoggedIn"
+      class="w-full h-full max-w-[3rem] max-h-[3rem] rounded-full border border-base-300"
+    />
+    <Icon
+      v-else
+      name="kind-icon:person"
+      class="w-full h-full max-w-[3rem] max-h-[3rem]"
+    />
+  </router-link>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useUserStore } from '@/stores/userStore'
-import { useDisplayStore } from '@/stores/displayStore'
 import { useIconStore } from '@/stores/iconStore'
 import { storeToRefs } from 'pinia'
 
 const userStore = useUserStore()
-const displayStore = useDisplayStore()
 const iconStore = useIconStore()
-
 const { isEditing } = storeToRefs(iconStore)
-const { bigMode } = storeToRefs(displayStore)
 
 const isLoggedIn = computed(() => userStore.isLoggedIn)
 const username = computed(() => userStore.user?.username || 'User')
+const navLabel = computed(() => (isLoggedIn.value ? username.value : 'Login?'))
 const routeToNavigate = computed(() =>
   isLoggedIn.value ? '/dashboard' : '/login',
 )
+
+// expose for smart-icons label
+defineExpose({ navLabel })
 </script>

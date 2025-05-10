1e8dade5c9ae26b79c2beb408f98dbf74e25cbc2
Silas M Knight <silasfelinus@gmail.com>
2025-05-09 19:48:08 -0700

    Update jellybean-icon.vue

--- components/content/icons/jellybean-icon.vue
+++ components/content/icons/jellybean-icon.vue
@@ -1,42 +1,27 @@
-// /components/content/icons/jellybean-icon.vue
+<!-- /components/content/icons/jellybean-icon.vue -->
 <template>
-  <icon-shell>
-    <template #icon>
-      <router-link
-        to="/milestones"
-        class="w-full h-full flex items-center justify-center transition-transform hover:scale-110"
-      >
-        <Icon
-          name="kind-icon:jellybean"
-          class="w-full h-full max-w-[3rem] max-h-[3rem]"
-        />
-      </router-link>
-    </template>
-
-    <template #label>
-      <span
-        v-if="!isEditing && !bigMode"
-        class="absolute bottom-0 left-1/2 -translate-x-1/2 h-[1.25rem] w-full flex items-center justify-center pointer-events-none z-40 text-xs text-center leading-none"
-      >
-        {{ beanCount || 0 }} /11
-      </span>
-    </template>
-  </icon-shell>
+  <router-link
+    to="/milestones"
+    class="w-full h-full flex items-center justify-center transition-transform hover:scale-110"
+  >
+    <Icon
+      name="kind-icon:jellybean"
+      class="w-full h-full max-w-[3rem] max-h-[3rem]"
+    />
+  </router-link>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
 import { computed } from 'vue'
-import { storeToRefs } from 'pinia'
 import { useMilestoneStore } from '@/stores/milestoneStore'
-import { useDisplayStore } from '@/stores/displayStore'
-import { useIconStore } from '@/stores/iconStore'
 
 const milestoneStore = useMilestoneStore()
-const displayStore = useDisplayStore()
-const iconStore = useIconStore()
 
-const { bigMode } = storeToRefs(displayStore)
-const { isEditing } = storeToRefs(iconStore)
+const navLabel = computed(() => {
+  const count = milestoneStore.milestoneCountForUser || 0
+  return `${count} /11`
+})
 
-const beanCount = computed(() => milestoneStore.milestoneCountForUser)
+// expose for smart-icons
+defineExpose({ navLabel })
 </script>

9d9859cd78cc8f9099686b9aeaadcb244732e2fc
Silas Knight <silasfelinus@gmail.com>
2025-05-10 16:14:19 -0700

    moved butterfly animation to app.vue

--- components/content/icons/swarm-icon.vue
+++ components/content/icons/swarm-icon.vue
@@ -1,23 +1,17 @@
-<!-- /components/content/story/swarm-icon.vue -->
 <template>
   <div
-    class="group relative flex items-center justify-center hover:scale-110"
+    class="group relative flex items-center justify-center"
     @click="toggleAmiSwarm"
   >
-    <div class="flex items-center justify-center w-[3rem] h-[3rem]">
+    <div
+      class="flex items-center justify-center w-[3rem] h-[3rem] hover:scale-110 transition-transform duration-300"
+    >
       <Icon
         name="kind-icon:butterfly"
         title="Kind Butterflies"
-        :class="['transition-transform', 'duration-300', { glow: showSwarm }]"
+        :class="['transition-transform duration-300', { glow: showSwarm }]"
         class="w-full h-full max-w-[3rem] max-h-[3rem]"
       />
-    </div>
-
-    <div
-      v-if="showSwarm"
-      class="fixed inset-0 overflow-hidden z-50 pointer-events-none full-page"
-    >
-      <butterfly-animation />
     </div>
   </div>
 </template>
@@ -31,9 +25,8 @@ const iconStore = useIconStore()
 const showSwarm = computed(() => iconStore.showSwarm)
 
 const toggleAmiSwarm = () => {
-  setTimeout(() => {
-    iconStore.toggleSwarm()
-  }, 10)
+  iconStore.toggleSwarm()
+  console.log('swarm toggled')
 }
 </script>
 
@@ -41,9 +34,5 @@ const toggleAmiSwarm = () => {
 .glow {
   box-shadow: 0 0 8px rgba(255, 255, 0, 0.8);
   transition: box-shadow 0.3s ease-in-out;
-}
-.full-page {
-  width: 100vw;
-  height: 100vh;
 }
 </style>

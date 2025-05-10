aad3b12e507a26c4a861333a73523601297f44f4
Silas Knight <silasfelinus@gmail.com>
2025-05-09 18:18:33 -0700

    testring simpler labels

--- components/content/icons/login-icon.vue
+++ components/content/icons/login-icon.vue
@@ -21,7 +21,7 @@
     <template #label>
       <span
         v-if="!isEditing && !bigMode"
-        class="absolute bottom-0 left-1/2 -translate-x-1/2 h-[1.25rem] w-full flex items-center justify-center pointer-events-none z-40"
+        class="text-center w-full text-xs truncate"
       >
         {{ isLoggedIn ? username : 'Login?' }}
       </span>

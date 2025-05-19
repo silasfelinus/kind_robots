32321665be96b9949a0c73a1c090fba76327b74f
Silas Knight <silasfelinus@gmail.com>
2025-05-18 17:12:52 -0700

    removed localStorage check when not client

--- pages/auth/google.vue
+++ pages/auth/google.vue
@@ -7,17 +7,20 @@ const router = useRouter()
 const route = useRoute()
 const userStore = useUserStore()
 
-const token = route.query.token as string | undefined
+onMounted(async () => {
+  const token = route.query.token as string | undefined
 
-if (token) {
-  // Save token locally
-  localStorage.setItem('googleToken', token)
-  await userStore.initialize(token)
-  await router.push('/dashboard')
-} else {
-  await router.push('/login')
-}
+  if (token) {
+    // Save token locally
+    localStorage.setItem('googleToken', token)
+    await userStore.initialize(token)
+    await router.push('/dashboard')
+  } else {
+    await router.push('/login')
+  }
+})
 </script>
+
 <template>
   <div class="text-center p-6 text-info">Logging you in via Google…</div>
 </template>

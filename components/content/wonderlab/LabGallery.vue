d5c63a614e282bab7b662a7f34ef7c4570367815
Silas M Knight <silasfelinus@gmail.com>
2025-05-18 20:18:39 -0700

    Update LabGallery.vue

--- components/content/wonderlab/LabGallery.vue
+++ components/content/wonderlab/LabGallery.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="bg-base-300 min-h-screen">
     <!-- Folder View -->
-    <div
-      v-if="folderNames.length && !selectedComponents.length"
-      class="overflow-y-auto grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4"
-    >
-      <div
-        v-for="folder in folderNames"
-        :key="folder"
-        class="rounded-lg hover:bg-primary hover:text-default cursor-pointer transition-transform transform hover:scale-105"
-        @click="fetchComponentsFromFolder(folder)"
-      >
-        <div class="text-center">
-          <Icon name="kind-icon:folder" class="text-4xl" />
-          <p class="mt-2">{{ folder }}</p>
-        </div>
-      </div>
+<div
+  v-if="folderNames.length && !selectedComponents.length"
+  class="flex-1 min-h-0 overflow-y-auto grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4 p-4"
+>
+  <!-- Folders -->
+  <div
+    v-for="folder in folderNames"
+    :key="folder"
+    class="rounded-lg hover:bg-primary hover:text-default cursor-pointer transition-transform transform hover:scale-105"
+    @click="fetchComponentsFromFolder(folder)"
+  >
+    <div class="text-center">
+      <Icon name="kind-icon:folder" class="text-4xl" />
+      <p class="mt-2">{{ folder }}</p>
     </div>
+  </div>
+</div>
 
     <!-- Component List View -->
     <div v-if="selectedComponents.length" class="flex flex-col h-[80vh]">

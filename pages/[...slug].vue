be31036c73ca577492f0964f3c4c9c0a6fb43540
Silas Knight <silasfelinus@gmail.com>
2025-05-18 19:23:47 -0700

    removed #content

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,4 +1,3 @@
-
 <!-- /pages/[...slug].vue -->
 <template>
   <NuxtLayout :name="layout">
@@ -13,7 +12,6 @@
 
 <script setup lang="ts">
 import { useRoute, useRouter } from '#app'
-import { queryCollection } from '#content'
 
 import { useUserStore } from '@/stores/userStore'
 import { useBotStore } from '@/stores/botStore'
@@ -23,20 +21,25 @@ import { useChatStore } from '@/stores/chatStore'
 import { usePitchStore } from '@/stores/pitchStore'
 import { usePromptStore } from '@/stores/promptStore'
 import { useDisplayStore } from '@/stores/displayStore'
-import type { displayModeState, displayActionState } from '@/stores/displayStore'
+import type {
+  displayModeState,
+  displayActionState,
+} from '@/stores/displayStore'
 
 const route = useRoute()
 const router = useRouter()
 
-const { data: page } = await useAsyncData(route.path, () =>
-  queryCollection('content').path(route.path).first()
+const fullPath = route.path || '/'
+const { data: page } = await useAsyncData(fullPath, () =>
+  queryCollection('content').path(fullPath).first(),
 )
 
 const layout = computed(() => {
   const val = page.value?.layout
-  return ['default', 'minimal', 'vertical-scroll'].includes(val) ? val : 'default'
-})
-
+  return ['default', 'minimal', 'vertical-scroll'].includes(val as string)
+    ? (val as LayoutKey)
+    : 'default'
+}) as ComputedRef<LayoutKey>
 const displayStore = useDisplayStore()
 const userStore = useUserStore()
 const botStore = useBotStore()
@@ -59,15 +62,16 @@ const {
 } = route.query
 
 if (displayMode) displayStore.displayMode = displayMode as displayModeState
-if (displayAction) displayStore.displayAction = displayAction as displayActionState
+if (displayAction)
+  displayStore.displayAction = displayAction as displayActionState
 if (botId) botStore.selectBot(Number(botId))
 if (characterId) characterStore.selectCharacter(Number(characterId))
 if (scenarioId) scenarioStore.selectScenario(Number(scenarioId))
 if (chatId) chatStore.selectChat(Number(chatId))
 if (pitchId) pitchStore.selectPitch(Number(pitchId))
 if (promptId) promptStore.selectPrompt(Number(promptId))
-if (queryToken && !userStore.user) await userStore.initialize(queryToken as string)
+if (queryToken && !userStore.user)
+  await userStore.initialize(queryToken as string)
 if (!userStore.user && queryToken) await router.push('/login')
 if (!page.value) await router.push('/error')
 </script>
-

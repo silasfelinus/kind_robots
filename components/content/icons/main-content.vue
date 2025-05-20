2629cc59a3a77284a9adb6b353857fc060a88dba
Silas Knight <silasfelinus@gmail.com>
2025-05-19 18:27:18 -0700

    working on bug fixes for scroll

--- components/content/icons/main-content.vue
+++ components/content/icons/main-content.vue
@@ -1,23 +1,27 @@
 <!-- /components/content/icons/main-content.vue -->
 <template>
-  <div class="relative min-h-[100dvh] w-full rounded-2xl bg-base-300">
-    <!-- Mobile View: Flip between page and splash -->
-    <div v-if="displayStore.isMobileViewport" class="min-h-[100dvh] w-full">
+  <div
+    class="relative flex flex-col min-h-[100dvh] w-full rounded-2xl bg-base-300"
+  >
+    <!-- Mobile View -->
+    <div v-if="displayStore.isMobileViewport" class="flex-1 w-full">
       <flip-panel :flipped="sidebarRightOpen">
         <template #front>
-          <div class="relative min-h-[100dvh] w-full overflow-y-auto">
-            <NuxtPage :key="$route.fullPath" class="min-h-[100dvh] w-full" />
+          <div class="relative h-[100dvh] w-full overflow-y-auto">
+            <NuxtPage :key="$route.fullPath" class="min-h-full w-full" />
           </div>
         </template>
         <template #back>
-          <splash-tutorial />
+          <div class="relative h-[100dvh] w-full overflow-y-auto">
+            <splash-tutorial />
+          </div>
         </template>
       </flip-panel>
     </div>
 
-    <!-- Desktop View: Always show both -->
-    <div v-else class="relative min-h-[100dvh] w-full overflow-y-auto">
-      <NuxtPage :key="$route.fullPath" class="min-h-[100dvh] w-full" />
+    <!-- Desktop View -->
+    <div v-else class="relative flex-1 min-h-[100dvh] w-full overflow-y-auto">
+      <NuxtPage :key="$route.fullPath" class="min-h-full w-full" />
     </div>
 
     <!-- Right Sidebar & Toggle -->
@@ -30,14 +34,13 @@
     />
     <aside
       v-if="!displayStore.isMobileViewport && sidebarRightOpen"
-      class="fixed z-30 rounded-2xl border-6 border-secondary"
+      class="fixed z-30 rounded-2xl border-6 border-secondary overflow-y-auto bg-base-200"
       :style="displayStore.rightSidebarStyle"
     >
       <splash-tutorial />
     </aside>
   </div>
 </template>
-
 
 <script setup lang="ts">
 // /components/content/icons/main-content.vue

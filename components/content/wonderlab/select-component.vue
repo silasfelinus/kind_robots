41cecc692da973d66ec59b595c125867b925f499
Silas Knight <silasfelinus@gmail.com>
2025-05-20 21:11:40 -0700

    select-component

---
+++ components/content/wonderlab/select-component.vue
@@ -0,0 +1,40 @@
+<!-- /components/content/wonderlab/select-component.vue -->
+<template>
+  <div
+    class="relative w-full h-full min-h-[100dvh] bg-base-100 text-base-content"
+  >
+    <!-- Fullscreen Component Display -->
+    <component
+      :is="component.componentName"
+      v-if="component?.componentName"
+      class="w-full h-full"
+    />
+
+    <!-- Floating Back Button -->
+    <button
+      class="fixed bottom-4 left-4 z-30 btn btn-sm btn-outline btn-primary shadow-md"
+      @click="handleBack"
+    >
+      <Icon name="kind-icon:arrow-left" class="mr-1" />
+      Back to WonderLab
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRef } from 'vue'
+import { useComponentStore } from '@/stores/componentStore'
+import type { KindComponent as Component } from '@/stores/componentStore'
+
+const props = defineProps<{
+  component: Component
+}>()
+
+const component = toRef(props, 'component')
+const componentStore = useComponentStore()
+
+const handleBack = () => {
+  componentStore.clearSelectedComponent()
+  componentStore.clearSelectedFolder()
+}
+</script>

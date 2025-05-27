e55f36e4c16e1b2ca47a60dbfd8d7a87112b5b3c
Silas M Knight <silasfelinus@gmail.com>
2025-05-26 19:06:32 -0700

    Update themeHelpers.ts

--- stores/helpers/themeHelpers.ts
+++ stores/helpers/themeHelpers.ts
@@ -36,6 +36,16 @@ export const extraVars = [
   '--noise',
 ]
 
+export const daisyuiThemes = [
+  'light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate',
+  'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden',
+  'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black',
+  'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade',
+  'night', 'coffee', 'winter', 'dim', 'nord', 'sunset', 'caramellatte',
+  'abyss', 'silk',
+]
+
+
 // Default fallback extras
 export const defaultExtraVars: Record<string, string> = {
   '--radius-selector': '0.5rem',

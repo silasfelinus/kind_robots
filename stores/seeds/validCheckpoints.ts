eb1f326c78b04e4174b1dd8d949f07fb1d8b0554
Silas Knight <silasfelinus@gmail.com>
2025-06-07 17:09:25 -0700

    better checkpoints and setModel

--- stores/seeds/validCheckpoints.ts
+++ stores/seeds/validCheckpoints.ts
@@ -6,8 +6,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'darkSushiMixMix_colorful.safetensors',
     customLabel: 'darkSushiMixMix_colorful',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/1.5/darkSushiMixMix_colorful.safetensors',
+    localPath: '1.5/darkSushiMixMix_colorful.safetensors',
     description: 'Auto-imported from 1.5 model folder.',
     generation: '1.5',
     isMature: true,
@@ -26,21 +25,9 @@ export const validCheckpoints: Partial<Resource>[] = [
     userId: 1,
   },
   {
-    name: 'v3_sd15_mm.ckpt',
-    customLabel: 'v3_sd15_mm',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/1.5/v3_sd15_mm.ckpt',
-    description: 'Auto-imported from 1.5 model folder.',
-    generation: '1.5',
-    isMature: false,
-    resourceType: 'CHECKPOINT',
-    userId: 1,
-  },
-  {
-    name: 'ponyFaetality_v11.safetensors',
+    name: 'ponyFaetality_v11.safetensors [499513276a]',
     customLabel: 'ponyFaetality_v11',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/_Pony/ponyFaetality_v11.safetensors',
+    localPath: '_Pony/ponyFaetality_v11.safetensors',
     description: 'Auto-imported from _Pony model folder.',
     generation: 'Pony',
     isMature: true,
@@ -50,8 +37,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'cyberrealisticPony_v61.safetensors',
     customLabel: 'cyberrealisticPony_v61',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/ARCHIVE/CyberRealistic_Pony/cyberrealisticPony_v61.safetensors',
+    localPath: 'ARCHIVE/CyberRealistic_Pony/cyberrealisticPony_v61.safetensors',
     description: 'Auto-imported from ARCHIVE model folder.',
     generation: 'ARCHIVE',
     isMature: true,
@@ -61,8 +47,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'revAnimated_v2Rebirth.safetensors',
     customLabel: 'revAnimated_v2Rebirth',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/ARCHIVE/revAnimated_v2Rebirth.safetensors',
+    localPath: 'ARCHIVE/revAnimated_v2Rebirth.safetensors',
     description: 'Auto-imported from ARCHIVE model folder.',
     generation: 'ARCHIVE',
     isMature: true,
@@ -70,10 +55,9 @@ export const validCheckpoints: Partial<Resource>[] = [
     userId: 1,
   },
   {
-    name: 'cartoonArcadiaSDXLSD1_v2.safetensors',
+    name: 'cartoonArcadiaSDXLSD1_v2.safetensors [eb2b3e9a5f]',
     customLabel: 'cartoonArcadiaSDXLSD1_v2',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/cartoonArcadiaSDXLSD1_v2.safetensors',
+    localPath: 'cartoonArcadiaSDXLSD1_v2.safetensors',
     description: 'Auto-imported from base model folder.',
     generation: 'base',
     isMature: true,
@@ -83,8 +67,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'disneyrealcartoonmix_v10.1.safetensors',
     customLabel: 'disneyrealcartoonmix_v10.1',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/disneyrealcartoonmix_v10.1.safetensors',
+    localPath: 'disneyrealcartoonmix_v10.1.safetensors',
     description: 'Auto-imported from base model folder.',
     generation: 'base',
     isMature: false,
@@ -92,66 +75,9 @@ export const validCheckpoints: Partial<Resource>[] = [
     userId: 1,
   },
   {
-    name: 'flux1-dev-fp8.safetensors',
-    customLabel: 'flux1-dev-fp8',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/Flux/flux1-dev-fp8.safetensors',
-    description: 'Auto-imported from Flux model folder.',
-    generation: 'Flux',
-    isMature: false,
-    resourceType: 'CHECKPOINT',
-    userId: 1,
-  },
-  {
-    name: 'flux1-schnell-fp8.safetensors',
-    customLabel: 'flux1-schnell-fp8',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/Flux/flux1-schnell-fp8.safetensors',
-    description: 'Auto-imported from Flux model folder.',
-    generation: 'Flux',
-    isMature: false,
-    resourceType: 'CHECKPOINT',
-    userId: 1,
-  },
-  {
-    name: 'flux_dev_fp8_scaled_diffusion_model.safetensors',
-    customLabel: 'flux_dev_fp8_scaled_diffusion_model',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/Flux/flux_dev_fp8_scaled_diffusion_model.safetensors',
-    description: 'Auto-imported from Flux model folder.',
-    generation: 'Flux',
-    isMature: false,
-    resourceType: 'CHECKPOINT',
-    userId: 1,
-  },
- 
-  {
-    name: 'trueRealVisionFlux_v276AIO.safetensors',
-    customLabel: 'trueRealVisionFlux_v276AIO',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/Flux/trueRealVisionFlux_v276AIO.safetensors',
-    description: 'Auto-imported from Flux model folder.',
-    generation: 'Flux',
-    isMature: true,
-    resourceType: 'CHECKPOINT',
-    userId: 1,
-  },
-  {
-    name: 'uncensoredFemalesFLUX_nf4FLUXDev.safetensors',
-    customLabel: 'uncensoredFemalesFLUX_nf4FLUXDev',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/Flux/uncensoredFemalesFLUX_nf4FLUXDev.safetensors',
-    description: 'Auto-imported from Flux model folder.',
-    generation: 'Flux',
-    isMature: true,
-    resourceType: 'CHECKPOINT',
-    userId: 1,
-  },
-  {
     name: 'hotart_3SPO.safetensors',
     customLabel: 'hotart_3SPO',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/hotart_3SPO.safetensors',
+    localPath: 'hotart_3SPO.safetensors',
     description: 'Auto-imported from base model folder.',
     generation: 'base',
     isMature: true,
@@ -161,8 +87,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'ivisionIllustration_ivision10.safetensors',
     customLabel: 'ivisionIllustration_ivision10',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/ivisionIllustration_ivision10.safetensors',
+    localPath: 'ivisionIllustration_ivision10.safetensors',
     description: 'Auto-imported from base model folder.',
     generation: 'base',
     isMature: true,
@@ -172,8 +97,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'realcartoonPony_v1.safetensors',
     customLabel: 'realcartoonPony_v1',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/realcartoonPony_v1.safetensors',
+    localPath: 'realcartoonPony_v1.safetensors [2295809170]',
     description: 'Auto-imported from base model folder.',
     generation: 'base',
     isMature: true,
@@ -183,8 +107,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'bigLove_xl25.safetensors',
     customLabel: 'bigLove_xl25',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/SDXL/bigLove_xl25.safetensors',
+    localPath: 'SDXL/bigLove_xl25.safetensors [19447d345a]',
     description: 'Auto-imported from SDXL model folder.',
     generation: 'SDXL',
     isMature: true,
@@ -194,8 +117,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'biglust17_v17.safetensors',
     customLabel: 'biglust17_v17',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/SDXL/biglust17_v17.safetensors',
+    localPath: 'SDXL/biglust17_v17.safetensors',
     description: 'Auto-imported from SDXL model folder.',
     generation: 'SDXL',
     isMature: true,
@@ -205,8 +127,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'lustifySDXLNSFW_oltFIXEDTEXTURES.safetensors',
     customLabel: 'lustifySDXLNSFW_oltFIXEDTEXTURES',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/SDXL/lustifySDXLNSFW_oltFIXEDTEXTURES.safetensors',
+    localPath: 'SDXL/lustifySDXLNSFW_oltFIXEDTEXTURES.safetensors',
     description: 'Auto-imported from SDXL model folder.',
     generation: 'SDXL',
     isMature: true,
@@ -216,8 +137,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'duskMixXLIllustration_v15.safetensors',
     customLabel: 'duskMixXLIllustration_v15',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/SDXL/tier2/duskMixXLIllustration_v15.safetensors',
+    localPath: 'SDXL/tier2/duskMixXLIllustration_v15.safetensors',
     description: 'Auto-imported from SDXL model folder.',
     generation: 'SDXL',
     isMature: true,
@@ -227,8 +147,7 @@ export const validCheckpoints: Partial<Resource>[] = [
   {
     name: 'ThinkDiffusionXL.safetensors',
     customLabel: 'ThinkDiffusionXL',
-    localPath:
-      '/stable-diffusion-webui/models/Stable-diffusion/SDXL/tier2/ThinkDiffusionXL.safetensors',
+    localPath: 'SDXL/tier2/ThinkDiffusionXL.safetensors [a21c9949ef]',
     description: 'Auto-imported from SDXL model folder.',
     generation: 'SDXL',
     isMature: true,

a1e8dedce81aa9eebfa84baf16b1f681164152a2
Silas M Knight <silasfelinus@gmail.com>
2025-06-16 04:17:41 -0700

    Update add-art.vue

--- components/content/art/add-art.vue
+++ components/content/art/add-art.vue
@@ -23,11 +23,17 @@
       <art-museum />
     </template>
 
-    <!-- Extra Section (bottom 20%) -->
-    <template #extra>
-      <prompt-handler />
-      <generate-button />
-    </template>
+<template #extra>
+  <div class="relative w-full h-full flex flex-col justify-end gap-4 p-4 pb-20">
+    <prompt-handler class="flex-grow" />
+    <div class="fixed bottom-4 left-0 right-0 z-40 px-4 flex justify-center pointer-events-none">
+      <div class="pointer-events-auto">
+        <generate-button />
+      </div>
+    </div>
+  </div>
+</template>
+
 
     <!-- Overlay (full-screen focus area) -->
     <template #overlay>

f1ad37990cea86aa1e0f0f7e97430139657a615e
Silas M Knight <silasfelinus@gmail.com>
2025-06-23 19:07:25 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -62,7 +62,7 @@
 
       <!-- Right Sidebar Toggle -->
       <right-toggle
-        class="fixed bottom-14 right-4 z-50"
+        class="fixed bottom-14 right-4 z-40"
         :class="{
           'bg-accent text-white shadow-xl': sidebarRightOpen,
           'bg-base-300 shadow': !sidebarRightOpen,

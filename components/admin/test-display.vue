b62d82c1780b1822140d11842bf9ac09d02309f8
Silas Knight <silasfelinus@gmail.com>
2025-06-30 14:01:18 -0700

    removed test pages

--- components/admin/test-display.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="w-full bg-warning text-black flex items-center justify-center text-2xl font-bold rounded-2xl p-4"
-  >
-    ✅ test-display.vue IS WORKING
-  </div>
-</template>

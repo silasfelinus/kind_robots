996c2e8c4e003283a99f05c73b34eeb7ca817475
Silas M Knight <silasfelinus@gmail.com>
2025-07-08 00:27:20 -0700

    Create sandbox-icon.vue

---
+++ components/sandbox/sandbox-icon.vue
@@ -0,0 +1,19 @@
+<!-- /components/content/sandbox/sandbox-icon.vue -->
+<template>
+  <button
+    class="btn btn-square btn-sm tooltip tooltip-right"
+    :data-tip="label"
+    @click="$emit('click')"
+  >
+    <Icon :name="icon" class="text-xl" />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  label?: string
+}>()
+
+defineEmits(['click'])
+</script>

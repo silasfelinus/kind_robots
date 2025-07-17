b83ad35cad08bca792b80ff6132950c52d0d6f48
Silas Knight <silasfelinus@gmail.com>
2025-07-16 17:03:15 -0700

    link items now utilized for consistent nav pages

---
+++ components/navigation/icon-nav.vue
@@ -0,0 +1,23 @@
+<!-- /components/content/story/icon-nav.vue -->
+<template>
+  <nav-grid :cards="cards" />
+</template>
+
+<script setup lang="ts">
+import type { LinkItem } from '~/stores/linkStore'
+
+const cards: LinkItem[] = [
+  {
+    title: 'Add Icon',
+    path: '/addicon',
+    icon: 'kind-icon:plus',
+    description: 'Create a new SmartIcon with custom metadata.',
+  },
+  {
+    title: 'Icon Gallery',
+    path: '/icons',
+    icon: 'kind-icon:grid',
+    description: 'Browse and manage all SmartIcons in the system.',
+  },
+]
+</script>
---
+++ components/navigation/vertical-nav.vue
@@ -0,0 +1,53 @@
+<!-- /components/content/layout/vertical-nav.vue -->
+<template>
+  <nav-grid :cards="tabs" />
+</template>
+
+<script setup lang="ts">
+import type { LinkItem } from '~/stores/linkStore'
+
+const tabs: LinkItem[] = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'kind-icon:home',
+    description: 'Return to the main world of Kind Robots',
+  },
+  {
+    title: 'Bot Cafe',
+    path: '/botcafe',
+    icon: 'kind-icon:addbot',
+    description: 'Create and customize your bots',
+  },
+  {
+    title: 'Prompt Factory',
+    path: '/pitch',
+    icon: 'kind-icon:chatbubble',
+    description: 'Cook up new ideas for AI generation',
+  },
+  {
+    title: 'Art Lab',
+    path: '/artmaker',
+    icon: 'kind-icon:easel',
+    description: 'Generate and remix AI artwork',
+  },
+  {
+    title: 'Memory Match',
+    path: '/memory',
+    icon: 'kind-icon:brain',
+    description: 'Play and train your memory skills',
+  },
+  {
+    title: 'Wonderlab',
+    path: '/wonderlab',
+    icon: 'kind-icon:gearhammer',
+    description: 'Experiment with advanced AI tools',
+  },
+  {
+    title: 'Dashboard',
+    path: '/dashboard',
+    icon: 'kind-icon:settings',
+    description: 'Manage your account and stats',
+  },
+]
+</script>

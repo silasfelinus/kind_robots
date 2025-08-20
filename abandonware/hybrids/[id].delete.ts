29de23794d7d931306e4314b056682665ea555f0
Silas Knight <silasfelinus@gmail.com>
2025-08-20 15:11:39 -0700

    removed pantheon sandbox blueprint hybrid

--- abandonware/hybrids/[id].delete.ts
+++ abandonware/hybrids/[id].delete.ts
@@ -1,8 +1,8 @@
 // /server/api/hybrids/[id].delete.ts
 import { defineEventHandler, createError } from 'h3'
-import prisma from '../utils/prisma'
-import { errorHandler } from '../utils/error'
-import { validateApiKey } from '../utils/validateKey'
+import prisma from '../../server/api/utils/prisma'
+import { errorHandler } from '../../server/api/utils/error'
+import { validateApiKey } from '../../server/api/utils/validateKey'
 
 export default defineEventHandler(async (event) => {
   let response

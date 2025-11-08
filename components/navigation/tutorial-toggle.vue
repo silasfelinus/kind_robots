8fcadd4899a6e5280d477a96b5025c8c860cd4ab
Silas Knight <silasfelinus@gmail.com>
2025-11-08 14:14:30 -0800

    removed border

--- components/navigation/tutorial-toggle.vue
+++ components/navigation/tutorial-toggle.vue
@@ -2,7 +2,7 @@
 <template>
   <button
     type="button"
-    class="group flex justify-center items-center rounded-full border border-base-content/30 hover:border-primary focus:outline-none transition-all duration-300 w-10 h-10 sm:w-11 sm:h-11"
+    class="group flex justify-center items-center focus:outline-none transition-all duration-300 w-10 h-10 sm:w-11 sm:h-11"
     @click="toggleTutorial"
   >
     <Icon
@@ -13,7 +13,7 @@
     <Icon
       v-else
       :name="icon || 'kind-icon:tutorial'"
-      class="w-6 h-6 text-base-content/70 group-hover:text-primary group-hover:scale-110 transition-all duration-300"
+      class="w-6 h-6 text-base-content/70 group-hover:text-primary group-hover:scale-120 transition-all duration-300"
     />
   </button>
 </template>

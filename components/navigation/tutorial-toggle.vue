16a854993765f1c9141a4d1cfa1f5f84da05da34
Silas Knight <silasfelinus@gmail.com>
2025-11-07 17:28:23 -0800

    slimmer toggle

--- components/navigation/tutorial-toggle.vue
+++ components/navigation/tutorial-toggle.vue
@@ -1,20 +1,27 @@
 <!-- /components/content/layout/tutorial-toggle.vue -->
 <template>
-  <div
-    class="flex justify-center items-center bg-primary hover:bg-accent transition duration-300 cursor-pointer rounded-lg shadow-md p-3"
+  <button
+    type="button"
+    class="group flex justify-center items-center rounded-full border border-base-content/30 hover:border-primary focus:outline-none transition-all duration-300 w-10 h-10 sm:w-11 sm:h-11"
     @click="toggleTutorial"
   >
     <Icon
       v-if="showTutorial"
-      name="mdi-information-outline"
-      class="text-base-200 w-8 h-8"
+      name="kind-icon:info"
+      class="w-6 h-6 text-primary group-hover:scale-110 transition-transform duration-300"
     />
-    <Icon v-else :name="icon || 'mdi-page-layout-sidebar-right'" class="text-base-200 w-8 h-8" />
-  </div>
+    <Icon
+      v-else
+      :name="icon || 'kind-icon:tutorial'"
+      class="w-6 h-6 text-base-content/70 group-hover:text-primary group-hover:scale-110 transition-all duration-300"
+    />
+  </button>
 </template>
 
 <script setup lang="ts">
+// /components/content/layout/tutorial-toggle.vue
 import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
 import { useDisplayStore } from '@/stores/displayStore'
 import { usePageStore } from '@/stores/pageStore'
 

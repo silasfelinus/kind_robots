3a4141c9e3a4b8255ee2f73670ae2c37d06e6b4d
Silas Knight <silasfelinus@gmail.com>
2025-12-30 00:31:33 -0800

    attempting to restore old prisma schema

--- server/api/utils/prisma.ts
+++ server/api/utils/prisma.ts
@@ -1,15 +1,9 @@
 // /server/api/utils/prisma.ts
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
-const devLog: Prisma.LogLevel[] = ['error', 'warn']
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'production' ? [] : devLog,
-  })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 

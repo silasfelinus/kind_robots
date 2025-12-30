cdbbf2351996c724fe442acbb033d8c40ada5b25
Silas Knight <silasfelinus@gmail.com>
2025-12-30 05:27:49 -0800

    upgrading to prisma 7. troubleshooting

--- server/api/utils/prisma.ts
+++ server/api/utils/prisma.ts
@@ -1,9 +1,21 @@
 // /server/api/utils/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from './../../generated/prisma/client'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const databaseUrl = process.env.DATABASE_URL
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is missing')
+}
+
+const adapter = new PrismaMariaDb(databaseUrl)
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 

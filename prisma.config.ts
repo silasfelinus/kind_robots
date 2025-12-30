762492e9b5b7d9c300216b8ffee1a99486d12260
Silas Knight <silasfelinus@gmail.com>
2025-12-30 05:12:15 -0800

    restoring prisma 7 config

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// /prisma.config.ts
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
+    shadowDatabaseUrl: env('SHADOW_DATABASE_URL'),
+  },
+})

ee1fce8399dd60a0b8c06fa5a2d3cb7251066af8
Silas Knight <silasfelinus@gmail.com>
2026-04-14 22:33:18 -0700

    removed .value

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,11 +22,11 @@
 <script setup lang="ts">
 // /layouts/default.vue
 import { computed } from 'vue'
-import { useDisplayStore } from '@/stores/displayStore'
-import { useLayoutStore, type ResolvedLayoutKey } from '@/stores/layoutStore'
 import MobileLayout from '@/layouts/mobile.vue'
 import TabletLayout from '@/layouts/tablet.vue'
 import DesktopLayout from '@/layouts/desktop.vue'
+import { useDisplayStore } from '@/stores/displayStore'
+import { useLayoutStore, type ResolvedLayoutKey } from '@/stores/layoutStore'
 
 const displayStore = useDisplayStore()
 const layoutStore = useLayoutStore()

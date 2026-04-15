3c97944e115e35aef18bdd8bdec9b606b08289a7
Silas Knight <silasfelinus@gmail.com>
2026-04-14 17:02:02 -0700

    removed outside set timeout

--- app.vue
+++ app.vue
@@ -1,35 +1,4 @@
 <!-- /app.vue -->
-<template>
-  <div class="relative min-h-screen w-full overflow-hidden bg-base-100">
-    <NuxtPage />
-
-    <div class="fixed inset-0 z-50 pointer-events-none">
-      <div class="pointer-events-auto">
-        <footer-toggle />
-        <kind-loader />
-        <milestone-popup />
-      </div>
-
-      <animation-loader class="fixed inset-0 z-50 pointer-events-none" />
-
-      <div
-        v-if="showSwarm"
-        class="fixed inset-0 z-50 overflow-hidden pointer-events-none"
-      >
-        <butterfly-animation class="pointer-events-none" />
-      </div>
-    </div>
-
-    <div
-      v-if="isNavigating"
-      class="fixed inset-0 z-40 flex items-center justify-center animate-fade-in pointer-events-none"
-    >
-      <screen-debug />
-      <div class="loading loading-dots loading-lg text-primary" />
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 // /app.vue
 import { ref, computed } from 'vue'
@@ -48,7 +17,7 @@ router.beforeEach(() => {
 })
 
 router.afterEach(() => {
-  window.setTimeout(() => {
+  setTimeout(() => {
     isNavigating.value = false
   }, 400)
 })

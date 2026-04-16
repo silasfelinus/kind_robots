edd23017d893e48547134f0b1f520b935ea8bb2f
Silas Knight <silasfelinus@gmail.com>
2026-04-16 03:46:37 -0700

    better error

--- pages/error.vue
+++ pages/error.vue
@@ -1,24 +1,24 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center h-screen w-full bg-cover bg-center"
+    class="flex flex-1 min-h-full w-full items-center justify-center bg-cover bg-center"
     style="background-image: url('/images/background/error.png')"
   >
-    <div
-      class="bg-opacity-60 bg-base-100 p-8 rounded-2xl text-center shadow-lg"
-    >
+    <div class="bg-base-100/60 p-8 rounded-2xl text-center shadow-lg">
       <h1 class="text-4xl font-bold text-primary mb-4">Page Not Found</h1>
+
       <p class="text-secondary mb-8">
         It looks like this page doesn't exist. Please use the buttons below to
         navigate.
       </p>
 
-      <div class="flex space-x-4">
+      <div class="flex space-x-4 justify-center">
         <button
           class="text-white bg-primary px-4 py-2 rounded-lg shadow-md hover:bg-secondary transition-colors"
           @click="goBack"
         >
           Go Back
         </button>
+
         <button
           class="text-white bg-primary px-4 py-2 rounded-lg shadow-md hover:bg-secondary transition-colors flex items-center space-x-2"
           @click="goHome"
@@ -34,17 +34,11 @@
 <script setup lang="ts">
 const router = useRouter()
 
-// Go back to the previous page
 const goBack = () => {
   router.back()
 }
 
-// Go to the home page
 const goHome = () => {
   router.push('/')
 }
 </script>
-
-<style scoped>
-/* You can add any additional styles for the page here */
-</style>

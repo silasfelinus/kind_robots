1faa3a6fd2a15ac15ff7ca4d3d5221a89846c77f
Silas Knight <silasfelinus@gmail.com>
2026-04-28 04:23:50 -0700

    removed requireEnv

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,6 @@
 // /nuxt.config.ts
 import tailwindcss from '@tailwindcss/vite'
 
-const requireEnv = (key: string) => {
-  const value = process.env[key]
-
-  if (!value) {
-    throw new Error(`Missing required environment variable: ${key}`)
-  }
-
-  return value
-}
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-08-13',
 

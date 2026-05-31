f678145cbf3301019d703e72a3182f0ff465394b
Silas M Knight <silasfelinus@gmail.com>
2026-05-30 22:51:20 -0700

    Update index.post.ts

--- server/api/bots/index.post.ts
+++ server/api/bots/index.post.ts
@@ -184,7 +184,7 @@ export default defineEventHandler(async (event) => {
             title: true,
             label: true,
             serverType: true,
-            generationEngine: true,
+            
           },
         },
         ArtImage: {

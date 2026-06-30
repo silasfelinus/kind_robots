888f5ceeb52f92739c7a65d26cfea25acf50c274
Silas M Knight <silasfelinus@gmail.com>
2026-06-30 00:10:49 -0700

    feat: mount wishmaster in conductor manager

--- components/pages/conductor-manager.vue
+++ components/pages/conductor-manager.vue
@@ -1,7 +1,14 @@
 <template>
-  <ConductorPage />
+  <WishmasterPage v-if="pageStore.workspaceCardKey === 'wishmaster'" />
+  <PortosPage v-else-if="pageStore.workspaceCardKey === 'portos'" />
+  <ConductorPage v-else />
 </template>
 
 <script setup lang="ts">
 import ConductorPage from '@/components/pages/conductor-page.vue'
+import PortosPage from '@/components/pages/portos-page.vue'
+import WishmasterPage from '@/components/pages/wishmaster-page.vue'
+import { usePageStore } from '@/stores/pageStore'
+
+const pageStore = usePageStore()
 </script>

0c096e57e6e053c45639816432b08f7a44c80030
Silas Knight <silasfelinus@gmail.com>
2026-07-14 05:15:38 -0700

    better test

---
+++ tmp-db-test.ts
@@ -0,0 +1,17 @@
+﻿import 'dotenv/config'
+
+async function main() {
+  const { default: prisma } = await import('./server/utils/prisma')
+
+  try {
+    const result = await prisma.$queryRawUnsafe('SELECT 1 AS ok')
+    console.log(result)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})

eb2da8a031e57dedbd13840cb0d08dbccd16f778
Silas M Knight <silasfelinus@gmail.com>
2026-07-29 15:53:31 -0700

    Tighten startup animation control state

--- stores/startupAnimationStore.ts
+++ stores/startupAnimationStore.ts
@@ -6,31 +6,42 @@ export const useStartupAnimationStore = defineStore(
   'startupAnimationStore',
   () => {
     const immersive = ref(false)
+    const controlsActive = ref(false)
     const exitRequest = ref(0)
 
     function setImmersive(value: boolean): void {
       immersive.value = value
     }
 
-    function toggleImmersive(): void {
-      immersive.value = !immersive.value
+    function enterControlMode(): void {
+      controlsActive.value = true
+      immersive.value = true
+    }
+
+    function leaveControlMode(): void {
+      controlsActive.value = false
+      immersive.value = false
     }
 
     function requestExit(): void {
+      controlsActive.value = false
       immersive.value = false
       exitRequest.value += 1
     }
 
     function reset(): void {
+      controlsActive.value = false
       immersive.value = false
       exitRequest.value = 0
     }
 
     return {
       immersive,
+      controlsActive,
       exitRequest,
       setImmersive,
-      toggleImmersive,
+      enterControlMode,
+      leaveControlMode,
       requestExit,
       reset,
     }

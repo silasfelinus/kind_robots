44932626aca70ae6d7ccc31ee97380241b8fe6a0
Silas M Knight <silasfelinus@gmail.com>
2026-08-11 11:37:01 -0700

    fix(model-builder): expose stage statuses semantically (#1782)

--- components/model-builder/model-builder-progress-matrix.vue
+++ components/model-builder/model-builder-progress-matrix.vue
@@ -115,6 +115,7 @@
               class="text-center"
             >
               <span
+                role="img"
                 class="inline-flex h-5 w-5 items-center justify-center rounded-full text-[10px]"
                 :class="statusClass(item.stages[stage.key].status)"
                 :title="item.stages[stage.key].status"

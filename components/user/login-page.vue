bc924c1a11136af24b32dfe2dcf6a78444c3ce56
Silas M Knight <silasfelinus@gmail.com>
2026-09-10 23:22:33 -0700

    style(interface): use shared icon sizing on login lock

--- components/user/login-page.vue
+++ components/user/login-page.vue
@@ -128,7 +128,7 @@
             >
               <Icon
                 name="kind-icon:lock"
-                class="h-5 w-5 shrink-0 text-secondary"
+                class="kr-icon-5 shrink-0 text-secondary"
               />
               <input
                 id="password"
